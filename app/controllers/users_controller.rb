c321483c7ff4bfdbd3d7d55a3adf8c552bf552ff
Sam Lin <greenlin111@gmail.com>
2018-01-27 16:18:03 +0800

    create user controller and tweets

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,6 @@
 class UsersController < ApplicationController
+  def tweets
+    @user = User.find(params[:id])
+    @tweets = @user.tweets
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   resources :tweets
 
+  resources :users, only: [:edit] do
+    member do
+      get :tweets
+    end
+  end
+
   root 'tweets#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
