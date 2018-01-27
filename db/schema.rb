8cd2ab8d18366bb958314d67aedc95ccd83933e6
Sam Lin <greenlin111@gmail.com>
2018-01-27 15:25:59 +0800

    create tweet model and add relation on user and tweet

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180127070234) do
+ActiveRecord::Schema.define(version: 20180127071104) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.text "description"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tweets_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
