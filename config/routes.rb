7dc0bacb49b1e65354f87c12e97e80ffab27c97b
Cindy Liu <cindyliu923@gmail.com>
2018-02-03 01:29:38 +0800

    add tweets and admin routes / add views for index

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,13 @@ Rails.application.routes.draw do
   devise_for :users
 
   # 請依照專案指定規格來設定路由
+  resources :tweets, only: [:index, :create]
+  root "tweets#index"
+
+  namespace :admin do 
+    resources :tweets, only: [:index, :destroy]
+    resources :users, only: [:index]
+    root "tweets#index"
+  end
 
 end
