5618c47e1425cee9f74a8f4851371ab00c6db5e8
Max <crono0720@gmail.com>
2018-02-02 23:42:01 +0800

    database setting

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180201102838) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "followships", force: :cascade do |t|
     t.integer "user_id"
     t.integer "following_id"
