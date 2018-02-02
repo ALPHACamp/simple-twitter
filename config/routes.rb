25367bc3cd4200ca7063761f8442eace88948792
binyaha <yahabin@gmail.com>
2018-02-02 23:26:57 +0800

    前後台基本頁面

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
 
+  resources :tweets
+  resources :user
+  root "tweets#index"
+
+  namespace :admin do
+    resources :tweets
+  end
+
   # 請依照專案指定規格來設定路由
 
 end
