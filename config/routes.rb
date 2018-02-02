7ba603ca088b7475198dc0bd9c0d1dbf05ccdb82
wusunghan   <diadora1166@gmail.com>
2018-02-02 22:35:43 +0800

    add tweets index action and view for admin

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
 
-  	 root"tweets#index"
+  root"tweets#index"
+  namespace :admin do
+    root "tweets#index"
+  end
+
   # 請依照專案指定規格來設定路由
 
 end
