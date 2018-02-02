5d3bb4e37de428b21a55225df7a0711eecbc9b48
eric07109 <eric07109@gmail.com>
2018-02-02 23:54:45 +0800

    extend devise registration view and application_controller to allow name as a parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,20 @@ class ApplicationController < ActionController::Base
   # https://github.com/plataformatec/devise#strong-parameters
   # 注意有 sign_up 和 account_update 兩種參數要處理
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
   def authenticate_admin
   	unless current_user.admin?
   		flash[:alert] = "You are not allowed to access Admin page"
   		redirect_to root_path
   	end
   end
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+
 end
