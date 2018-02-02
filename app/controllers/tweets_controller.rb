ae0560d346eeec9e51aa781e372db8658c66648d
binyaha <yahabin@gmail.com>
2018-02-03 07:11:16 +0800

    前台tweet資訊

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,15 +5,26 @@ class TweetsController < ApplicationController
   def index
     #@users # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
     @tweets = Tweet.all
+    @tweet = Tweet.new
   end
 
   def create
+    @tweet =  Tweet.new(tweet_params)
+    @tweet.user_id = current_user.id
+    @tweet.save
+    redirect_to tweets_path
   end
 
   def like
   end
 
   def unlike
+  end
+
+    private
+
+  def tweet_params
+    params.require(:tweet).permit(:description)
   end
 
 end
