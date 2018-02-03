1e99c6a601612a05e34750e290f0a81f5d3f3f0e
Tony Lin <tonylin1208@gmail.com>
2018-02-03 22:43:28 +0800

    finish 1st admin/tweet index

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -2,6 +2,7 @@ class Admin::TweetsController < Admin::BaseController
   before_action :authenticate_user!
   before_action :authenticate_admin
   def index
+    @tweets = Tweet.all.order(created_at: :desc).page(params[:page]).per(10)
   end
 
   def destroy
