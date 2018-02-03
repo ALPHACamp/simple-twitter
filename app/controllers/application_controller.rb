5b867c7b60811f64ea1d112b0c2814284203bede
spy <sp831031@gmail.com>
2018-02-03 13:21:50 +0800

    modify authentication_admin method location,and update tweet#index page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,12 +10,5 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 
-private
 
-  def authenticate_admin
-    unless current_user.admin?
-      flash[:alert] = "You are not admin!"
-      redirect_to root_path
-    end
-  end
 end
