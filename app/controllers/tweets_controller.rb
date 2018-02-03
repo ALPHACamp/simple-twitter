d2a92a0061c6e145574a86b28397a50e78fe00a6
annie chiang <annie100patty@gmail.com>
2018-02-03 23:15:00 +0800

    edit index action in tweets_controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,7 +4,7 @@ class TweetsController < ApplicationController
     @tweets = Tweet.all;
 
     # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
-    @users  = User.all;
+    @users  = User.all.limit(10);
   end
 
   def create
