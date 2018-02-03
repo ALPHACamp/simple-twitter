28c9cf365b0914a22e9314da902ebecfe3af2f71
Riemann <dr.mactt@gmail.com>
2018-02-03 18:45:57 +0800

    add index page for Front-End and def index and create method

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,16 +2,29 @@ class TweetsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    #@users # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
+    @users = User.order(followers_count: :desc).limit(10) # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
+    @tweets = Tweet.all
   end
 
   def create
+    @user == current_user.id
+    @tweet = Tweet.new(tweet_params)
+    @tweet.save
+    flash[:notice] = "tweet was successfully created"
+
+    redirect_to tweets_path
   end
 
   def like
   end
 
   def unlike
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:description, :user_id)
   end
 
 end
