5002dc3a80b7235cba503e63b84de9200837ff1b
smallwordWISH <word714cs@gmail.com>
2018-02-03 21:02:00 +0800

    complete like/unlike function

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,14 +2,12 @@ class TweetsController < ApplicationController
 
   def index
     @users = User.order(followers_count: :desc).limit(10) # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
-
     @tweets = Tweet.order(created_at: :desc)
 
     @tweet = Tweet.new
   end
 
-  def create
-    
+  def create    
     @tweet = Tweet.new(tweet_params)
     @tweet.user = current_user
 
@@ -23,9 +21,16 @@ class TweetsController < ApplicationController
   end
 
   def like
+    @tweet = Tweet.find(params[:id])
+    Like.create(tweet: @tweet, user: current_user)
+    redirect_back(fallback_location: root_path)
   end
 
   def unlike
+    @tweet = Tweet.find(params[:id])
+    likes = Like.where(tweet: @tweet, user: current_user)
+    likes.destroy_all
+    redirect_back(fallback_location: root_path)
   end
 
   private 
