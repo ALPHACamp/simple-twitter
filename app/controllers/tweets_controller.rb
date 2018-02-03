7788dadd844badf191b22094de3bb017c994982e
Chad <xmen11034@gmail.com>
2018-02-03 20:54:13 +0800

    created create action for public user

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -7,12 +7,27 @@ class TweetsController < ApplicationController
   end
 
   def create
+    @tweet = Tweet.new(tweet_params)
+    @tweet.user = current_user
+    if @tweet.save
+      flash[:notice] = "tweet was successfully created"
+      redirect_to tweets_path
+    else
+      flash.now[:alert] = "tweet was failed to create"
+      render :index
+    end
   end
 
   def like
   end
 
   def unlike
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:description)
   end
 
 end
