c84005e19a26f1f9b711e071daf99a2ac2579879
Tony Lin <tonylin1208@gmail.com>
2018-02-03 22:18:27 +0800

    add sign_in name and uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,12 @@ class User < ApplicationRecord
 
   mount_uploader :avatar, AvatarUploader
 
+  has_many :tweets
+
   # 需要 app/views/devise 裡找到樣板，加上 name 屬性
   # 並參考 Devise 文件自訂表單後通過 Strong Parameters 的方法
   validates_presence_of :name
+  validates :name, uniqueness: { message: "error. The name has already been taken."}
   # 加上驗證 name 不能重覆 (關鍵字提示: uniqueness)
 
   def admin?
