569212b59f3b0c87a51aa972cc657b782842501b
Wells <chawkeye@gmail.com>
2018-02-03 11:58:48 +0800

    user can reply other's tweet

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -3,9 +3,22 @@ class RepliesController < ApplicationController
   def index
     @tweet = Tweet.find(params[:tweet_id])
     @replies = @tweet.replies.all
+    @reply = Reply.new
   end
 
   def create
+    @tweet = Tweet.find(params[:tweet_id])
+    @reply = @tweet.replies.build(comment: reply_params[:comment], user: current_user)
+    if @reply.save
+      redirect_back fallback_location: root_path, notice: "reply success"
+    else
+      flash[:alert] = @reply.errors.full_messages.to_sentence
+      redirect_back fallback_location: root_path
+    end
   end
 
+  private
+  def reply_params
+    params.require(:reply).permit(:comment)
+  end
 end
--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,4 +1,4 @@
 class Reply < ApplicationRecord
   belongs_to :user
-  belongs_to :tweet, :counter_cathe => true
+  belongs_to :tweet, :counter_cache => true
 end
