2bb893a0f6c64bc03ca98163adc2bb671686ccf0
spy <sp831031@gmail.com>
2018-02-03 16:23:16 +0800

    add admin tweets#index page and admin can delete others's tweets

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -3,8 +3,14 @@ class Admin::TweetsController < Admin::BaseController
   before_action :authenticate_admin
 
   def index
+    @tweets = Tweet.all
+    
   end
 
   def destroy
+    @tweet = Tweet.find(params[:id])
+    @tweet.destroy
+    redirect_to admin_tweets_path
+    flash[:alert] = "The tweet was deleted."
   end
 end
