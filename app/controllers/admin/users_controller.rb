8e89865273b06480820ad510843e0a6af835c1a9
yvesen <l.sen.t@hotmail.com>
2018-02-03 21:13:23 +0800

    login and out

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,5 @@
 class Admin::UsersController < Admin::BaseController
+  before_action :authenticate_admin
   def index
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,11 +14,11 @@ class UsersController < ApplicationController
   end
 
   def followers
-    @followers # 基於測試規格，必須講定變數名稱
+    # @followers # 基於測試規格，必須講定變數名稱
   end
 
   def likes
-    @likes # 基於測試規格，必須講定變數名稱
+    # @likes # 基於測試規格，必須講定變數名稱
   end
 
 end
