43a5338c88c9ccd59d04a0dc626fb0998a46da66
chunchaolee <chunchaolee@gmail.com>
2018-02-03 17:07:47 +0800

    added name attribute as an additional strong parameter for Devise sign_up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,14 @@ class ApplicationController < ActionController::Base
   # 請參考 Devise 文件自訂表單後通過 Strong Parameters 的方法
   # https://github.com/plataformatec/devise#strong-parameters
   # 注意有 sign_up 和 account_update 兩種參數要處理
+
+  # 加上自訂欄位name到Devise的註冊和編輯頁面
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end
+  
 end
