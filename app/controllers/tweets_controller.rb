23e4d1269867d238b77ab5111464ae047d18fef5
Wells <chawkeye@gmail.com>
2018-02-03 12:57:33 +0800

    user can like tweet on index page

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,5 @@
 class TweetsController < ApplicationController
-
+  before_action :set_tweet, only: [:like, :unlike]
   def index
     @users # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
     @tweets = Tweet.all.order(created_at: :desc)
@@ -18,12 +18,20 @@ class TweetsController < ApplicationController
   end
 
   def like
+    @tweet.likes.create!(user: current_user)
+    redirect_back fallback_location: root_path
   end
 
   def unlike
+    @like = Like.where(user: current_user, tweet: @tweet).first
+    @like.destroy
+    redirect_back fallback_location: root_path
   end
 
   private
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
+  end
   def tweet_params
     params.require(:tweet).permit(:description)
   end
