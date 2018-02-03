41970c906d6bf539da6661aa25789a2a8da6ff1f
chunchaolee <chunchaolee@gmail.com>
2018-02-03 14:32:00 +0800

    added tweets#index as root on routes.rb, and created tweets index.html.erb under views/tweets

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
 
-  # 請依照專案指定規格來設定路由
-
+  # 設置tweets index作為網站首頁
+  resources :tweets, only: [:index]
+  root 'tweets#index'
 end
