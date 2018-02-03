da1a82e10fc8640094cf69111375459390561472
eggyy1224 <eggyy1224@gmail.com>
2018-02-03 16:08:04 +0800

    user可以新增tweet

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -2,6 +2,7 @@ class Admin::TweetsController < Admin::BaseController
   before_action :authenticate_user!
   before_action :authenticate_admin
   def index
+    @tweets = Tweet.all
   end
 
   def destroy
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,16 +2,26 @@ class TweetsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @tweets  = Tweet.all
+    @tweet = Tweet.new
   end
 
   def create
+    @tweet = current_user.tweets.build(tweet_params)
+    @tweet.save
+    flash[:notice] = "成功新增"
+    redirect_to root_path
   end
 
   def like
   end
 
   def unlike
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:description)
   end
 
 end
