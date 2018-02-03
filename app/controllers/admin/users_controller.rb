54c554975a160587072cc7e9d08e0ab40431d148
Wells <chawkeye@gmail.com>
2018-02-03 20:28:53 +0800

    fixed admin::users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,5 @@
 class Admin::UsersController < Admin::BaseController
   def index
-    @users = User.all
+    @users = User.all.order(tweets_count: :desc)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:edit, :update, :likes, :followings, :followers]
+  before_action :set_user, only: [:tweets, :edit, :update, :likes, :followings, :followers]
   def tweets
-    @tweets = current_user.tweets.order(created_at: :desc)
+    @tweets = @user.tweets.order(created_at: :desc)
   end
 
   def update
