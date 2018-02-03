bbb28fdb7844fbb07453fe2195e8fdd4259f2c0a
anna770822 <anna770822@hotmail.com>
2018-02-03 14:18:59 +0800

    create routes for tweets :show and create replies :index :create under tweets :show

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   devise_for :users
 
   # 請依照專案指定規格來設定路由
-  resources :tweets, only: [:index,:create]
+  resources :tweets, only: [:index, :create]
+
+  resources :tweets, only: [:show] do
+    resources :replies, only:[:index, :create]
+  end
+  
   root "tweets#index"
 
   namespace :admin do 
