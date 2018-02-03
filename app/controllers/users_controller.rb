08829e333e6ff3f2aa2f65796aa1f8ecca7cb4af
spreered <spreered@gmail.com>
2018-02-03 22:41:12 +0800

    add user#edit and user#tweets views, controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-
+  before_action :set_user , only: [:tweets,:edit,:update,:followings, :followers]
   def tweets
   end
 
@@ -7,6 +7,14 @@ class UsersController < ApplicationController
   end
 
   def update
+    if @user.update(user_params)
+      flash[:notice] = "資料更新成功"
+      redirect_to tweets_user_path(@user)
+    else
+      flash[:alert] = @user.errors.full_messages.to_sentence
+      render :edit
+    end
+
   end
 
   def followings
@@ -21,4 +29,13 @@ class UsersController < ApplicationController
     @likes # 基於測試規格，必須講定變數名稱
   end
 
+  private 
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name,:introduction,:avatar)
+  end
 end
