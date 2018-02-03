6cc0a92e8051a47f229c007c1f169f5a8129c335
eggyy1224 <eggyy1224@gmail.com>
2018-02-03 11:25:32 +0800

    首頁前台後台routes 設定完成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
 
-  # 請依照專案指定規格來設定路由
+  root "tweets#index"
+
+  resources :tweets, only: [:index, :create]
+
+  namespace :admin do
+    resources :tweets, only: [:index, :destroy]
+  end
 
 end
