27b3ac59d7fa2731d997c01bc27571eb033c3a92
KC <weilihmen@gmail.com>
2018-02-04 00:12:27 +0800

    cancel followers counter

--- app/models/followship.rb
+++ app/models/followship.rb
@@ -5,5 +5,4 @@ class Followship < ApplicationRecord
   belongs_to :following, class_name: "User" 
   #followship裡面有個following id，但沒有following這個model，所以要用class_name把這個id指向USER model
 
-  belongs_to :inverse_followship, class_name:"User", primary_key:"following_id", counter_cache: true
 end
