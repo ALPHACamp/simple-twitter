caa5221d1279858dd4d2312fde3f020b11280eca
Yuyu <qscez2001@gmail.com>
2018-02-03 15:06:28 +0800

    user can create a tweet

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,10 +2,21 @@ class TweetsController < ApplicationController
 
   def index
     @tweets = Tweet.all
+
+    @tweet = Tweet.new
+
     @users # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
   end
 
   def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      flash[:notice] = "tweet was successfully created"
+      redirect_to tweets_path
+    else
+      flash.now[:alert] = "tweet was failed to create"
+      render :index
+    end
   end
 
   def like
@@ -13,5 +24,12 @@ class TweetsController < ApplicationController
 
   def unlike
   end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:description)
+  end
+
 
 end
