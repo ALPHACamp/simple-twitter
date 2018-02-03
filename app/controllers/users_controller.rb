a2e6447c73c9a310781399f8160408bd65fa741b
eric07109 <eric07109@gmail.com>
2018-02-03 15:00:13 +0800

    make update and edit action available only for current user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,26 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:update, :edit]
 
   def tweets
   end
 
   def edit
+    if @user != current_user
+      redirect_to edit_user_path(current_user)
+      flash[:alert] = "You cannot edit other users"
+    end
   end
 
   def update
+    
+    if @user.update(user_params)
+      redirect_to edit_user_path(@user)
+      flash[:notice] = "user was successfully updated"
+    else
+      redirect_to edit_user_path(@user)
+      flash[:alert] = "user cannot be updated because you missed some fields"
+    end
+    
   end
 
   def followings
@@ -19,6 +33,15 @@ class UsersController < ApplicationController
 
   def likes
     @likes # 基於測試規格，必須講定變數名稱
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :avatar)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
