91aed278254e1a7808ea24ab6fcad83b1e9acb32
Riemann <dr.mactt@gmail.com>
2018-02-03 14:47:30 +0800

    setup user index page for back-end

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,5 @@
 class Admin::UsersController < Admin::BaseController
   def index
+    @users = User.all     # S16L1U2 有最近十筆排序 參考一下
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  private
-
-  # 後台身份驗證
-  def authenticate_admin
-    unless current_user.admin?
-      flash[:alert] = "Not allow!"
-      redirect_to tweets_path
-    end
-  end
-
+  # 後台身份驗證，移到 base_controller
 
   # 請參考 Devise 文件自訂表單後通過 Strong Parameters 的方法
   # https://github.com/plataformatec/devise#strong-parameters
