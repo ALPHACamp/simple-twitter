4cfcbc51198989311ad64e742c1b0ccbc9f71077
Wells <chawkeye@gmail.com>
2018-02-03 09:37:39 +0800

    user can set name in signup page and name should be unique

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,12 @@ class ApplicationController < ActionController::Base
   # 請參考 Devise 文件自訂表單後通過 Strong Parameters 的方法
   # https://github.com/plataformatec/devise#strong-parameters
   # 注意有 sign_up 和 account_update 兩種參數要處理
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :introduction, :avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :introduction, :avatar])
+  end
 end
