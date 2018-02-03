ad16591f2fe3c6e77488963e21a2413f0cf886cd
Wells <chawkeye@gmail.com>
2018-02-03 09:27:02 +0800

    setup admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+file = File.open("#{Rails.root}/public/avatar/user#{rand(1..20)}.jpg")
+User.create(
+  email: "admin@well.com",
+  password: "123456",
+  name: "Admin",
+  introduction: "Administrator",
+  avatar: file,
+  role: "admin"
+)
+puts "admin has created"
