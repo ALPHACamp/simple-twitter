021857182dd28ca654234a325826d4ed124c16df
Chia-Hung Lin <qaz741012@gmail.com>
2018-02-03 16:32:17 +0800

    user should sign up with name which can't be repetitive with other existed user

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
 
+  resources :users, only: [:edit]
+
   root "tweets#index"
 
   namespace :admin do
