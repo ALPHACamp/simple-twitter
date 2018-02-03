06213208d0b2c8519490ef8247f7087c1ae3f829
binyaha <yahabin@gmail.com>
2018-02-03 19:31:16 +0800

    完成tweets(首頁)初版

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,7 +3,7 @@ class TweetsController < ApplicationController
   
 
   def index
-    #@users # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
+    @users = User.all
     @tweets = Tweet.all
     @tweet = Tweet.new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
 
-  resources :tweets
-  resources :user
   root "tweets#index"
+  resources :tweets
+
+  resources :users ,only: [:show] do
+    
+    resources :tweets, only: [:index]
+   
+  end
+  
 
   namespace :admin do
     resources :tweets
