db04f782ee204fb18bcd41cc26bec2e4a7ec89d1
Wells <chawkeye@gmail.com>
2018-02-03 14:10:36 +0800

    user can follow/unfollow other user

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -1,7 +1,18 @@
 class FollowshipsController < ApplicationController
   def create
+    @followship = current_user.followships.build(following_id: params[:following_id])
+    if @followship.save
+      redirect_back fallback_location: root_path, notice: "follow success"
+    else
+      flash[:alert] = @followship.errors.full_messages.to_sentences
+      redirect_back fallback_location: root_path
+    end
   end
 
   def destroy
+    @followship = current_user.followships.where(following_id: params[:id]).first
+    @followship.destroy
+    flash[:alert] = "unfollowed!!"
+    redirect_back fallback_location: root_path
   end
 end
