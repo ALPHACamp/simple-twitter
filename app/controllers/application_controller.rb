39af56623aa91821b037624203b8a55edd41d956
jiananWang <a0918666589@gmail.com>
2018-02-03 14:34:53 +0800

    authenticate user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :authenticate_user!
+
   # 請參考 Devise 文件自訂表單後通過 Strong Parameters 的方法
   # https://github.com/plataformatec/devise#strong-parameters
   # 注意有 sign_up 和 account_update 兩種參數要處理
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Default admin
+User.create!(
+    name: "Jianan",
+    email: "root@example.com",
+    password: "12345678",
+    role: "admin",
+    introduction: "Sed pulvinar eleifend elit, et ultricies arcu iaculis in. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Duis blandit non nisi id efficitur. In commodo ipsum id ligula pretium, sed pharetra nisl ullamcorper.",
+  )
+
+puts "Default admin created!"
