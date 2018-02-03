2804d5b8a922f779f4b289e975ba93e7817d972c
Chia-Hung Lin <qaz741012@gmail.com>
2018-02-03 22:21:53 +0800

    create some users and tweets routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,31 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
 
-  resources :users, only: [:edit, :update]
-
   root "tweets#index"
+
+  resources :users, only: [:edit, :update] do
+
+    member do
+      get :tweets
+    end
+
+  end
+
+  resources :tweets, only: :create do
+
+    member do
+      post :like
+      post :unlike
+    end
+
+  end
 
   namespace :admin do
     root "tweets#index"
+
+    resources :tweets, only: :destroy
+
+    resources :users, only: :index
   end
 
 end
