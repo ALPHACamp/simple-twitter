45d686a919e144bb92abb6e408737bbd38e0f0e3
yvesen <l.sen.t@hotmail.com>
2018-02-03 22:45:29 +0800

    fake_user and seed

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -1,8 +1,17 @@
 class Admin::TweetsController < Admin::BaseController
+  before_action :set_restaurant, only:  [:show, :destroy]
   def index
     @tweets = Tweet.all
   end
 
-  def destroy
+  def show
+    @tweet = Tweet.find(params[:id])
   end
+
+  def destroy
+    @tweet.destroy
+    redirect_to admin_tweets_path
+    flash[:alert] = "tweet was deleted"
+  end
+
 end
