13078c3c6b3fd0f19b4a28a85ad658bfba6a6040
KC <weilihmen@gmail.com>
2018-02-03 19:29:07 +0800

    following and followers model association

--- app/models/followship.rb
+++ app/models/followship.rb
@@ -1,4 +1,8 @@
 class Followship < ApplicationRecord
   validates :following_id, uniqueness: { scope: :user_id }
+  
+  belongs_to :user
+  belongs_to :following, class_name: "User" 
+  #followship裡面有個following id，但沒有following這個model，所以要用class_name把這個id指向USER model
 
 end
