4dd5280eea2193f6024bb5af443d0daa39564318
spy <sp831031@gmail.com>
2018-02-03 11:10:53 +0800

    setup user and admin authenticatin and add user name to registration and edit form

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
   # 需要 app/views/devise 裡找到樣板，加上 name 屬性
   # 並參考 Devise 文件自訂表單後通過 Strong Parameters 的方法
   validates_presence_of :name
+  validates_uniqueness_of :name
   # 加上驗證 name 不能重覆 (關鍵字提示: uniqueness)
 
 end
