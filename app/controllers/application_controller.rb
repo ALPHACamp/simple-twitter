f41d9e1401f57ab7c50ae58a20641f2f3b06b9aa
chunchaolee <chunchaolee@gmail.com>
2018-02-03 18:08:02 +0800

    added admin method on user model, authenticate_user! on ApplicationController, modified application.html.erb, style.scss

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::Base
   # https://github.com/plataformatec/devise#strong-parameters
   # 注意有 sign_up 和 account_update 兩種參數要處理
 
+  # UsersController, TweetsController, RepliesController, FollowshipsController
+  before_action :authenticate_user!
+
+
   # 加上自訂欄位name到Devise的註冊和編輯頁面
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -13,5 +17,5 @@ class ApplicationController < ActionController::Base
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     end
-  
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,10 @@ class User < ApplicationRecord
   # 並參考 Devise 文件自訂表單後通過 Strong Parameters 的方法
   validates_presence_of :name
   # 加上驗證 name 不能重覆 (關鍵字提示: uniqueness)
-  validates_uniqueness_of :email, :name
+  validates_uniqueness_of :name
+
+  def admin?
+    self.role == "admin"
+  end
 
 end
