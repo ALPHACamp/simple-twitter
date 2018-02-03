0cbd012b824c504c626ac4fb9cd848155d014389
laibuc <ljyuan5193@gmail.com>
2018-02-03 09:24:09 +0800

    finish routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,29 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
 
+  resources :tweets, only: [:index, :create] do
+  member do
+      resources :replies, only: [:index, :create]
+        post :like
+        post :unlike
+  end
+    end
+  resources :users, only: [:edit] do
+      member do
+        get :tweets
+        get :followings
+        get :followers
+        get :likes
+      end
+        end
+  resources :followships, only: [:create, :destroy]
+
+  namespace :admin do
+  resources :tweets, only: [:index,:destroy]
+  resources :users, only: [:index]
+  root "tweets#index"
+end
+  root "tweets#index"
   # 請依照專案指定規格來設定路由
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+User.create(email: "iiiii@is", password: "123456", role: "admin", name: "root")
+puts "Default admin created!"
