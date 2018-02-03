96785eb4f48785349125229e4ac0b19b9664a8f4
fion chen <wakinglife_615@yahoo.com>
2018-02-03 23:24:25 +0800

    set routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,42 @@ Rails.application.routes.draw do
 
   # 請依照專案指定規格來設定路由
 
+     root "tweets#index"
+
+     authenticated :user do
+        root to: "tweets#index"
+      end
+      unauthenticated :user do
+        root 'static_pages#index'
+      end
+
+      resources :tweets, except: [:new, :edit] do
+        member do
+          post :reply
+        end
+      end
+      resources :users, except: [:new, :create] do
+        member do
+          get 'followers'
+          get 'followings'
+          get 'likes'
+          get 'replies'
+        end
+      end
+
+      resources :followships, only: [:create, :destroy]
+      resources :likes, only: [:create, :destroy]
+      resources :replies, only: [:create, :destroy]
+      # resources :find_friends, only: :index
+
+
+
+
+
+     namespace :admin do
+       resources :tweets
+       resources :users
+       root "tweets#index"
+  end
+
 end
