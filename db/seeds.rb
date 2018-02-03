b47d04994718dffdf76fa097511d666e7296c02d
Yuyu <qscez2001@gmail.com>
2018-02-03 14:07:23 +0800

    set up admin authentication in base_controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,8 @@
 
 User.create(email: "root@example.com", password: "123456", role: "admin", name: "root")
 puts "Default admin created!"
+
+# Default user
+
+User.create(email: "yuyu@example.com", password: "123456", name: "yuyu")
+puts "Default user created!"
