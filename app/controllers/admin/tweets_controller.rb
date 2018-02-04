a350e520dff08eb0580ec31009bacf94830c77b8
Chad <xmen11034@gmail.com>
2018-02-04 21:55:46 +0800

    create admin delete tweet function

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -1,6 +1,7 @@
 class Admin::TweetsController < Admin::BaseController
   before_action :authenticate_user!
   before_action :authenticate_admin
+
   
   def index
     @tweets = Tweet.all
@@ -8,5 +9,9 @@ class Admin::TweetsController < Admin::BaseController
   end
 
   def destroy
+    @tweet = Tweet.find(params[:id])
+    @tweet.destroy
+    redirect_to admin_tweets_path
+    flash[:alert] = "Tweet was deleted"
   end
 end
