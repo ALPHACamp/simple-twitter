f0da714f390affe765e249d2faa8c2e47d79505c
eggyy1224 <eggyy1224@gmail.com>
2018-02-04 15:01:42 +0800

    使用者編輯資料功能初步完成

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
     resources :tweets, only: [:index, :destroy]
   end
 
-  resources :users, only: [:edit] do
+  resources :users, only: [:edit, :update] do
     member do
       get :tweets
     end
