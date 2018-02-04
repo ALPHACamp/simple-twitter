0c8a895cbae9671ddc3d903865856f4b628bbeda
Tony Lin <tonylin1208@gmail.com>
2018-02-04 11:58:30 +0800

    change validate of admin into basecontroller

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -1,6 +1,5 @@
 class Admin::TweetsController < Admin::BaseController
-  before_action :authenticate_user!
-  before_action :authenticate_admin
+  
   def index
     @tweets = Tweet.all.order(created_at: :desc).page(params[:page]).per(10)
   end
