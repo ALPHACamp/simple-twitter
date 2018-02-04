64e01b171b3972e39ed24350c3a0cfc9d74f855a
LanceRabbit <lancerabbit@gmail.com>
2018-02-04 20:50:56 +0800

    add tweet destroy action for admin

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -4,8 +4,12 @@ class Admin::TweetsController < Admin::BaseController
   before_action :authenticate_admin
   
   def index
+    @tweets = Tweet.page(params[:page]).per(10)
   end
 
   def destroy
+    @tweet.destroy
+    redirect_to admin_tweets_path
+    flash[:alert] = "tweet was deleted"    
   end
 end
