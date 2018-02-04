643760c8e4ad13e92309f7437441937d805f5915
Max <crono0720@gmail.com>
2018-02-05 02:24:38 +0800

    add like buttom

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -5,5 +5,10 @@ class Tweet < ApplicationRecord
   belongs_to :user
   has_many :replies, dependent: :destroy
   has_many :likes
+  has_many :liked_users, through: :likes, source: :user
+
+  def is_liked?(user)
+    self.liked_users.include?(user)
+  end
 
 end
