dc961f0e590f0141a8ffa0d0482541c9920f025a
LanceRabbit <lancerabbit@gmail.com>
2018-02-04 19:37:20 +0800

    add tweet index action and view for public users

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,22 @@ Rails.application.routes.draw do
   devise_for :users
 
   # 請依照專案指定規格來設定路由
+  # 前台
+  root "tweets#index"
+  # # 推播
+  # resources :tweets, only: [:index, :show] do
+  
+  # end
+  
+  # # 個人資訊
+  # resources :users, only: [:index, :show, :edit, :update]
 
+  # # 追蹤達人
+  # resources :followships, only: [:create, :destroy]
+
+  # 後台
+  # namespace :admin do
+  #   resources :tweets
+  #   root "tweets#index"
+  # end  
 end
