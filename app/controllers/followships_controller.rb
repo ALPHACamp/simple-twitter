ec6e38066d4cbf7757b9c261f645df7a95148130
KC <weilihmen@gmail.com>
2018-02-04 09:21:32 +0800

    followships controller first setup

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -1,7 +1,15 @@
 class FollowshipsController < ApplicationController
-  def create
+	def create
+    @followship = current_user.followships.new(following_id: params[:following_id])
+    if @followship.save
+      redirect_back(fallback_location: root_path)
+    else
+      redirect_back(fallback_location: root_path)
+    end
   end
-
   def destroy
+    @followship = current_user.followships.where(following_id: params[:id])
+    @followship.destroy_all
+    redirect_back(fallback_location: root_path)
   end
 end
