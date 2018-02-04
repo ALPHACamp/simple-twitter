da0d990bbb8a7d67659e07f7d427853c19950739
spy <sp831031@gmail.com>
2018-02-04 13:02:55 +0800

    add the user's tweet page

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,7 +2,7 @@ class Tweet < ApplicationRecord
   validates_length_of :description, maximum: 140
 
   belongs_to :user
-  has_many :replies
-  has_many :likes
+  has_many :replies ,dependent: :destroy 
+  has_many :likes ,dependent: :destroy 
   
 end
