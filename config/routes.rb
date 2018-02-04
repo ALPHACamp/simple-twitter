d675e368c2b3e4ca972f86e975967fa2bff285a9
Max <crono0720@gmail.com>
2018-02-04 23:32:58 +0800

    add reply routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
   # 請依照專案指定規格來設定路由
   root 'tweets#index'
 
-  resources :tweets
+  resources :tweets, only: [:index, :create] do
+    resources :replies, only: [:index, :create]
+  end
+
   resources :users, only: [:edit, :update] do
     member do
      get :tweets
