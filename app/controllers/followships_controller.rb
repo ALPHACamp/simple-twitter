9ea32106952781f3bf5023cdec49c17817a1e83f
Max <crono0720@gmail.com>
2018-02-05 03:51:56 +0800

    user can't follow self

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -1,8 +1,10 @@
 class FollowshipsController < ApplicationController
   def create
     @following = User.find(params[:following_id])
-    @followship = Followship.new(following: @following, user: current_user)
-    @followship.save
+    if @following != current_user
+      @followship = Followship.new(following: @following, user: current_user)
+      @followship.save
+    end
     redirect_back(fallback_location: root_path)
   end
 
