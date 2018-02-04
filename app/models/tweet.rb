440617752a622653d6dd8d1bdfcaaf117738aa7f
iceland101113 <r02521115@ntu.edu.tw>
2018-02-04 22:28:51 +0800

    add like(count) button

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,11 +3,23 @@ class Tweet < ApplicationRecord
 
   belongs_to :user
   has_many :likes, dependent: :destroy 
+  has_many :liked_users, through: :likes, source: :user 
+
   has_many :replies, dependent: :destroy 
 
   def count_replies
     self.replies_count = self.replies.size
     self.save
+  end
+
+  def count_likes
+    self.likes_count = self.likes.size
+    self.save
+  end
+
+
+  def is_liked?(user)
+    self.liked_users.include?(user)
   end
 
 end
