806ed1815bac566f1d0f4ba564f31827273748ea
Tony Lin <tonylin1208@gmail.com>
2018-02-04 12:38:41 +0800

    refactoring the validate

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,6 @@
 class Admin::UsersController < Admin::BaseController
   def index
-    @users = User.all
+    # 管理者可以瀏覽站內所有的使用者清單，該清單按推播文數排序
+    @users = User.all.page(params[:page]).per(10)
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
+  
   protected
 
   def configure_permitted_parameters
@@ -12,14 +13,4 @@ class ApplicationController < ActionController::Base
   # 請參考 Devise 文件自訂表單後通過 Strong Parameters 的方法
   # https://github.com/plataformatec/devise#strong-parameters
   # 注意有 sign_up 和 account_update 兩種參數要處理
-
-
-  private
-
-  def authenticate_admin
-    unless current_user.admin?
-      flash[:alert] = "Not allow!"
-      redirect_to root_path
-    end
-  end
 end
