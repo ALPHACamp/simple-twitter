298b414b980056fea04113b5b09b290c4127ca70
Max <crono0720@gmail.com>
2018-02-05 02:24:24 +0800

    add like and unlike action

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,5 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:like ,:unlike]
 
   def index
     @users # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
@@ -21,12 +22,21 @@ class TweetsController < ApplicationController
   end
 
   def like
+    Like.create(tweet: @tweet , user: current_user)
+    redirect_back(fallback_location: root_path)
   end
 
   def unlike
+    like = Like.where(tweet: @tweet , user: current_user)
+    like.destroy_all
+    redirect_back(fallback_location: root_path)
   end
 
   private
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
+  end
 
   def tweet_params
     params.require(:tweet).permit(:description)
