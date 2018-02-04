4238f9728ad16e3b68ddf9c444286f639bcada2c
iceland101113 <r02521115@ntu.edu.tw>
2018-02-04 20:53:45 +0800

    create reply area and add destroy reply action and button

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,9 +1,43 @@
 class RepliesController < ApplicationController
+  before_action :set_reply
 
   def index
+    @tweet = Tweet.find(params[:tweet_id])
+    @replies = @tweet.replies.order(created_at: :desc)
   end
 
   def create
+    @tweet = Tweet.find(params[:tweet_id])
+    @reply = @tweet.replies.build(reply_params)
+    @reply.user = current_user
+    @reply.save!
+    if @reply.save
+      flash[:notice] = "reply was successfully created"
+      redirect_to tweet_replies_path(@tweet)     
+    else
+      @replies = @tweet.replies.order(created_at: :desc)
+      render :index
+    end
+
+  end
+
+  def destroy
+    @tweet = Tweet.find(params[:tweet_id])
+    @reply = Reply.find(params[:id])
+    @reply.destroy
+    redirect_to tweet_replies_path(@tweet) 
+  end
+
+  def reply_params
+    params.require(:reply).permit(:comment)
+  end
+
+  def set_reply
+    if  params[:id]
+      @reply = Reply.find(params[:id])
+    else
+      @reply = Reply.new
+    end
   end
 
 end
