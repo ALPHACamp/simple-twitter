8626046f02e6d717d758bfa4a7ae351d9ed9f38b
jiananWang <a0918666589@gmail.com>
2018-02-04 21:58:51 +0800

    user can set name in registrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,8 @@ class User < ApplicationRecord
   validates_presence_of :name
   # 加上驗證 name 不能重覆 (關鍵字提示: uniqueness)
 
+  validates_uniqueness_of :email, :name
+
   def admin?
     self.role == 'admin'
   end
