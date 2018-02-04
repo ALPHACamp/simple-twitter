182ebc7e16668cf0105177562b1ab93ae463646d
Kansetsu Kuo <kansetsu4@gmail.com>
2018-02-04 16:33:26 +0800

    Done: user can tweet on index

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,17 +2,35 @@ class TweetsController < ApplicationController
 
   def index
     # @users # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
-    @tweets = Tweet.order('created_at desc').page(params[:page]).per(9)
+    @tweets = Tweet.order('created_at desc').page(params[:page]).per(10)
+    # @tweets = current_user.tweets
     @tweet = Tweet.new
   end
 
   def create
+    @tweet = Tweet.new(tweet_params) 
+    @tweet.user = current_user
+    if @tweet.save
+      flash[:notice] = 'tweet was successfully created'
+      puts 'good!'
+      redirect_to root_path
+    else
+      flash.now[:alert] = 'tweet was failed to create'
+      puts 'gg!'
+      render :index
+    end
   end
 
   def like
   end
 
   def unlike
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:description)
   end
 
 end
