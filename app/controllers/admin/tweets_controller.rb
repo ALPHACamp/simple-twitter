00bdf718c46f8604e73c392a6555dee7d3273210
wusunghan   <diadora1166@gmail.com>
2018-02-04 11:46:02 +0800

    管理者可以瀏覽所有的推播與推播回覆內容管理者可以刪除使用者的推播M

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -1,7 +1,12 @@
 class Admin::TweetsController < Admin::BaseController
   def index
+    @tweets = Tweet.all
   end
 
   def destroy
+    @tweet.destroy
+    redirect_to admin_tweets_path
+    flash[:alert] = "tweett was deleted"
   end
+
 end
