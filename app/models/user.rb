76973d61d4ebb276aebbc05a4237a014d9bd634a
Max <crono0720@gmail.com>
2018-02-05 03:44:36 +0800

    fix followership relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
   has_many :likes
   has_many :liked_tweets, through: :likes, source: :tweet
   has_many :followships
-  has_many :following, through: :followships
+  has_many :followings, through: :followships
   has_many :inverse_followships, class_name: "Followship", foreign_key: "following_id"
   has_many :followers, through: :inverse_followships, source: :user
 
