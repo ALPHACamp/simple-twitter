665f7944120e38fbb638ccc728b6373540866666
eric07109 <eric07109@gmail.com>
2018-02-05 07:49:50 +0800

    modified tweet index view and tweet create action

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,13 +1,17 @@
 class TweetsController < ApplicationController
   before_action :authenticate_user!
   
+
   def index
+    @tweet = Tweet.new
     @users # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
   end
 
   def create
-    @user = User.find(params[:id])
-    @tweet = @user.tweets.create(params[:tweet])
+    @user = current_user
+    @tweet = @user.tweets.new(tweet_params)
+    @tweet.save!
+    redirect_to tweets_path
   end
 
   def like
@@ -20,5 +24,4 @@ class TweetsController < ApplicationController
   def tweet_params
     params.require(:tweet).permit(:description)
   end
-
 end
