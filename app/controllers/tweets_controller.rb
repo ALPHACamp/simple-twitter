da7df66c795d379d95b8a3a8dcdf64ec5eb71b83
annie chiang <annie100patty@gmail.com>
2018-02-04 15:41:22 +0800

    add new tweet form and edit tweet-item/user-item in public tweet index

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,6 +2,7 @@ class TweetsController < ApplicationController
 
   def index
     @tweets = Tweet.all;
+    @tweet = Tweet.new;
 
     # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
     @users  = User.all.limit(10);
