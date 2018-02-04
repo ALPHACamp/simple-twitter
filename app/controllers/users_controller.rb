9bbf584444a8e70462eff5e87d8244bbfb7d662c
spy <sp831031@gmail.com>
2018-02-04 18:16:44 +0800

    add user#edit page and edit action in user#controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
-
+  before_action :set_user, only: [:tweets,:followings,:followers,:edit]
   def tweets
     @user = User.find(params[:id])
     @tweets = @user.tweets
   end
 
   def edit
+    
   end
 
   def update
@@ -25,4 +26,8 @@ class UsersController < ApplicationController
     @likes # 基於測試規格，必須講定變數名稱
   end
 
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
