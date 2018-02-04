6ba04fc1a6e9449334942562c277da5fc45953ee
caseydyps <caseydyps@gmail.com>
2018-02-04 16:42:45 +0800

    update tweet model

--- db/migrate/20180127071104_create_tweets.rb
+++ db/migrate/20180127071104_create_tweets.rb
@@ -2,7 +2,7 @@ class CreateTweets < ActiveRecord::Migration[5.1]
   def change
     create_table :tweets do |t|
       t.text :description
-      t.integer :user_id, :repiles_count, :likes_count
+      t.integer :user_id
 
       t.timestamps
     end
