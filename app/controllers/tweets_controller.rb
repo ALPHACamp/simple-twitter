1b9fc19f613e707119f7d0736f8f06f6235c03ed
KC <weilihmen@gmail.com>
2018-02-04 08:52:52 +0800

    tweets controller first setup

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,16 +1,39 @@
 class TweetsController < ApplicationController
 
   def index
-    @users # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
+    @users = User.all.order("followers_count DESC").limit(10)
+    # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
+    @tweet=Tweet.new
   end
 
   def create
+    @tweet = Tweet.new(tweet_params) #實例變數必須和new相同，這樣才能回傳error訊息回去render
+    if @tweet.save
+      flash[:notice] = "您的推特已成功推送"
+      redirect_back(fallback_location: root_path)
+    else
+      flash.now[:alert] = "失敗"
+      render :index
+    end
   end
 
   def like
+    @tweet = Tweet.find(params[:id])
+    @tweet.likes.create!(user: current_user)
+    redirect_back(fallback_location: root_path)  # 導回上一頁
   end
 
   def unlike
+    @tweet = Tweet.find(params[:id])
+    like = Like.where(tweet: @tweet, user: current_user)
+    like.destroy_all
+    redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:description, :user_id)
   end
 
 end
