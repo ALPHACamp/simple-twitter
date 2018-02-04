9f893ef6af71267bd1fc84b5d86373d5d3e306e4
Max <crono0720@gmail.com>
2018-02-05 03:36:12 +0800

    add followship routes

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,10 @@ Rails.application.routes.draw do
     member do
      get :tweets
      get :likes
+     get :followings
+     get :followers
    end
   end
+
+  resources :followships, only: [:create, :destroy]
 end
