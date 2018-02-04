03f6c5847f1785ec1d6b60e8bf53c1653770a8ec
Tony Lin <tonylin1208@gmail.com>
2018-02-04 10:12:43 +0800

    relation between tweet & reply

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,5 @@
 class Admin::UsersController < Admin::BaseController
   def index
-    
+    @users = User.all
   end
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,6 +4,7 @@ class TweetsController < ApplicationController
     @users # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
   end
 
+  
   def create
   end
 
