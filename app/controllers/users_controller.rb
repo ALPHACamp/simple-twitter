7736b86f45873789a206d39e25ed64b698d84581
Chad <xmen11034@gmail.com>
2018-02-04 17:30:10 +0800

    add user page for current user to edit profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,23 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:tweets, :update, :edit]
 
   def tweets
-    @user = User.find(params[:id])
   end
 
   def edit
+    unless @user == current_user
+      redirect_to user_path(@user)
+    end
   end
 
   def update
+    if @user.update(user_params)
+      flash[:notice] = "user was successfully updated"
+      redirect_to tweets_user_path
+    else
+      flash[:alert] = @user.errors.full_messages.to_sentence
+      redirect_to edit_user_path(@user)
+    end
   end
 
   def followings
@@ -20,6 +30,16 @@ class UsersController < ApplicationController
 
   def likes
     @likes # 基於測試規格，必須講定變數名稱
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :avatar)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
