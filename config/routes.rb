441097c3469c33d54630066f56edf3853966598d
wusunghan   <diadora1166@gmail.com>
2018-02-04 18:39:09 +0800

    新增推文的route只有 index和create

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
-
+  resources :tweets, only: [:index, :create]
   root"tweets#index"
+
   namespace :admin do
     resources :tweets, only: [:index, :destroy]
     resources :users, only: [:index]
