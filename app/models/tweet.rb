552a5ef5ba0b105272201ccd7ad2733b2588a298
spreered <spreered@gmail.com>
2018-02-04 19:59:44 +0800

    set tweet validate length message

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,8 @@
 class Tweet < ApplicationRecord
-  validates_length_of :description, maximum: 140
+  validates_length_of :description, maximum: 140,:message => "超過140個字"
   belongs_to :user
   #tweet只能有一個作者
 end
+
+
+        
