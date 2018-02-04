5c7b5d894a7f56b80b3c6ce70579bdde69ff4b24
binyaha <yahabin@gmail.com>
2018-02-04 18:37:31 +0800

    前端改成真正得圖片

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,4 +1,5 @@
 class Reply < ApplicationRecord
+  validates_presence_of :comment
   belongs_to :user, optional: true
   belongs_to :tweet, optional: true
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,6 @@
 class Tweet < ApplicationRecord
   validates_length_of :description, maximum: 140
+  validates_presence_of :description
   belongs_to :user, optional: true
   has_many :replies, dependent: :destroy
 end
