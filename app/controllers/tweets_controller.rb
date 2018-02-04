e8198babaa95e289b3e619da120c4db973a651fb
iceland101113 <r02521115@ntu.edu.tw>
2018-02-04 16:19:34 +0800

    setup tweet index action and view, show all tweet, des by create time

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,7 @@
 class TweetsController < ApplicationController
 
   def index
+    @tweets = Tweet.order(created_at: :desc).page(params[:page]).per(9)
     @users # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
   end
 
