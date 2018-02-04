5736d302e2d4e4b4f2b032f5a89f4e95a69efc03
spy <sp831031@gmail.com>
2018-02-04 20:06:37 +0800

    edit follow page style,and add profile update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,22 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:tweets,:followings,:followers,:edit]
+  before_action :set_user, only: [:tweets,:followings,:followers,:edit,:update]
   def tweets
     @user = User.find(params[:id])
     @tweets = @user.tweets
   end
 
   def edit
-    
+    session[:return_to] ||= request.referer
   end
 
   def update
+    if @user.update(user_params)
+      redirect_to session.delete(:return_to)
+      flash[:notice] = "User profile was successfully updated"
+    else
+      render :edit
+      flash[:alert] = "User profile was failed to update"
+    end
   end
 
   def followings
@@ -27,6 +34,12 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :avatar)
+  end
+
+
   def set_user
     @user = User.find(params[:id])
   end
