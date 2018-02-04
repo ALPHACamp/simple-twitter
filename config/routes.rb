348614e21558771e15a5221491e4ffc37f860f08
anna770822 <anna770822@hotmail.com>
2018-02-04 13:57:11 +0800

    reset users#tweets route using member

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
-  resources :users, only: [:edit]
-  get 'users/:id' => 'users#tweets',:as => "users/tweets"
+  resources :users, only: [:edit] do
+    member do 
+      get :tweets
+    end
+  end
+
   # 請依照專案指定規格來設定路由
   resources :tweets, only: [:index, :create] 
 
@@ -11,11 +15,6 @@ Rails.application.routes.draw do
     resources :replies, only:[:index, :create]
   end
 
-
-  
-  
-  
-  
   
 
   root "tweets#index"
