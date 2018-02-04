af4c80b715bf96172a989a9825dc034012dd71dc
Max <crono0720@gmail.com>
2018-02-05 03:36:49 +0800

    add create followship action

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -1,5 +1,9 @@
 class FollowshipsController < ApplicationController
   def create
+    @following = User.find(params[:following_id])
+    @followship = Followship.new(following: @following, user: current_user)
+    @followship.save
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
