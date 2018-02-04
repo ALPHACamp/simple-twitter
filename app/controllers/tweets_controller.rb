137c19b41870ba67f99f9e13c177672b6950422b
wusunghan   <diadora1166@gmail.com>
2018-02-04 18:38:06 +0800

    讓user可以新增推文(tweet)

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,9 +2,17 @@ class TweetsController < ApplicationController
 
   def index
     @users # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
+    @tweet = Tweet.new #寫一篇tweet
   end
-
   def create
+    @tweet = Tweet.new(tweet_params)
+    user = current_user
+    if @tweet.save
+      flash[:notice] = '新增成功'
+      redirect_to tweets_path
+    else
+      render action: :index
+    end
   end
 
   def like
@@ -13,4 +21,9 @@ class TweetsController < ApplicationController
   def unlike
   end
 
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:description)
+  end
 end
