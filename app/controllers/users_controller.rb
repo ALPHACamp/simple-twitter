44c4ec9a8533ac9b1784aab3250b69d45c25089c
Riemann <dr.mactt@gmail.com>
2018-02-04 17:36:07 +0800

    tweets order setting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
   def tweets
     @user = User.find(params[:id])
+    @tweets = Tweet.where(user_id: @user.id).order(created_at: :desc) # 回傳 array
   end
 
   def edit
