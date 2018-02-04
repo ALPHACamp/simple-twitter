96c5f5d947ab6f2d159d279ad4305f494dfe0e60
Wells <chawkeye@gmail.com>
2018-02-04 14:17:17 +0800

    fixed sorting for following/follower/likes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,15 +15,14 @@ class UsersController < ApplicationController
   end
 
   def followings
-    @followings = @user.followings # 基於測試規格，必須講定變數名稱
+    @followings = @user.followings.includes(:followships).order("followships.updated_at desc") # 基於測試規格，必須講定變數名稱
   end
 
   def followers
-    @followers = @user.followers # 基於測試規格，必須講定變數名稱
+    @followers = @user.followers.includes(:followships).order("followships.updated_at desc") # 基於測試規格，必須講定變數名稱
   end
-
   def likes
-    @likes = @user.like_tweets.all.order(created_at: :desc) # 基於測試規格，必須講定變數名稱
+    @likes = @user.like_tweets.includes(:likes).order("likes.created_at desc") # 基於測試規格，必須講定變數名稱
   end
 
   private
