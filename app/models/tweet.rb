9c72eeebf2964d4c507e53e0a0ca21bc951d50f3
spreered <spreered@gmail.com>
2018-02-05 01:01:39 +0800

    set reply relationship with user and tweet

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,6 +2,9 @@ class Tweet < ApplicationRecord
   validates_length_of :description, maximum: 140,:message => "超過140個字"
   belongs_to :user
   #tweet只能有一個作者
+
+  has_many :replies, dependent: :destroy
+  #刪掉tweet 就會刪掉reply
 end
 
 
