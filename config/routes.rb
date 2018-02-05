913f4eb15a3f95718db838dea93e7fff0293569d
jiananWang <a0918666589@gmail.com>
2018-02-05 13:23:40 +0800

    setup tweet index and create routes and member rplies and like routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,23 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   resources :users, only: [:edit, :update] do
-    
+
   end
 
   # 請依照專案指定規格來設定路由
+  resources :tweets, only: [:index, :create] do
 
+    member do
+      # reply page
+      get :replies
+      post :replies
+
+      # like button
+      post :like
+      post :unlike
+    end
+
+  end
   root "tweets#index"
 
   namespace :admin do
