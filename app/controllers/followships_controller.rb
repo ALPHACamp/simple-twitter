b8edc7ad69f9d4b75e7c40bd8380bbd5d0d30a80
spreered <spreered@gmail.com>
2018-02-05 16:14:35 +0800

    add followship@create followship#destroy action

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -1,7 +1,24 @@
 class FollowshipsController < ApplicationController
   def create
+    @user = current_user.followships.build(following_id: params[:following_id])
+    if @followships.save
+      flash[:notice] = "Successfully followed"
+      redirect_back(fallback_location: root_path)
+    else
+      flash[:alert] = @followship.errors.full_messages.to_sentence
+      redirect_back(fallback_location: root_path)
+    end
   end
 
   def destroy
+    @followship = current_user.followships.where(following_id:params[:id])
+    if @followship.length>0
+      @followship.destroy_all
+      flash[:alert] = "Followship destroy"
+    else
+      flash[:alert] = "no Followship!"
+    end
+
+    redirect_back(fallback_location: root_path)
   end
 end
