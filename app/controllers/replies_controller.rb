378f30790f6039ff060dfc0976d89418c8146f3d
Tony Lin <tonylin1208@gmail.com>
2018-02-05 14:13:31 +0800

    add reply controller contains

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,9 +1,17 @@
 class RepliesController < ApplicationController
 
   def index
+    @tweet = Tweet.find(params[:tweet_id])
+    @replies = @tweet.replies.all
+    @user = @tweet.user
   end
 
   def create
+    @tweet = Tweet.find(params[:tweet_id])
+    @reply = @tweet.replies.build(reply_params)
+    @reply.user = current_user
+    @reply.save!
+    redirect_to tweet_replies_path(@tweet)
   end
 
 end
