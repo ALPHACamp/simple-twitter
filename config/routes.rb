077367b68232008744986c7ea552099cf6bee580
Jerry Xu <csj5dl@hotmail.com>
2018-02-05 23:36:38 +0800

    setup routes, tweets#index, admin:tweets#index

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,12 @@ Rails.application.routes.draw do
   devise_for :users
 
   # 請依照專案指定規格來設定路由
+  # =======前台=======
+  root "tweets#index"
+
+  # =======後台=======
+  namespace :admin do
+    root "tweets#index"
+  end
 
 end
