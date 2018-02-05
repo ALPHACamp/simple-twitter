54af4853e2a09150a155798e1e2b25688e6e80f8
annie chiang <annie100patty@gmail.com>
2018-02-05 18:42:59 +0800

    add index/create actions of reply

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,9 +1,31 @@
 class RepliesController < ApplicationController
 
   def index
+    @tweet = Tweet.find(params[:id])
+    @reply = Reply.new
   end
 
   def create
+    @tweet = Tweet.find(params[:id])
+    @reply = current_user.replies.build(reply_params)
+    @reply.tweet = @tweet
+    if @reply.save
+      redirect_back(fallback_location: root_path)
+    else
+      if @reply.errors.present?
+        @reply.errors.each do |error, message|
+          flash[:alert] = "#{error.capitalize} #{message}"
+        end
+      else
+        flash[:alert] = "Failed to reply"
+      end
+      redirect_to replies_path
+    end
   end
 
+  private
+
+  def reply_params
+    params[:reply].permit(:comment)
+  end
 end
--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,5 +1,6 @@
 class Reply < ApplicationRecord
+  validates_presence_of :comment
+
   belongs_to :user
   belongs_to :tweet, counter_cache: true
-  
 end
