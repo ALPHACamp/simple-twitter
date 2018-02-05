15ffe51d538476ab33beaca7ea394aa928352b92
spreered <spreered@gmail.com>
2018-02-05 16:54:11 +0800

    add user following action and page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,8 @@ class UsersController < ApplicationController
   end
 
   def followings
-    @followings # 基於測試規格，必須講定變數名稱
+    @user = User.find(params[:id])
+    @followings = @user.followings
   end
 
   def followers
