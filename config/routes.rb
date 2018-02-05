02e3d519ef3e81a48607353f2779658f3640bd12
Unknown <thetasteofleavesisgreat@gmail.com>
2018-02-06 06:56:05 +0800

    setup admin routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
 
-  # 請依照專案指定規格來設定路由
+
+
+
+  namespace :admin do
+    resources :tweets, only: [:index, :destroy]
+    resources :users, only: [:index]
+    root "tweets#index"
+  end
 
 end
