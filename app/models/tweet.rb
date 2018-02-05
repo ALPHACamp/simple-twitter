c7434f0c41a3f4c827dc31fb24acb88e7e582f1e
chunchaolee <chunchaolee@gmail.com>
2018-02-06 01:10:48 +0800

    setup relation between user and tweet, and modified tweets/index.html.erb for this change

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,7 @@
 class Tweet < ApplicationRecord
   validates_length_of :description, maximum: 140, :message => "超過字數限制"
 
+  belongs_to :user
 
   # find_user method 用於tweets頁面 取得@user資料，avatar
   def find_user
