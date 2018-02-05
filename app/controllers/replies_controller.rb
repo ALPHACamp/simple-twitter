8ffd88fda5fc174bbe357ff6f40c0b5081b99e67
victorloo <ila9970@gmail.com>
2018-02-06 06:31:43 +0800

    user can add reply on reply page

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -2,10 +2,22 @@ class RepliesController < ApplicationController
 
   #replies是點擊某則tweets後出現的頁面
   def index
+    @tweet = Tweet.find(params[:id])
   end
 
   #編輯完回覆，replays
   def create
+    @tweet = Tweet.find(params[:tweet_id])
+    @reply = @reply.comments.build(reply_params)
+    @reply.user = current_user
+    @reply.save!
+    redirect_to reply_path(@reply)
+  end
+
+  private
+
+  def reply_params
+    params.require(:reply).permit(:comment)
   end
 
 end
