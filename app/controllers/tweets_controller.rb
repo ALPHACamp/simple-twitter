e3f9b2438bfa49e100c175e46d3b1a3d10d11c7a
jiananWang <a0918666589@gmail.com>
2018-02-05 14:33:20 +0800

    create tweet function in index page and user the bootstrap

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,9 +2,18 @@ class TweetsController < ApplicationController
 
   def index
     @users # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
+    @tweet = Tweet.new
   end
 
   def create
+    @tweet = current_user.tweets.build(tweet_params)
+    if @tweet.save
+      flash[:notice] = "Tweet successfully!!!"
+      redirect_back fallback_location: root_path
+    else
+      flash[:alert] = "Your tweet is more than 140 characters!!!Please simplify your tweet."
+      render :index
+    end
   end
 
   def like
@@ -13,4 +22,9 @@ class TweetsController < ApplicationController
   def unlike
   end
 
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:description)
+  end
 end
