e0c3cef5a606afb75ffab0eef2073d5cba38c998
Riemann <dr.mactt@gmail.com>
2018-02-06 03:05:03 +0800

    add increment setting of user.followers_count in followships controller

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -2,9 +2,9 @@ class FollowshipsController < ApplicationController
 
   def create
     @followship = current_user.followships.build(following_id: params[:following_id])
-
+    @user = User.find(params[:following_id])
     if @followship.save
-       # following_id 的  user.followers_count += 1
+       @user.increment!(:followers_count)
       flash[:notice] = "Successfully followed"
       redirect_back(fallback_location: "users/tweets") # 不同 controller 的 template 的轉向方法。Action Controller-控制 HTTP 流程
     else
@@ -15,7 +15,9 @@ class FollowshipsController < ApplicationController
 
   def destroy
     @followship = current_user.followships.where(following_id: params[:id]).first
+    @user = User.find(params[:following_id])
     @followship.destroy
+    @user.decrement!(:followers_count)
     flash[:alert] = "Followship destroyed"
     redirect_back(fallback_location: "users/tweets")
   end
