afbf0b953ad3264927b04dd7aa58538d8326e9f6
caseydyps <caseydyps@gmail.com>
2018-02-05 20:32:30 +0800

    association of followship like reply tweet and user

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,8 @@
 class Tweet < ApplicationRecord
   validates_length_of :description, maximum: 140
-
+  validates_presence_of :description
+  belongs_to :user, counter_cache: true
+  has_many :replies, dependent: :delete_all
+  has_many :likes, dependent: :delete_all
+  has_many :like_users, through: :likes, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,15 @@ class User < ApplicationRecord
   validates_presence_of :name
   # 加上驗證 name 不能重覆 (關鍵字提示: uniqueness)
   validates_uniqueness_of :name
+  has_many :tweets, dependent: :delete_all
+  has_many :replies, dependent: :delete_all
+  has_many :likes, dependent: :delete_all
+  has_many :like_tweets, through: :likes, source: :tweet
+  has_many :followships, dependent: :destroy
+  has_many :followings, through: :followships
+  has_many :inverse_followships, class_name: "Followship", foreign_key: "following_id"
+  has_many :followers, through: :inverse_followships, source: :user
+
   def admin?
     self.role == "admin"
   end
