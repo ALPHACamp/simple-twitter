9e33f3503b84b9e8e6008a0110edd98c52c2b937
Chia-Hung Lin <qaz741012@gmail.com>
2018-02-05 14:07:16 +0800

    add validation in followship that user can't follow himself/herself

--- app/models/followship.rb
+++ app/models/followship.rb
@@ -1,7 +1,16 @@
 class Followship < ApplicationRecord
   validates :following_id, uniqueness: { scope: :user_id }
+  validate :follow_itself_validator
 
   belongs_to :user
   belongs_to :following, class_name: "User", counter_cache: :followers_count
+
+  private
+
+  def follow_itself_validator
+    if user_id == following_id
+      errors[:you] << "can't follow yourself!"
+    end
+  end
 
 end
