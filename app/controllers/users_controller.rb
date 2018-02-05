fa91a964e67c9fae4ef6c294f759e05e60b7fbbc
Tony Lin <tonylin1208@gmail.com>
2018-02-05 23:20:34 +0800

    add like view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,15 +20,22 @@ class UsersController < ApplicationController
   end
 
   def followings
-    @followings # 基於測試規格，必須講定變數名稱
+    # 基於測試規格，必須講定變數名稱
+    @user = User.find(params[:id])
+    @followings 
   end
 
   def followers
-    @followers # 基於測試規格，必須講定變數名稱
+    # 基於測試規格，必須講定變數名稱
+    @user = User.find(params[:id])
+    @followers 
   end
 
   def likes
-    @likes # 基於測試規格，必須講定變數名稱
+    # 基於測試規格，必須講定變數名稱
+    @user = User.find(params[:id])
+    @tweets = @user.liked_tweets
+    @likes = @user.likes.all
   end
 
   private
