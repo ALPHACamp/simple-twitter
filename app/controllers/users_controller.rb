5cdcd22ee34282540f3275960d81205ac6c55bd5
sather33 <court.dream3@gmail.com>
2018-02-05 15:50:04 +0800

    user can edit user's profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,18 @@
 class UsersController < ApplicationController
+  before_action :fetch_user, only: [:show, :update]
 
   def tweets
   end
 
-  def edit
-  end
-
   def update
-  end
+		if @user.update(user_params)
+      redirect_to user_path(@user)
+      flash[:notice] = "User was successfully updated"
+    else
+      render :edit
+      flash[:alert] = "User was failed to update"
+    end
+	end
 
   def followings
     @followings # 基於測試規格，必須講定變數名稱
@@ -21,4 +26,13 @@ class UsersController < ApplicationController
     @likes # 基於測試規格，必須講定變數名稱
   end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :avatar)
+  end
+
+  def fetch_user
+		@user = current_user
+	end
 end
