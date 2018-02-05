0efdd04c1e8c670734f36b3a4b0eb60e472f6c73
penciler <eraser41@livemail.tw>
2018-02-06 06:59:21 +0800

    add routes for admin

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,11 @@ Rails.application.routes.draw do
   # 請依照專案指定規格來設定路由
   root "tweets#index"
 
-  
+
 
   namespace :admin do
+    resources :tweets, only:[:destroy]
+    resources :users, only:[:index]
     root "tweets#index"
   end
 
