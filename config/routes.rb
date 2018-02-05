b92d75d52f2d8d84bcaf21e3689bb3a88dcd7449
eric07109 <eric07109@gmail.com>
2018-02-05 21:34:55 +0800

    coufigured nested routes for replies based on tweets

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
 
   # 請依照專案指定規格來設定路由
   root "tweets#index"
-  resources :tweets, only: [:index, :create]
+  resources :tweets, only: [:index, :create] do
+    resources :replies, only: [:index, :create]
+  end
+
+
   resources :users, only: [:edit, :update]
   
   # Admin
