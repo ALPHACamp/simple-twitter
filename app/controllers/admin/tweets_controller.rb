8b09b9cd7767c00a4d55cba8082c1385c2b0fff1
caseydyps <caseydyps@gmail.com>
2018-02-05 19:54:21 +0800

    add restaurant destroy action for admin and rework admin index view

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -1,9 +1,14 @@
 class Admin::TweetsController < Admin::BaseController
 
+
+
   def index
     @tweets = Tweet.all
   end
 
   def destroy
+    @tweet.destroy
+    redirect_to admin_tweets_path
+    flash[:alert] = "Tweet was deleted"
   end
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,6 +6,7 @@ class TweetsController < ApplicationController
   end
 
   def create
+
   end
 
   def like
