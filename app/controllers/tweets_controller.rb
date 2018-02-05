aeba43340ad2f81529c4eceb9b533e71c8bbc60f
Kansetsu Kuo <kansetsu4@gmail.com>
2018-02-05 18:20:36 +0800

    Done: user can like and unlike others tweet, but not themself's

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,5 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet ,only: [:like, :unlike]
 
   def index
     # @users # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
@@ -22,15 +23,24 @@ class TweetsController < ApplicationController
   end
 
   def like
+    @tweet.likes.create!(user: current_user)
+    redirect_back(fallback_location: root_path)
   end
 
   def unlike
+    like = Like.where(user: current_user, tweet: @tweet)
+    like.destroy_all
+    redirect_back(fallback_location: root_path)
   end
 
   private
 
   def tweet_params
     params.require(:tweet).permit(:description)
+  end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
   end
 
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -4,4 +4,8 @@ class Tweet < ApplicationRecord
   has_many :replies, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
+
+  def is_liked(user)
+    self.liked_users.include?(user)
+  end
 end
