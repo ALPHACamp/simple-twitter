f08cf3796f8c560bde53ae15c6c9a96651aeef71
badbug wne <sweewen45@gmail.com>
2018-02-05 15:13:12 +0800

    add admin and normal test users. Fix root error for admin pannel in application view. add uniquencess in users name validation. set up user route and show/edit views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
+  resources :users, only: [:show, :edit, :update] do
+  end  
 
   # 請依照專案指定規格來設定路由
 
