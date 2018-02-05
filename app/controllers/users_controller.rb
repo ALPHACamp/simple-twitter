b48a7c5702b810555d89c6dcf40cb28359e98f36
Kansetsu Kuo <kansetsu4@gmail.com>
2018-02-05 10:11:32 +0800

    Done: show user tweets on user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
+  before_action :set_user ,only: [:tweets]
 
   def tweets
+    @tweets = @user.tweets.order('created_at desc').page(params[:page]).per(10)
   end
 
   def edit
@@ -19,6 +21,12 @@ class UsersController < ApplicationController
 
   def likes
     @likes # 基於測試規格，必須講定變數名稱
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
