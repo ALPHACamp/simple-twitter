22a68fb252e270fd77227899a3f5b38eae7ea34e
sather33 <court.dream3@gmail.com>
2018-02-05 17:12:23 +0800

    user can create tweet on index

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,13 +2,20 @@ class TweetsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @users = User.all
+    @users = User.includes(:tweets)
     # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
+    @tweets = Tweet.all
+    @tweet = Tweet.new
   end
 
   def show
   end
+
   def create
+    @tweet = Tweet.new(tweet_params)
+    @tweet.user = current_user
+    @tweet.save
+    redirect_to tweets_path
   end
 
   def like
@@ -17,4 +24,9 @@ class TweetsController < ApplicationController
   def unlike
   end
 
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:description)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :tweets, dependent: :destroy
   validates :name, uniqueness: true, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
