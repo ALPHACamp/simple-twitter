40c1ab4efe1118dfa8e9dbfe03048abd43578cf8
Yipeechen <yipeechen0418@gmail.com>
2018-02-05 23:00:29 +0800

    add routes of user & tweets

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,23 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
 
+  resources :tweets, only: [:index, :create] do
+    member do
+      post :like
+      post :unlike
+    end
+  end
   root 'tweets#index'
   # 請依照專案指定規格來設定路由
+  resources :user, only: [:edit] do
+
+    member do
+      get :tweets
+    end
+  end
 
   namespace :admin do
+    resources :tweets, only: [:index, :destroy]
     root "tweets#index"
   end
 end
