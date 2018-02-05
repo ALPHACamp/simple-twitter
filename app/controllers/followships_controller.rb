8b74a31c2dbf128ab5b895e6c42aeab865b96d6d
spreered <spreered@gmail.com>
2018-02-05 16:22:26 +0800

    fix bug : followships controller typo

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -1,7 +1,7 @@
 class FollowshipsController < ApplicationController
   def create
-    @user = current_user.followships.build(following_id: params[:following_id])
-    if @followships.save
+    @followship = current_user.followships.build(following_id: params[:following_id])
+    if @followship.save
       flash[:notice] = "Successfully followed"
       redirect_back(fallback_location: root_path)
     else
