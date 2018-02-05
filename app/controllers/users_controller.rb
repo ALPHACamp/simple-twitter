1a98c79fd48401180c5a974762ac006ced98916a
Tony Lin <tonylin1208@gmail.com>
2018-02-05 14:19:38 +0800

    remove note and comment out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,12 +6,10 @@ class UsersController < ApplicationController
   end
 
   def edit
-    puts "You enter the edit controller."
     @user = User.find(params[:id])
   end
 
   def update
-    puts "You enter the update controller."
     if current_user.update(user_params)
       redirect_to tweets_user_path(current_user)
       flash[:notice] = "#{current_user.name} was successfully updated"
