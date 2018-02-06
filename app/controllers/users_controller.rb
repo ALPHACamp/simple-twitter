0fefc949e06bb9c7fb18b49cf3e63c9c60ed8e1d
mordochi <lilynnchi@gmail.com>
2018-02-06 18:18:17 +0800

    user can see his/hers following people on users#followings

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,10 +23,12 @@ class UsersController < ApplicationController
   end
 
   def followings
-    @followings # 基於測試規格，必須講定變數名稱
+    @user = User.find(params[:id])
+    @followings = @user.followings # 基於測試規格，必須講定變數名稱
   end
 
   def followers
+    @user = User.find(params[:id])
     @followers # 基於測試規格，必須講定變數名稱
   end
 
