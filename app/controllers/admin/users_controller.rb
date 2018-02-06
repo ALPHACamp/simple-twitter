c9278b17294131b5926b59eaf17e568cb6b0007e
Adamlin0708 <kuso12610@gmail.com>
2018-02-06 22:32:07 +0800

    create admin tweets#index, tweets#destroy, users#index action

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,6 @@
 class Admin::UsersController < Admin::BaseController
   def index
+    @users = User.all.page(params[:page]).per(10).order(created_at: :desc)            
+    @count = User.all.count
   end
 end
