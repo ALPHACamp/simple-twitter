685795c8640878a3cc923b6a8cabbd98c1987dc0
Duncan <loxa10077@gmail.com>
2018-02-06 11:59:02 +0800

    user can update profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,25 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only: [:show, :edit, :update]
+
   def tweets
   end
 
   def show
-    @user = User.find(params[:id])
     @tweets = Tweet.all.order(created_at: :desc).limit(10)
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
+    if @user.update(user_params)
+       flash[:notice] = "Updated!"
+       redirect_to user_path(@user)
+     else
+      flash[:notice] = "Name can't be blank!"
+      render :edit
+    end
   end
 
   def followings
@@ -25,6 +32,16 @@ class UsersController < ApplicationController
 
   def likes
     @likes # 基於測試規格，必須講定變數名稱
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :avatar)
   end
 
 end
