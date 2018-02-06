15d5867162defecd7746c9f9194d98b00859f58c
chunchaolee <chunchaolee@gmail.com>
2018-02-06 12:30:26 +0800

    setup relation between tweet and reply on models

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,2 +1,6 @@
 class Reply < ApplicationRecord
+
+  # 設定關聯
+  belongs_to :tweet
+
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,12 +1,8 @@
 class Tweet < ApplicationRecord
   validates_length_of :description, maximum: 140, :message => "超過字數限制"
 
+  # 設定關聯
   belongs_to :user
-
-  # find_user method 用於tweets頁面 取得@user資料，avatar
-  def find_user
-    user_id = self.user_id
-    @user = User.find(user_id)
-  end
+  has_many :replies
 
 end
