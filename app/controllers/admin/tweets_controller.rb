7e4a437089bcc3b8994afeec74661883c8f6c95e
francishuang <francis3925@gmail.com>
2018-02-06 21:35:14 +0800

    enable admin user to delete tweet on admin tweets index page

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -4,5 +4,9 @@ class Admin::TweetsController < Admin::BaseController
   end
 
   def destroy
+    @tweet = Tweet.find(params[:id])
+    @tweet.destroy
+    flash.alert = "推播已刪除！"
+    redirect_to admin_tweets_path
   end
 end
