170ded16f909d9ec8a4bd79bdcf4a11ffc495be4
Yipeechen <yipeechen0418@gmail.com>
2018-02-06 12:27:23 +0800

    link user & tweet model and fix the routes bug

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   end
   root 'tweets#index'
   # 請依照專案指定規格來設定路由
-  resources :user, only: [:edit] do
+  resources :users, only: [:edit] do
 
     member do
       get :tweets
