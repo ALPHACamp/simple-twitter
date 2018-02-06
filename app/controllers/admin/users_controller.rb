706c3dad4807d2c2c5d3e3ff5d878951546c7cbb
yvesen <l.sen.t@hotmail.com>
2018-02-07 00:29:05 +0800

    admin profile broken

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,4 +2,6 @@ class Admin::UsersController < Admin::BaseController
   before_action :authenticate_admin
   def index
   end
+
+  
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,11 +1,15 @@
 class TweetsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_user,only:[:show, :edit, :update]
+  before_action :set_tweet,only:[:show, :edit, :update]
   # before_action :authenticate_admin
   def index
     @tweets = Tweet.all
     # @users 
     # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
+  end
+
+  def show
+    @tweet = Tweet.find(params[:id])
   end
 
 
@@ -21,6 +25,16 @@ class TweetsController < ApplicationController
   end
 
   def unlike
+  end
+
+  private
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])  
+  end
+
+  def tweet_params
+    params.require(:tweet).permit(:name, :description)
   end
 
 
