a9fcc12e141869106609bb4c722342f076cdf6fc
badbug wne <sweewen45@gmail.com>
2018-02-07 00:47:36 +0800

    add kaminari pagination in admin/users. add admin/tweets/users shift link in both pages. add user list table

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,8 @@
 class Admin::UsersController < Admin::BaseController
   def index
     # 管理者可以瀏覽站內所有的使用者清單，該清單按推播文數排序
-    @twiters = User.joins(:tweets).group("user_id").order("count(user_id) DESC")
+    @users = User.joins(:tweets).group("user_id").order("count(user_id) DESC")
+    @twitters = @users.page(params[:page]).per(10)
+
   end
 end
