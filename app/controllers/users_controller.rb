6d06a09a945117ddc892ea25e74d9e341e203806
annie chiang <annie100patty@gmail.com>
2018-02-06 23:49:48 +0800

    add likes_user page and user#likes action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,8 @@ class UsersController < ApplicationController
 
   def likes
     @user = User.find(params[:id])
-    @likes # 基於測試規格，必須講定變數名稱
+    @likes = @user.liked_tweets.all.order("likes.created_at DESC") # 基於測試規格，必須講定變數名稱
+    # a = @likes.first.likes.first.created_at
   end
 
 end
