b33914a58afdc3e606c408dbcc241608636fcd70
annie chiang <annie100patty@gmail.com>
2018-02-06 20:09:01 +0800

    add public user pages and links

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   def tweets
+    @user = User.find(params[:id])
   end
 
   def edit
@@ -10,14 +11,17 @@ class UsersController < ApplicationController
   end
 
   def followings
+    @user = User.find(params[:id])
     @followings # 基於測試規格，必須講定變數名稱
   end
 
   def followers
+    @user = User.find(params[:id])
     @followers # 基於測試規格，必須講定變數名稱
   end
 
   def likes
+    @user = User.find(params[:id])
     @likes # 基於測試規格，必須講定變數名稱
   end
 
