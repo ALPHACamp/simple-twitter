161f834e89fa9c4eba21a9d5786c4af6db697e5f
Wells <chawkeye@gmail.com>
2018-02-06 15:31:19 +0800

    ajax for admin::tweet#destroy

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -1,11 +1,11 @@
 class Admin::TweetsController < Admin::BaseController
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.all.includes(:replies, :user).order(replies_count: :desc)
   end
 
   def destroy
     @tweet = Tweet.find(params[:id])
     @tweet.destroy
-    redirect_to admin_root_path
+    # redirect_to admin_root_path
   end
 end
