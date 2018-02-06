b5b4bb4def386786449e0ebbab293dbb3c6cc1a5
mordochi <lilynnchi@gmail.com>
2018-02-06 23:26:17 +0800

    show data in the descendant order of created time

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
   def tweets
     @user = User.find(params[:id])
+    @tweets = @user.tweets.order(Tweet.arel_table['created_at'].desc)
   end
 
   def edit
@@ -24,17 +25,17 @@ class UsersController < ApplicationController
 
   def followings
     @user = User.find(params[:id])
-    @followings = @user.followings # 基於測試規格，必須講定變數名稱
+    @followings = @user.followings.order(Followship.arel_table['created_at'].desc) # 基於測試規格，必須講定變數名稱
   end
 
   def followers
     @user = User.find(params[:id])
-    @followers = @user.followers # 基於測試規格，必須講定變數名稱
+    @followers = @user.followers.order(Followship.arel_table['created_at'].desc) # 基於測試規格，必須講定變數名稱
   end
 
   def likes
     @user = User.find(params[:id])
-    @likes = @user.liked_tweets # 基於測試規格，必須講定變數名稱
+    @likes = @user.liked_tweets.order(Like.arel_table['created_at'].desc) # 基於測試規格，必須講定變數名稱
   end
 
   private
