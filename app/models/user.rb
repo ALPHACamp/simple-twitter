979f5f0297c49c04cae7d49fbdc28c3b4103806c
penciler <eraser41@livemail.tw>
2018-02-07 06:49:57 +0800

    set up db relation between user, followship and reply

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,14 @@ class User < ApplicationRecord
 
   has_many :likes
 
+  has_many :followships
+  has_many :followings, through: :followships
+
+  has_many :inverse_followships, class_name: "Followship", foreign_key: "following_id"
+  has_many :followers, through: :inverse_followships, source: :user
+
+  has_many :reply
+
 # add admin? method
   def admin?
     self.role == "admin"
