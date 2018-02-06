d169da333a9c96d466ec73c0106edf18e0721464
beranemo <beranemo@gmail.com>
2018-02-06 22:53:52 +0800

    tweet 的首頁初製

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,8 @@
 class TweetsController < ApplicationController
 
   def index
-    @users # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
+    #@users # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
+    @tweets = Tweet.all
   end
 
   def create
