bae63a18a352ebc8513373c916912f085c27f630
Max <crono0720@gmail.com>
2018-02-06 09:05:44 +0800

    add user follower and following page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,11 +20,11 @@ class UsersController < ApplicationController
   end
 
   def followings
-    @followings # 基於測試規格，必須講定變數名稱
+    @followings = @user.followings # 基於測試規格，必須講定變數名稱
   end
 
   def followers
-    @followers # 基於測試規格，必須講定變數名稱
+    @followers = @user.followers # 基於測試規格，必須講定變數名稱
   end
 
   def likes
