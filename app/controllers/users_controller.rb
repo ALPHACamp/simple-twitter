e83bafb8b0eaa3b1ceda2b8b2e9f1adedcc8124f
anna770822 <anna770822@hotmail.com>
2018-02-06 20:00:26 +0800

    add _follow to shared folder and

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
 
   def update
     @user = current_user
-    @user.update(user_params)
+    @user.update!(user_params)
     redirect_to tweets_user_path
   end
 
