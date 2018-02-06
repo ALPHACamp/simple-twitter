5262c965146ce4ed3b7adb06dd6b198f01e84706
caseydyps <caseydyps@gmail.com>
2018-02-06 20:53:50 +0800

    show avator

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,8 @@
 class UsersController < ApplicationController
 
-  before_action :set_user, only: [:edit, :update, :likes]
+  before_action :set_user, only: [:tweets, :edit, :update, :likes, :followings, :followers]
 
   def tweets
-    @user = User.find(params[:id])
     @tweets = @user.tweets.all.order(created_at: :desc)
   end
 
@@ -20,11 +19,11 @@ class UsersController < ApplicationController
   end
 
   def followings
-    @followings # 基於測試規格，必須講定變數名稱
+    @followings = @user.followings# 基於測試規格，必須講定變數名稱
   end
 
   def followers
-    @followers # 基於測試規格，必須講定變數名稱
+    @followers = @user.followers# 基於測試規格，必須講定變數名稱
   end
 
   def likes
@@ -39,7 +38,7 @@ class UsersController < ApplicationController
   private
 
   def set_user
-    @user = User.find(params)
+    @user = User.find(params[:id])
   end
 
   def user_params
