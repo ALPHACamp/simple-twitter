c44839125a9e73834a1251e88b48cbd9c741fcda
chunchaolee <chunchaolee@gmail.com>
2018-02-06 22:23:10 +0800

    setup followships and user relation

--- app/models/followship.rb
+++ app/models/followship.rb
@@ -1,4 +1,9 @@
 class Followship < ApplicationRecord
   validates :following_id, uniqueness: { scope: :user_id }
 
+  # 一個followship屬於一個user
+  belongs_to :user
+  # 一個followsip屬於一個following關係
+  belongs_to :following, class_name: "User"
+
 end
