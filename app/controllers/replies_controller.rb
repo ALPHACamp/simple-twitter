2c18ddaf98d2b870968b876d6691be23706602d4
chunchaolee <chunchaolee@gmail.com>
2018-02-06 12:25:34 +0800

    setup replies controller

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,9 +1,27 @@
 class RepliesController < ApplicationController
 
   def index
+    @tweet = Tweet.find(params[:id])
+    @replies = Reply.where(tweet_id: params[:id])
+    @reply = Reply.new
   end
 
   def create
+    @reply = Reply.new(reply_params)
+    @reply.user_id = current_user.id
+    if @reply.save
+      flash[:notice] = "留言成功！"
+      redirect_back(fallback_location: root_path)
+    else
+      lash[:alert] = @reply.errors.full_messages.to_sentence
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  private
+
+  def reply_params
+    require(:reply).permit(:comment)
   end
 
 end
