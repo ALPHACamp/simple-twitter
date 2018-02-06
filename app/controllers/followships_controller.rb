b2d24edc819eb589f93bad5ea874eb1964d358f1
chunchaolee <chunchaolee@gmail.com>
2018-02-06 22:25:46 +0800

    added create and destroy method on followships controller

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -1,7 +1,20 @@
 class FollowshipsController < ApplicationController
+  
   def create
+    @followship = current_user.followships.build(following_id: params[:following_id])
+    if @followship.save
+      flash[:notice] = "已成功Follow該使用者"
+      redirect_back(fallback_location: root_path)
+    else
+      flash[:alert] = @followship.errors.full_messages.to_sentence
+      redirect_back(fallback_location: root_path)
+    end
   end
 
   def destroy
+    @followship = current_user.followships.where(following_id: params[:id]).first
+    @followship.destroy
+    flash[:notice] = "已成功Unfollow該使用者"
+    redirect_back(fallback_location: root_path)
   end
 end
