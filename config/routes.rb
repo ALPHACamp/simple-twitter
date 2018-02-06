54c9916f3bf12860573c02f5481fe8d62b26bc33
chunchaolee <chunchaolee@gmail.com>
2018-02-06 16:53:21 +0800

    setup user#tweets routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
 
-  # 設置tweets index作為網站首頁
+  # 設置tweets
   resources :tweets, only: [:index,:create] do 
 
     member do
@@ -11,11 +11,16 @@ Rails.application.routes.draw do
 
   end
 
+  # 設置user
+  resources :users, only: [:edit, :update] do
+    
+    member do
+      get :tweets
+    end
 
+  end
 
+  # 設置tweets index作為網站首頁
   root 'tweets#index'
-
-  # user頁面
-  resources :users, only: [:edit, :update]
 
 end
