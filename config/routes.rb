78db6af0e2d13688e72d1246bf29150e4609e707
penciler <eraser41@livemail.tw>
2018-02-07 07:15:28 +0800

    set up routes according to simple_twitter_ui.jpg

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,21 @@ Rails.application.routes.draw do
   # 請依照專案指定規格來設定路由
   root "tweets#index"
 
+  resources :user, only:[:edit] do
+    
+    member do
+      get :tweets
+      get :followings
+      get :followers
+      get :likes
+    end 
+  end
 
+  resources :tweet, only:[:create] do
+    member do
+      get :replies
+    end
+  end
 
   namespace :admin do
     resources :tweets, only:[:destroy]
