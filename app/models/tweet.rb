2345af83ad85580590abcedbd77478964d49ebd9
mordochi <lilynnchi@gmail.com>
2018-02-06 18:41:37 +0800

    set up relations among tweet, user and like

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -4,4 +4,6 @@ class Tweet < ApplicationRecord
   
   belongs_to :user
   has_many :replies, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :liked_users, through: :likes, source: :user
 end
