86681ca4c2c05987e6867795551ac1ad3f79dcba
Max <crono0720@gmail.com>
2018-02-07 01:17:15 +0800

    add count_cloumn to users table

---
+++ db/migrate/20180205181043_add_count_cloumns_to_users.rb
@@ -0,0 +1,7 @@
+class AddCountCloumnsToUsers < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :followings_count, :integer, default: 0
+    add_column :users, :tweets_count, :integer, default: 0
+    add_column :users, :liked_tweets_count, :integer, default: 0
+  end
+end
