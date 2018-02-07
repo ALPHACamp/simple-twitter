3400f1b9cc647e18a297e6b188312a1e6a613ea5
Tony Lin <tonylin1208@gmail.com>
2018-02-08 03:17:31 +0800

    test something not important

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,9 @@
 class Admin::UsersController < Admin::BaseController
   def index
     # 管理者可以瀏覽站內所有的使用者清單，該清單按推播文數排序
-    # @users= User.joins(:tweet).merge(Tweet.order(priority: :desc))
-    @users = User.all.page(params[:page]).per(10)
+    # @users= User.joins(:tweet).merge(Tweet.order(created_at: :desc))
+    # @users = User.joins(:user_extension).merge(UserExtension.order(company: :desc))
+    # @users = User.includes(:tweets).order("tweet.created_at desc").page(params[:page]).per(10)
+    @users = User.page(params[:page]).per(10)
   end
 end
