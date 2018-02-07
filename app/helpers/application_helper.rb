2f89719eab481b92a135b11c45f37ee0ec270a4d
Zarek <simen1127@gmail.com>
2018-02-08 00:10:15 +0800

    user login/create username/admin default page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+   def icon_link_to(label, path,icon) 
+    link_to path do
+      content_tag :span, class: "glyphicon glyphicon-#{icon}" do
+      label
+      end
+    end 
+  end
 end
