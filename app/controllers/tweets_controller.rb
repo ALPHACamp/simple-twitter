9910ef3477492e8211d25c564673d786d92aa2b9
KayCheng <kaycheng717@gmail.com>
2018-02-07 19:33:20 +0800

    Add top 10 tweets on tweet index page

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,16 +1,28 @@
 class TweetsController < ApplicationController
 
   def index
-    @users # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
+    # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
+    @users 
+    @tweets = Tweet.order(created_at: :desc).limit(10)
   end
 
   def create
+    @tweet = Tweet.build(tweet_params)
+    @tweet.user = current_user
+    @tweet.save
+    redirect_to root_path
   end
 
   def like
   end
 
   def unlike
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:description)
   end
 
 end
