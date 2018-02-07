14b68dc6785a52c979060eeb7bb5b8d4fad93b79
Brian <finder.chen@gmail.com>
2018-02-07 23:38:35 +0800

    add tweet index and create action

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,10 +1,19 @@
 class TweetsController < ApplicationController
 
   def index
-    @users # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
+    @tweets = User.find(current_user.id).tweets.order(created_at: :desc)
+    @tweet = Tweet.new
+    @users = User.all.limit(10) # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
   end
 
   def create
+    @tweet = current_user.tweets.build(tweet_params)
+    if @tweet.save
+      flash[:notice] = "tweet created successfully"
+    else
+      flash[:alert] = @tweet.errors.full_messages.to_sentence
+    end
+    redirect_to root_path
   end
 
   def like
@@ -13,4 +22,8 @@ class TweetsController < ApplicationController
   def unlike
   end
 
+  private
+  def tweet_params
+    params.require(:tweet).permit(:description)
+  end
 end
