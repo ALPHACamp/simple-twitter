6eb9f6765406e6a2bb97ca2d26ce3f9189640520
mordochi <lilynnchi@gmail.com>
2018-02-07 21:31:00 +0800

    add set_user in UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,17 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:tweets, :edit, :update, :followings, :followers, :likes]
 
   def tweets
-    @user = User.find(params[:id])
     @tweets = @user.tweets.order(Tweet.arel_table['created_at'].desc)
   end
 
   def edit
-    @user = User.find(params[:id])
     if current_user != @user
-      redirect_to user_path(@user)
+      redirect_to tweets_user_path(@user)
     end
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:notice] = "User profile has been updated"
       redirect_to tweets_user_path(@user)
@@ -24,21 +22,22 @@ class UsersController < ApplicationController
   end
 
   def followings
-    @user = User.find(params[:id])
     @followings = @user.followings.order(Followship.arel_table['created_at'].desc) # 基於測試規格，必須講定變數名稱
   end
 
   def followers
-    @user = User.find(params[:id])
     @followers = @user.followers.order(Followship.arel_table['created_at'].desc) # 基於測試規格，必須講定變數名稱
   end
 
   def likes
-    @user = User.find(params[:id])
     @likes = @user.liked_tweets.order(Like.arel_table['created_at'].desc) # 基於測試規格，必須講定變數名稱
   end
 
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:name, :introduction, :avatar)
