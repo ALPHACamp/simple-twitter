d4b739c073f8c2db79812365ca494e71cfd3f233
francishuang <francis3925@gmail.com>
2018-02-07 20:41:23 +0800

    enable user to edit personal info on users edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,21 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+    unless @user == current_user
+      flash.alert = "只能修改自己的資料喔！"
+      redirect_to tweets_path
+    end
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash.notice = "修改個人資料成功！"
+      redirect_back fallback_location: root_path
+    else
+      render :edit
+    end
   end
 
   def followings
@@ -19,6 +31,12 @@ class UsersController < ApplicationController
 
   def likes
     @likes # 基於測試規格，必須講定變數名稱
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :avatar)
   end
 
 end
