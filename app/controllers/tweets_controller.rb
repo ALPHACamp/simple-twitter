26bab2ec999bba4c09997722ff788fe47479eb80
Yuyu <qscez2001@gmail.com>
2018-02-07 22:06:36 +0800

    add bootstrap to application.scss

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,14 +2,12 @@ class TweetsController < ApplicationController
 
   def index
     @tweets = Tweet.all
+    @tweet = Tweet.new
     # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
     @users = User.order(followers_count: :desc).limit(10)
     
   end
 
-  def new
-    @tweet = Tweet.new
-  end
 
 
   def create
