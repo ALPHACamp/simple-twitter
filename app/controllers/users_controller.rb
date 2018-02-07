675de1603d414b99db80fe6747e50dc010f51ff3
Siegmund <amceog88@gmail.com>
2018-02-07 17:52:27 +0800

    fix #edit controller problem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,44 +3,31 @@ class UsersController < ApplicationController
 
 
   def tweets
-    @tweeted = @user.tweets.order("created_at DESC").uniq
-    @followings = @user.followings
-    @followers = @user.followers
-    @liked_tweets = @user.liked_tweets
+    @tweets = @user.tweets.order("created_at DESC")
   end
 
   def edit
     unless @user == current_user
-      redirect_to tweets_user_path(current_user)
+      redirect_to tweets_user_path(@user)
     end
   end
 
   def update
-    unless @user == current_user
-      redirect_to tweets_user_path(current_user)
-    end
+    @user.update(user_params)
+    redirect_to tweets_user_path(@user)
   end
 
   def followings
-    @tweeted = @user.tweets
     @followings = @user.followings.order("created_at DESC")
-    @followers = @user.followers
-    @liked_tweets = @user.liked_tweets
     # 基於測試規格，必須講定變數名稱
   end
 
   def followers
-    @tweeted = @user.tweets
-    @followings = @user.followings
     @followers = @user.followers.order("created_at DESC")
-    @liked_tweets = @user.liked_tweets
     # 基於測試規格，必須講定變數名稱
   end
 
   def likes
-    @tweeted = @user.tweets
-    @followings = @user.followings
-    @followers = @user.followers
     @liked_tweets = @user.liked_tweets.order("created_at DESC")
     # 基於測試規格，必須講定變數名稱
   end
