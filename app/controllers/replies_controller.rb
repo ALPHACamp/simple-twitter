19ac5cdd368a3a5578a0113344dc94922ccd3300
Chad <xmen11034@gmail.com>
2018-02-08 04:57:04 +0800

    replies page function completed and styled

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -2,9 +2,27 @@ class RepliesController < ApplicationController
 
   def index
     @tweet = Tweet.find(params[:tweet_id])
+    @reply = Reply.new
+    @replies = @tweet.replies
   end
 
   def create
+    @tweet = Tweet.find(params[:tweet_id])
+    @reply = @tweet.replies.build(comment: reply_params[:comment], user: current_user)
+    if @reply.save
+      flash[:notice] = "reply success!"
+      redirect_to tweets_path
+    else
+      flash[:alert] = @reply.errors.full_messages.to_sentence
+      redirect_back fallback_location: tweets_path
+    end
+  end
+
+
+  private
+
+  def reply_params
+    params.require(:reply).permit(:comment)
   end
 
 end
