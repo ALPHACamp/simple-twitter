0537cb2ac834f7b5aa9ff74efc11b96de8e4365c
sapphirekuo <sapphirekuo@gmail.com>
2018-02-07 15:45:26 +0800

    add reply page and add Reply function on reply page.

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,9 +1,36 @@
 class RepliesController < ApplicationController
 
   def index
+    @tweets = Tweet.all.where(id: params[:tweet_id])
+    @tweet = Tweet.find(params[:tweet_id])
+    @replies = Reply.all.order(created_at: :asc)
+    @reply = Reply.new
   end
 
   def create
+    @tweets = Tweet.all.where(id: params[:tweet_id])
+    @tweet = Tweet.find(params[:tweet_id])
+    @replies = Reply.all.where(tweet_id: params[:tweet_id])
+    
+    @reply = @tweet.replies.build(reply_params)
+    @reply.user = current_user
+    
+    if @reply.save
+      flash[:notice] = "Reply was successfully created"
+      redirect_to tweet_replies_path
+    else
+      flash.now[:alert] = "Reply can't be blank or longer than 140 characters"
+      render :index
+    end
+  end
+
+
+
+
+  private
+
+  def reply_params
+    params.require(:reply).permit(:comment)
   end
 
 end
--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,4 +1,7 @@
 class Reply < ApplicationRecord
-  belongs_to :tweet
+  validates_length_of :comment, maximum: 140
+  validates_presence_of :comment
+
+  belongs_to :tweet, counter_cache: true
   belongs_to :user
 end
