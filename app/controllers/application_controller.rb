3d294b7e671e44ae9c657d8b3daafbd60743098b
Chia-Hung Lin <qaz741012@gmail.com>
2018-02-07 17:03:25 +0800

    admin will be redirect_to admin_root_path after log in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,4 +23,12 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def after_sign_in_path_for(resource)
+    if current_user.admin?
+      admin_root_path
+    else
+      root_path
+    end
+  end
+
 end
