673da142ad73aecdaa0408f8da76597098b3fc8d
Tony Lin <tonylin1208@gmail.com>
2018-02-08 03:16:40 +0800

    reset the root_path

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -1,14 +1,15 @@
 class Admin::TweetsController < Admin::BaseController
   
   def index
-    @tweets = Tweet.all.order(created_at: :desc).page(params[:page]).per(10)
+    @tweets = Tweet.all
+    @tweets = @tweets.order(created_at: :desc).page(params[:page]).per(10)
     # @replies = Tweet.replies.order(created_at: :desc).page(params[:page]).per(10)
   end
 
   def destroy
     @tweet = Tweet.find(params[:id])
     @tweet.destroy
-    redirect_to admin_tweets_path
+    redirect_to(admin_root_path)
     flash[:alert] = "Tweet was deleted"
   end
 end
