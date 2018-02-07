4875d891a46ba08c25e446e75fd93c435336c66b
BJ0815 <jtt11220@gmail.com>
2018-02-07 13:56:33 +0800

    add minimagic gem to resize the avatar

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,7 +1,7 @@
 class AvatarUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -21,14 +21,19 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
   #
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
-  #
-  # version :medium do
-  #   process resize_to_fit: [150, 150]
-  # end
+  process resize_to_fit: [800, 800]
 
+  version :thumb do
+    process resize_to_fill: [50, 50]
+  end
+  
+  version :medium do
+    process resize_to_fill: [150, 150]
+  end
+
+  version :large do
+    process resize_to_fill: [300, 300]
+  end
   # Process files as they are uploaded:
   # process scale: [200, 300]
   #
@@ -43,9 +48,9 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
