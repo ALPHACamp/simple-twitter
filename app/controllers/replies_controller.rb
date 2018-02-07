fa0f0fc2bee69ca028edae13e5eb65777171c545
Cindy Liu <cindyliu923@gmail.com>
2018-02-07 22:46:10 +0800

    add reply function and view for tweet reply

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,9 +1,28 @@
 class RepliesController < ApplicationController
 
   def index
+    @tweet = Tweet.find(params[:tweet_id])
+    @reply = Reply.new
   end
 
   def create
+    @tweet = Tweet.find(params[:tweet_id])
+    @reply = @tweet.replies.build(reply_params)
+    @reply.user = current_user
+    if @reply.save
+      flash[:notice] = "回覆成功！"
+      redirect_to tweet_replies_path(@tweet)
+    else
+      @tweet = Tweet.find(params[:tweet_id])     
+      flash[:alert] = "回覆不可以空白!!"
+      redirect_to tweet_replies_path(@tweet)
+    end
+  end
+
+  private
+
+  def reply_params
+    params.require(:reply).permit(:comment)
   end
 
 end
