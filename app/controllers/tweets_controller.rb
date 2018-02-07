aebf0c9d21d4d9a099d7fd79e7eb43d4756ddc15
Wayne <wayne0042@gmail.com>
2018-02-07 15:31:09 +0800

    finished tweet create

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,16 +3,28 @@ class TweetsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @tweet = Tweet.new
+    @tweets = Tweet.all
     @users # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
   end
 
   def create
+    @tweet = Tweet.new(tweet_params)
+    @tweet.user = current_user
+    @tweet.save!
+    redirect_to root_path
   end
 
   def like
   end
 
   def unlike
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:description)
   end
 
 end
