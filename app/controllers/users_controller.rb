05863e50c5c1c63962f7f1fc40690e3e5fb895dc
chunchaolee <chunchaolee@gmail.com>
2018-02-07 23:01:12 +0800

    setup likes method on users controller, and refactoring users controller also

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,18 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only: [:tweets, :edit, :update, :followings, :followers, :likes]
+
   def tweets
-    @user = User.find(params[:id])
+    # set_user
     @tweets = Tweet.where(user_id: params[:id]).order(created_at: :desc)
   end
 
   def edit
-    @user = User.find(params[:id])
+    # set_user
   end
 
   def update
-    @user = User.find(params[:id])
+    # set_user
     if current_user == @user
       if @user.update_attributes(user_params)
         redirect_to root_url, :notice => "個人資料更新成功"
@@ -23,23 +25,28 @@ class UsersController < ApplicationController
   end
 
   def followings
-    @user = User.find(params[:id])
+    # set_user
     @followings = @user.followings
   end
 
   def followers
-    @user = User.find(params[:id])
+    # set_user
     @followers = @user.followers
   end
 
   def likes
-    @likes # 基於測試規格，必須講定變數名稱
+    # set_user
+    @likes = @user.liked_tweets
   end
 
   private
 
   def user_params
     params.require(:user).permit(:email, :name, :introduction, :avatar)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 
