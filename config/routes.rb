b94e67465ba788a45aaa5b92c81f1dc0ced1b989
francishuang <francis3925@gmail.com>
2018-02-07 18:49:36 +0800

    create route for users#tweets, replies#index & create

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,16 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
 
-  resources :users, only: [:edit]
+  resources :users, only: [:edit] do
+    member do
+      get :tweets
+    end
+  end
 
   # 請依照專案指定規格來設定路由
-  resources :tweets, only: [:index]
+  resources :tweets, only: [:index, :create] do
+    resources :replies, only: [:index, :create]
+  end
   root 'tweets#index'
 
   namespace :admin do
