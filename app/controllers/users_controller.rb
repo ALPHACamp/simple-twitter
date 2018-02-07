05a3e6023c7c355b15b22420e50e572a2aba07e3
victorloo <ila9970@gmail.com>
2018-02-08 04:26:22 +0800

    build ptototype to each user's pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,25 +8,30 @@ class UsersController < ApplicationController
 
   #編輯自己的資料
   def edit
+    @user = User.find(params[:id])
   end
 
   #編輯好自己的資料好，上傳
   def update
+    @user = User.find(params[:id])
   end
 
   #我在追蹤誰
   def followings
     @followings # 基於測試規格，必須講定變數名稱
+    @user = User.find(params[:id])
   end
 
   #誰在追蹤我
   def followers
     @followers # 基於測試規格，必須講定變數名稱
+    @user = User.find(params[:id])
   end
 
   #tweet被喜歡的數量
   def likes
     @likes # 基於測試規格，必須講定變數名稱
+    @user = User.find(params[:id])
   end
 
 end
