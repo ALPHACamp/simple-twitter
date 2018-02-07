50d995adf812108883df6171ae44962f28d39e63
annie chiang <annie100patty@gmail.com>
2018-02-07 16:40:43 +0800

    add follow/unfollow button and their actions

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -1,7 +1,17 @@
 class FollowshipsController < ApplicationController
   def create
+    @followship = current_user.followships.build(following_id: params[:following_id])
+    if @followship.save
+      redirect_back(fallback_location: root_path)
+    else
+      flash[:alert] = @followship.errors.full_messages.to_sentence
+      redirect_back(fallback_location: root_path)
+    end
   end
 
   def destroy
+    @followship = Followship.where(user: current_user, following_id: params[:id]).first
+    @followship.destroy
+    redirect_back(fallback_location: root_path)
   end
 end
