0b3c245cdd6d455211cabff65f0ed6f88f8df324
Tony Lin <tonylin1208@gmail.com>
2018-02-08 00:28:43 +0800

    user login user case_sensitive

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,13 @@ class User < ApplicationRecord
 
   mount_uploader :avatar, AvatarUploader
 
+  # 需要 app/views/devise 裡找到樣板，加上 name 屬性
+  # 並參考 Devise 文件自訂表單後通過 Strong Parameters 的方法
+  # 加上驗證 name 不能重覆 (關鍵字提示: uniqueness)
+  validates_presence_of :name
+  # 使用者的名稱不能重覆，若有重覆會跳出錯誤
+  validates :name, uniqueness: { case_sensitive: true, message: "has already been taken"}
+
   #reply
   has_many :tweets, dependent: :destroy
   has_many :replies, dependent: :destroy
@@ -21,14 +28,6 @@ class User < ApplicationRecord
   has_many :inverse_followships, class_name: "Followship", foreign_key: "following_id"
   has_many :followers, through: :inverse_followships, source: :user
 
-  # 需要 app/views/devise 裡找到樣板，加上 name 屬性
-  # 並參考 Devise 文件自訂表單後通過 Strong Parameters 的方法
-  # 加上驗證 name 不能重覆 (關鍵字提示: uniqueness)
-  validates_presence_of :name
-
-  # 使用者的名稱不能重覆，若有重覆會跳出錯誤
-  validates :name, uniqueness: { message: " has already been taken."}
-
   def admin?
     self.role == "admin"
   end
@@ -37,13 +36,14 @@ class User < ApplicationRecord
     self.followings.include?(user)
   end
 
-  def self.recent_following
-    Followship.select('following_id, count(keyword_id) as frequency').
-      order('frequency desc').
-      group('keyword_id').
-      take(20).
-      map(&:keyword)
-  end
+  # def self.recent_following
+  #   Person.includes(:pets).order("pet.name DESC")
+  #   Followship.select('following_id, count(following_id) as frequency').
+  #     order('frequency desc').
+  #     group('following_id').
+  #     take(20).
+  #     map(&:keyword)
+  # end
 
   def tweet_reply_count
     self.tweets.count + self.replies.count
