80d06c00114faf515d0262d5c7196451f8348bf4
chunchaolee <chunchaolee@gmail.com>
2018-02-07 09:53:00 +0800

    added following method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,8 @@ class UsersController < ApplicationController
   end
 
   def followings
-    @followings # 基於測試規格，必須講定變數名稱
+    @user = User.find(params[:id])
+    @followings = @user.followings.order(created_at: :desc)
   end
 
   def followers
