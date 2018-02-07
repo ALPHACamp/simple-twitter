bd8c122940c8fcb9e1811e148dfa396eaf97e608
Lienchi <Lienchi.Lai@gmail.com>
2018-02-07 23:15:54 +0800

    add like/unlike function

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   # 請依照專案指定規格來設定路由
   resources :tweets, only: [:index, :create] do
     resources :replies, only: [:index, :create]
+
+    member do
+      post :like
+      post :unlike
+    end
   end
   root "tweets#index"
 
