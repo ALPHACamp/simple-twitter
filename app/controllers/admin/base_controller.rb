bb03c0e0971af215c787156611dc759b173fa361
spreered <spreered@gmail.com>
2018-02-07 23:19:59 +0800

    fix bug: gitignore QQ

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,7 +1,7 @@
 class Admin::BaseController < ApplicationController
   before_action :authenticate_admin
   #確認是admin才可以進後台
-  #layout "admin"
+  layout "admin"
   private 
     def authenticate_admin
       unless current_user.admin?
