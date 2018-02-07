8d6525948a98849c3dd4a2a137fcab898430b62c
Unknown <thetasteofleavesisgreat@gmail.com>
2018-02-07 22:58:29 +0800

    set reply action and  view

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,9 +1,30 @@
 class RepliesController < ApplicationController
 
   def index
+    @tweet =Tweet.find(params[:tweet_id])
+    @user = User.find(@tweet.user_id)
+    @replies = Reply.all
+    @reply = Reply.new
   end
 
   def create
+    @tweet = Tweet.find(params[:tweet_id])
+    @reply = @tweet.replies.build(comment_params)
+    @reply.user = current_user
+    if @reply.save
+      flash[:notice] = "Reply was successfully created"
+      redirect_back(fallback_location: tweet_replies_path)
+    else
+      flash[:alert] = @reply.errors.full_messages.to_sentence
+      redirect_back(fallback_location: tweet_replies_path)
+    end
+
+
   end
+
+  def comment_params
+    params.require(:reply).permit(:comment)
+  end
+
 
 end
