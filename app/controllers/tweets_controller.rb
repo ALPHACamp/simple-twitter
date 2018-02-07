758b7da7da9127ab99a8d9b148d8d1149622656f
victorloo <ila9970@gmail.com>
2018-02-07 22:07:12 +0800

    add avatar as button on nav-bar

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,6 +5,7 @@ class TweetsController < ApplicationController
   def index
     @tweets = Tweet.all
     @users = User.all
+    @user = current_user
     #@users # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
   end
 
