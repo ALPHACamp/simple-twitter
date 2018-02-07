f5a4fcd951c402f80e93b8fd357d835002075d45
sapphirekuo <sapphirekuo@gmail.com>
2018-02-07 14:16:23 +0800

    tweets on main page are ordered by created_at

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,13 +2,13 @@ class TweetsController < ApplicationController
 
   def index
     @users # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
-    @tweets = Tweet.all
+    @tweets = Tweet.all.order(created_at: :desc)
     @tweet = Tweet.new
   end
 
   def create
     @users
-    @tweets = Tweet.all
+    @tweets = Tweet.all.order(created_at: :desc)
 
     @tweet = Tweet.new(tweet_params)
     @tweet.user = current_user
