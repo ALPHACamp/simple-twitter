93d2aa133e81fc0b04a3c9808a4bdc284bb9c67a
KayCheng <kaycheng717@gmail.com>
2018-02-07 23:08:53 +0800

    Add form on tweet index page

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,15 +2,20 @@ class TweetsController < ApplicationController
 
   def index
     # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
-    @users 
+    #@users 
     @tweets = Tweet.order(created_at: :desc).limit(10)
+    
+    @tweet = Tweet.new
   end
 
   def create
-    @tweet = Tweet.build(tweet_params)
-    @tweet.user = current_user
-    @tweet.save
-    redirect_to root_path
+    @tweet = current_user.tweets.build(tweet_params)
+    if @tweet.save
+      redirect_to root_path
+    else
+      @tweets = Tweet.order(created_at: :desc).limit(10)
+      render :index
+    end
   end
 
   def like
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "tweets#index"
+  resources :tweets, only: [:index]
   resources :users, only: [:edit, :update] do
     member do
       get :tweets
