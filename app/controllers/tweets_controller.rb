ded89d334662891d7d95e8d1c4fcb2edb6fb3dd7
eric07109 <eric07109@gmail.com>
2018-02-08 22:53:13 +0800

    created unlike action and its link on reply thumbnail

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -20,8 +20,8 @@ class TweetsController < ApplicationController
     @like = current_user.likes.build(tweet_id: params[:id])
     if @like.save
       flash[:notice] = "Successfully liked"
-        current_user.increment!(:likes_count)
-        @tweet.increment!(:likes_count)
+      current_user.increment!(:likes_count)
+      @tweet.increment!(:likes_count)
       redirect_back(fallback_location: root_path)
     else
       flash[:alert] = @like.errors.full_messages.to_sentence
@@ -30,6 +30,15 @@ class TweetsController < ApplicationController
   end
 
   def unlike
+    @tweet = Tweet.find(params[:id])
+    @like = current_user.likes.where(tweet_id: params[:id])
+    if @like
+      @like.destroy_all()
+      current_user.decrement!(:likes_count)
+      @tweet.decrement!(:likes_count)
+      flash[:notice] = "Successfully unliked"
+      redirect_back(fallback_location: root_path)
+    end
   end
 
   private
