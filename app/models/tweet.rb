77db17d772e0e7948b0c27c81c4cfb2d5efcee09
eric07109 <eric07109@gmail.com>
2018-02-08 22:48:51 +0800

    added liked by user method in tweet model and use it to determine like/unlike button

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -8,4 +8,9 @@ class Tweet < ApplicationRecord
 
   has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
+
+  def liked_by_user?(user)
+  	self.liked_users.include?(user)
+  end
+
 end
