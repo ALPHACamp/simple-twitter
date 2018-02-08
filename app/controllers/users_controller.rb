62c6a270f8c743f8b68915f92990046777b5aa91
KC <weilihmen@gmail.com>
2018-02-08 23:13:54 +0800

    user edit controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,22 @@ class UsersController < ApplicationController
   end
 
   def edit
+    #功能已併入devise頁面,但還是先寫起來放著
+  end
+
+  def update
+    #功能已併入devise頁面,但還是先寫起來放著
+    @new_restaurant = Restaurant.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "更新成功"
+      redirect_to root_path
+    else
+      flash.now[:alert] = "更新失敗"
+      render :edit
+    end
+  end
+
+  def edit
     #併入devise頁面
   end
 
@@ -31,6 +47,10 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :avatar, :introduction)
   end
 
 end
