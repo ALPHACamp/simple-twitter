1796f634220404d177434618190463d44dca28d1
SHLIPH <wontstoplearn@gmail.com>
2018-02-08 16:02:21 +0800

    admin can see all users and all tweets

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -5,8 +5,14 @@ class Admin::TweetsController < Admin::BaseController
   before_action :authenticate_admin
 
   def index
+    @users = User.all
+    @tweets = Tweet.all
   end
 
   def destroy
+    @tweets = Tweet.find(params[:id])
+    @tweet.destroy
+    redirect_to admin_tweets_path
+    flash[:alert] = "tweet was deleted"
   end
 end
