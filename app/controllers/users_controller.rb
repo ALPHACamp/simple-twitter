c5cd0dc808142b449b462186c2db156a7a657ebf
Unknown <thetasteofleavesisgreat@gmail.com>
2018-02-08 15:20:25 +0800

    edit  user profile controller and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,20 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
-
+    unless @user == current_user
+      redirect_to tweets_user_path(@user)
+    end     
   end
 
   def update
     @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "User was successfully updated"
+      redirect_to tweets_user_path(@user)
+    else
+      flash.now[:alert] = "#{@user.errors.full_messages.to_sentence}"
+      render :edit
+    end 
   end
 
   def followings
@@ -28,5 +37,19 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @likes  =  @user.liked_tweets.order(created_at: :desc)
   end
+
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :introduction, :avatar)
+  end
+
+
+
+
 
 end
