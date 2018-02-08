57061c62f5f5c71b17be4265ec62bbfafac831cf
imwayneliao <imwayneliao@gmail.com>
2018-02-08 21:17:53 +0800

    set up relationship between followship and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :tweets
+  has_many :followships, dependent: :destroy
+  has_many :followings, through: :followships
 
   mount_uploader :avatar, AvatarUploader
 
