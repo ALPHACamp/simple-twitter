3251c869bb67b1eff0e0cef700d14b4b79b05cb7
Zarek <simen1127@gmail.com>
2018-02-08 19:47:54 +0800

    edit profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,17 @@
 class UsersController < ApplicationController
-
+  before_action :set_restaurant, only: [:edit, :update]
   def tweets
     @user_tweets = User.find(params[:id]).tweets
   end
 
-  def edit
-  end
-
   def update
+    if @users.update_attributes(users_params)    
+      flash[:notice] = "users was scuccessfully updated"
+      redirect_to tweets_user_path(current_user)
+    else
+      flash.now[:alert] ="users was failed to update"
+      render :edit
+    end  
   end
 
   def followings
@@ -20,6 +24,17 @@ class UsersController < ApplicationController
 
   def likes
     @likes # 基於測試規格，必須講定變數名稱
+  end
+
+
+  private
+
+  def set_restaurant
+    @users = User.find(params[:id])    
+  end
+
+  def users_params
+    params.require(:user).permit(:name, :introduction, :avatar)  
   end
 
 end
--- lib/tasks/dev.rake
+++ lib/tasks/dev.rake
@@ -2,7 +2,7 @@ namespace :dev do
   # 請先執行 rails dev:fake_user，可以產生 20 個資料完整的 User 紀錄
   # 其他測試用的假資料請依需要自行撰寫
   task fake_user: :environment do
-    User.destroy_all
+    #User.destroy_all
     20.times do |i|
       name = FFaker::Name::first_name
       file = File.open("#{Rails.root}/public/avatar/user#{i+1}.jpg")
@@ -20,4 +20,17 @@ namespace :dev do
     end
   end
 
+  task fake_tweet: :environment do
+    Tweet.destroy_all
+    User.all.each do |tweet|
+      3.times do |i|
+      Tweet.create!(
+        description: FFaker::Lorem.sentence[0..140],
+        user: User.all.sample
+      )  
+      end
+    end
+      puts "have created fake Tweet"
+      puts "now you have #{Tweet.count} Tweet data"
+  end
 end
