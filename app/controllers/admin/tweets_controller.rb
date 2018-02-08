4b57f60be244c3fda0dfc6b57f49925d00beea48
caseydyps <caseydyps@gmail.com>
2018-02-08 22:25:43 +0800

    admin tweets page update

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -1,12 +1,12 @@
 class Admin::TweetsController < Admin::BaseController
 
-
-
   def index
     @tweets = Tweet.all.includes(:replies, :user).order(replies_count: :desc)
   end
 
   def destroy
+
+    @tweet = Tweet.find(params[:id])
     @tweet.destroy
     redirect_to admin_tweets_path
     flash[:alert] = "Tweet was deleted"
