1f8afaff315f9ab19f4ca81c228840b2d2f53e22
badbug wne <sweewen45@gmail.com>
2018-02-09 07:59:23 +0800

    add tweet#index/create action and views

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,10 +1,17 @@
 class TweetsController < ApplicationController
   before_action :set_tweet, only:[:like, :unlike]
   def index
-    @users # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
+    # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
+    @users = User.order(followers_count: :DESC).limit(10) 
+    @tweets = Tweet.order(created_at: :desc)
+    @tweet = Tweet.new
   end
 
   def create
+    @tweet = Tweet.new(tweet_params)
+    @tweet.user = current_user
+    @tweet.save!
+    redirect_to root_path
   end
 
   def like
@@ -26,4 +33,7 @@ class TweetsController < ApplicationController
     @tweet = Tweet.find(params[:id])
   end
 
+  def tweet_params
+    params.require(:tweet).permit(:description)
+  end
 end
