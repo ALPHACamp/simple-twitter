9ffa81211253896ba888d5e4c6d09ebc9a5bf72a
Adamlin0708 <kuso12610@gmail.com>
2018-02-09 03:03:35 +0800

    fix error

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module SimpleTwitter
     config.time_zone = "Asia/Taipei"
     config.filestack_rails.api_key = "ARz3g0HLfR5i0KuobcdJmz"
     config.filestack_rails.client_name = "itweets"
+    config.assets.initialize_on_precompile=false                                                  
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
