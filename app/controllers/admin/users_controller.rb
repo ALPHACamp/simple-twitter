35d91bcdb6e82618132de9003a52eff96cb360e8
Kansetsu Kuo <kansetsu4@gmail.com>
2018-02-08 09:37:42 +0800

    Fix bug: tweets_spec.rb:24 by add order to users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,5 @@
 class Admin::UsersController < Admin::BaseController
   def index
-    @users = User.all  # not follow the spec but passed. WTF are you guys doing?
-    # @users = User.all.order('tweets_count desc')  # follow the spec but not passed
+    @users = User.all.order('tweets_count desc')
   end
 end
