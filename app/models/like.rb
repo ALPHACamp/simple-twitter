fb7653866b94c38f6a9dc30952ba43162e6d703f
victorloo <ila9970@gmail.com>
2018-02-09 03:53:23 +0800

    add count action for reply and like

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,4 +2,7 @@ class Like < ApplicationRecord
   belongs_to :user, class_name: "User"
   belongs_to :tweet, class_name: "Tweet"
   #如何知道被喜歡
+
+  #counter_cache 計算關聯物件的數量
+  belongs_to :tweet, counter_cache: true
 end
--- app/models/reply.rb
+++ app/models/reply.rb
@@ -2,4 +2,7 @@ class Reply < ApplicationRecord
   belongs_to :user, class_name: "User"
   belongs_to :tweet, class_name: "Tweet"
   #就是留言，留言的模型跟推特不一樣
+
+  #counter_cache 計算關聯物件的數量
+  belongs_to :tweet, counter_cache: true
 end
