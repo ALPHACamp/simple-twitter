5876a0eea5d9b179c9e6eb50737f0a72db9ea670
penciler <eraser41@livemail.tw>
2018-02-08 15:59:26 +0800

    modify update function of user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,9 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user=User.find(params[:id])
+    @user.update(user_params)
+    redirect_to tweets_user_path(@user)
   end
 
   def followings
@@ -29,6 +32,10 @@ class UsersController < ApplicationController
     @likes=User.find(params[:id]).likes # 基於測試規格，必須講定變數名稱
   end
 
+  private
 
+  def user_params
+    params.require(:user).permit(:name, :avatar)
+  end
 
 end
