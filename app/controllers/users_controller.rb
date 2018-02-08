f40829011740c7fabec9be3420f9e4afa26719d8
spreered <spreered@gmail.com>
2018-02-09 01:49:53 +0800

    add users#likes comment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,9 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     # 不確定
     @followings = @user.followings.order('followships.updated_at DESC')
+    #SELECT  "users".* FROM "users" INNER JOIN "followships"
+    # ON "users"."id" = "followships"."following_id" WHERE "followships"."user_id" = ? 
+    # ORDER BY followships.updated_at DESC
   end
 
   def followers
@@ -37,6 +40,11 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @likes = @user.likes.order(created_at: :desc)
     #@likes_tweet = @user.liked_tweets.order('likes.updated_at DESC')
+    #原本的寫法會錯, 產生的query是
+    #SELECT  "tweets".* FROM "tweets" INNER JOIN 
+    #"likes" ON "tweets"."id" = "likes"."tweet_id" 
+    #WHERE "likes"."user_id" = ? ORDER BY "tweets"."created_at" DESC, likes.updated_at DESC
+    # 會把tweets.id拉進來排序
   end
 
   private 
