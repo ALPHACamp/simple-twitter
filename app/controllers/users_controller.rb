698eb57e772680cdddf70fedb6923279ca1f28be
Brian <finder.chen@gmail.com>
2018-02-08 08:45:45 +0800

    add tweets edit and update action for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,21 @@
 class UsersController < ApplicationController
 
+  before_action :get_user, only: [:tweets, :edit, :update]
   def tweets
+    @tweets = @user.tweets.order(created_at: :desc)
   end
 
   def edit
   end
 
   def update
+    if @user.update(user_params)
+      flash[:notice] = "user profile has been updated successfully"
+      redirect_to root_path
+    else
+      flash[:alert] = @user.errors.full_messages.to_sentence
+      render :edit
+    end
   end
 
   def followings
@@ -21,4 +30,12 @@ class UsersController < ApplicationController
     @likes # 基於測試規格，必須講定變數名稱
   end
 
+  private
+  def get_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :introdution, :avatar)
+  end   
 end
