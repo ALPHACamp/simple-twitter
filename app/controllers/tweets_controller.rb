04172e287b677d9234bc90039a5b6126a57551eb
yvesen <l.sen.t@hotmail.com>
2018-02-08 16:29:48 +0800

    popular 10

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,10 +1,10 @@
 class TweetsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_tweet,only:[:show, :edit, :update]
+  before_action :set_tweet,only:[:show, :create, :edit, :update]
   # before_action :authenticate_admin
   def index
     @tweets = Tweet.all
-    @users = User.all
+    @users = User.all.limit(10)
     @tweet = Tweet.new
     # @users 
     # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
@@ -24,14 +24,23 @@ class TweetsController < ApplicationController
     redirect_back(fallback_location: root_path)  # 導回上一頁   
   end
 
-
   def create
-    @tweet = Tweet.find(params[:user_id])
+    @tweet = Tweet.new(tweet_params)
     @tweet = @tweets.build(tweet_params)
     @tweet.user = current_user
-    @tweet.save!
-    redirect_to tweet_path(@tweet)
+    @tweet.save
+      redirect_to tweets_path
+    
   end
+
+
+  # def create
+  #   @tweet = Tweet.find(params[:user_id])
+  #   @tweet = @tweets.build(tweet_params)
+  #   @tweet.user = current_user
+  #   @tweet.save!
+  #   redirect_to tweet_path(@tweet)
+  # end
 
   def like
   end
