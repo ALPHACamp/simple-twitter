9c3f52912b6fce0cbc8505c3880f09dcdea00f7e
Kansetsu Kuo <kansetsu4@gmail.com>
2018-02-08 17:14:33 +0800

    Done: sort admin#user by name, tweets, followings and followers

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,40 @@
 class Admin::UsersController < Admin::BaseController
+  helper_method :sort_column, :sort_direction, :current_title
+
   def index
-    @users = User.all.order('tweets_count desc')
+    # @users = User.all.order('tweets_count desc')
+    @users = User.all.order(sort_column + " " + sort_direction)
+    @current_title = current_title
+  end
+
+
+  private
+
+  def sort_column
+    case params[:sort]
+      when 'Name'
+        'name'
+
+      when 'Tweets'
+        'tweets_count'
+
+      when 'Followings'
+        'followings_count'
+
+      when 'Followers'
+        'followers_count'
+
+      else
+        'tweets_count'
+
+    end
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : 'desc'
+  end
+
+  def current_title
+    %w[Name Tweets Followings Followers].include?(params[:sort]) ? params[:sort] : 'Tweets'
   end
 end
--- app/helpers/admin/users_helper.rb
+++ app/helpers/admin/users_helper.rb
@@ -1,2 +1,20 @@
 module Admin::UsersHelper
+
+  def sortable(column, title = nil)
+    title ||= column
+    if column == current_title && sort_direction == 'desc'
+      direction = 'asc'
+      icon_direction = '-alt'
+    else
+      direction = 'desc'
+      icon_direction = ''
+    end
+
+    if column == current_title
+      link_to("#{column}<span class='glyphicon glyphicon-sort-by-attributes#{icon_direction}'></span>".html_safe, :sort => column, :direction => direction)
+    else
+      link_to(column, :sort => column, :direction => direction)
+    end
+  end
+
 end
