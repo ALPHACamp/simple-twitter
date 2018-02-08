381f748b31010c002a3dcbfe8000bea06f384f9d
sapphirekuo <sapphirekuo@gmail.com>
2018-02-08 09:47:18 +0800

    add user-edit page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:tweets, :edit, :update, :followings, :followers, :likes]
 
   def tweets
-    @user = User.find(params[:id])
     @tweets = Tweet.where(user_id: params[:id])
   end
 
@@ -9,21 +9,36 @@ class UsersController < ApplicationController
   end
 
   def update
+    if @user.update(user_params)
+      flash[:notice] = "User was successfully updated"
+      redirect_to tweets_user_path(@user)
+    else
+      flash[:alert] = "User was failed to update"
+      render:edit
+    end
   end
 
   def followings
-    @user = User.find(params[:id])
     @followings = @user.followings # 基於測試規格，必須講定變數名稱
   end
 
   def followers
-    @user = User.find(params[:id])
     @followers = @user.followers # 基於測試規格，必須講定變數名稱
   end
 
   def likes
-    @user = User.find(params[:id])
     @likes = Like.where(user_id: params[:id]) # 基於測試規格，必須講定變數名稱
   end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :avatar)
+  end
+
 
 end
