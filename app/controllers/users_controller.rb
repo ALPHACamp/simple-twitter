1fc94b2748962b5dd1837b255c38be6b918ee995
sakuraki <a1989198@hotmail.com>
2018-02-09 02:41:44 +0800

    add followers and followings & setup shared userlist

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:tweets, :edit, :update]
+  before_action :set_user, only: [:tweets, :edit, :update, :likes, :followings, :followers]
   #before_action :set_user
   def tweets
     @tweets = @user.tweets.order(updated_at: :desc)
@@ -17,11 +17,17 @@ class UsersController < ApplicationController
   end
 
   def followings
-    @followings # 基於測試規格，必須講定變數名稱
+    #@followings # 基於測試規格，必須講定變數名稱
+    #@followings = @user.followings
+    @user = User.find(params[:id])
+    @followings = @user.followings.order('followships.updated_at DESC')
+    #@followings = @user.followings.includes(:followships).order("followships.created_at desc")
+    
   end
 
   def followers
-    @followers # 基於測試規格，必須講定變數名稱
+    #@followers # 基於測試規格，必須講定變數名稱
+    @followers = @user.followers # 需至 User Model 自訂方法
   end
 
   def likes
