75cc93f371036c4142e91f1b00fe587120338998
Duncan <loxa10077@gmail.com>
2018-02-08 10:59:35 +0800

    add create action for user can reply tweets

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -8,6 +8,23 @@ class RepliesController < ApplicationController
   end
 
   def create
+    @tweet = Tweet.find(params[:tweet_id])
+    @reply = @tweet.replies.build(reply_parmas)
+    @reply.user = current_user
+    if @reply.save
+       flash[:notice] = "Reply created!"
+       redirect_back(fallback_location: root_path)
+     else
+      flash[:alert] = @reply.errors.full_messages.to_sentence
+      redirect_back(fallback_location: root_path)
+    end
+
+  end
+
+  private
+
+  def reply_parmas
+    params.require(:reply).permit(:comment)
   end
 
 end
