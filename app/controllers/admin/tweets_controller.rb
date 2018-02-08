31fd546d5ce0a6fdf14b3b8a9aa0feacd65acb41
eric07109 <eric07109@gmail.com>
2018-02-09 07:32:57 +0800

    created delete tweet action and its link for admin

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -3,8 +3,14 @@ class Admin::TweetsController < Admin::BaseController
   before_action :authenticate_admin
 
   def index
+    @tweets = Tweet.order(created_at: :desc)
+    @users = User.order(followers_count: :desc)
   end
 
   def destroy
+  	@tweet = Tweet.find(params[:id])
+  	@tweet.destroy
+  	flash[:notice] = "Successfully remove tweet"
+  	redirect_back(fallback_location: root_path)
   end
 end
