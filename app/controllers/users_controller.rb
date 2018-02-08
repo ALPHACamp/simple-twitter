3365636710d05f429bc5d82ff07a2de0a5c57b4a
David <biki3507@gmail.com>
2018-02-08 18:13:44 +0800

    2.3 - user edit name introduction avatur
    2.3使用者能編輯自己的名稱、介紹和大頭照

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,22 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
+  def index
+    @users = User.all
+  end
 
   def tweets
   end
 
   def edit
+    unless @user == current_user
+      redirect_to user_path(@user)
+    end
   end
 
   def update
+    @user.update(user_params)
+    redirect_to user_path(@user)
   end
 
   def followings
@@ -19,6 +29,16 @@ class UsersController < ApplicationController
 
   def likes
     @likes # 基於測試規格，必須講定變數名稱
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :avatar)
   end
 
 end
