d6101109b0bec3dc5abe463f226fca0b85819e58
Brian <finder.chen@gmail.com>
2018-02-09 02:03:01 +0800

    add admin tweet_list index and destroy action

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -1,7 +1,12 @@
 class Admin::TweetsController < Admin::BaseController
   def index
+    @tweets = Tweet.page(params[:page]).per(10)
   end
 
   def destroy
+    @tweet = Tweet.find(params[:id])
+    @tweet.destroy
+    flash[:notice] = "Tweet was deleted"
+    redirect_to admin_tweets_path
   end
 end
