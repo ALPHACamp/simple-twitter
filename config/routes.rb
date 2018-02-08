93472e52bfaf4388638d91db4e29f30504548fd9
imwayneliao <imwayneliao@gmail.com>
2018-02-08 21:01:30 +0800

    user can create new tweet on index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
-
-  # 請依照專案指定規格來設定路由
   root "tweets#index"
+  # 請依照專案指定規格來設定路由
+  resources :tweets, only: [:index, :create]
   resources :users, only: [:edit] do
     member do
       get :tweets
