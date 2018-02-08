2ceeeb018df232682c3f389fa3c88976f97c2d59
sakuraki <a1989198@hotmail.com>
2018-02-09 00:20:36 +0800

    add reply button and function

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,9 +1,32 @@
 class RepliesController < ApplicationController
 
   def index
+      @tweet = Tweet.find(params[:tweet_id])
+      @replies = @tweet.replies.all
+      @reply = Reply.new
   end
 
   def create
+     @tweet = Tweet.find(params[:tweet_id])
+     @reply = @tweet.replies.build(comment: reply_params[:comment], user: current_user)
+     
+    if @reply.save
+      
+      @replies = @tweet.replies.all
+      flash[:notice] = "Successfully replied"
+      redirect_to tweet_replies_url
+    else
+      flash.now[:alert] = @reply.errors.full_messages.to_sentence if @reply.errors.any?
+      #@tweets = Tweet.all
+      #render :index
+    end  
+    
+  end
+  
+  private
+  
+  def reply_params
+    params.require(:reply).permit(:comment)
   end
 
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,5 @@
 class Tweet < ApplicationRecord
   validates_length_of :description, maximum: 140
   belongs_to :user, counter_cache: true
+  has_many :replies, dependent: :delete_all
 end
