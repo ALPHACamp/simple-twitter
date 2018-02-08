e55fc1cfbf92787ac747bb7647f4505d16015289
KC <weilihmen@gmail.com>
2018-02-08 17:23:54 +0800

    done admin reply show page and delete function

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -3,6 +3,29 @@ class Admin::TweetsController < Admin::BaseController
   	@tweets=Tweet.all.order(created_at: :desc)
   end
 
+  def show
+  	@tweet=Tweet.find(params[:id])
+  end
+
   def destroy
+    @tweet = Tweet.find(params[:id])
+    if @tweet.destroy
+      flash[:notice] = "刪除成功"
+      redirect_back(fallback_location: "root_path")
+    else
+      flash.now[:alert] = "刪除失敗"
+      render :index
+    end
+  end
+
+  def delete_reply
+  	@reply = Reply.find(params[:id])
+    if @reply.destroy
+      flash[:notice] = "刪除成功"
+      redirect_back(fallback_location: "root_path")
+    else
+      flash.now[:alert] = "刪除失敗"
+      render :show
+    end
   end
 end
