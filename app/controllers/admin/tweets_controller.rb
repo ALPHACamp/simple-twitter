57e51b71143078382c70ea22627bc53043b4a8d6
Dex15900122 <you@example.com>
2018-02-08 16:58:48 +0800

    add admin

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -1,7 +1,18 @@
 class Admin::TweetsController < Admin::BaseController
   def index
+    @tweets = Tweet.all
+    @replies = Reply.all
+    @users = User.order(@tweets.count :desc) 
+
+
   end
 
   def destroy
+    @tweet = Tweet.find(params[:id])
+
+    if current_user.admin?
+      @tweet.destroy
+      redirect_back(fallback_location: root_path)
+    end
   end
 end
