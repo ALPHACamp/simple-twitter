33b2fbbf4070342c8ab427e7819c10bc28ed15ad
Yipeechen <yipeechen0418@gmail.com>
2018-02-09 01:44:08 +0800

    add admin tweet index

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -3,6 +3,8 @@ class Admin::TweetsController < Admin::BaseController
   before_action :authenticate_admin
   
   def index
+    @tweets = Tweet.order(created_at: :desc)
+    @users = User.all
   end
 
   def destroy
