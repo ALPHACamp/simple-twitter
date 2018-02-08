5cd31aebcb12f35d5e9d7f6e1be2c4ed44767e99
Yipeechen <yipeechen0418@gmail.com>
2018-02-08 16:48:33 +0800

    setup relation and add tweets#like & tweets#unlike

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -6,4 +6,8 @@ class Tweet < ApplicationRecord
 
   has_many :replies, dependent: :destroy
   has_many :replied_users, through: :replies, source: :user
+
+  has_many :likes, dependent: :destroy
+  has_many :liked_users, through: :likes, source: :user
+  
 end
