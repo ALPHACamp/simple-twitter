9b5781eff82f0fa7127be003d4bc5e233c0ca969
spy <sp831031@gmail.com>
2018-02-08 19:43:20 +0800

    make user actully can't follow self

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -1,15 +1,17 @@
 class FollowshipsController < ApplicationController
   def create
+    if current_user != User.find(params[:following_id])
 
-    @followship = current_user.followships.build(following_id: params[:following_id])
-    
-    
-    if @followship.save 
-      flash[:notice] = "Successfully followed"
-    else
-      flash[:alert] = @followship.errors.full_messages.to_sentence 
+      @followship = current_user.followships.build(following_id: params[:following_id])
+      
+      
+      if @followship.save 
+        flash[:notice] = "Successfully followed"
+      else
+        flash[:alert] = @followship.errors.full_messages.to_sentence 
+      end
+      redirect_back(fallback_location: root_path)
     end
-    redirect_back(fallback_location: root_path)
   end
 
   def destroy
