e804cf491a55b4c7f4285cec557098fd1c4cd431
Wayne <wayne0042@gmail.com>
2018-02-08 17:11:03 +0800

    finished user's like html

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,8 @@ class UsersController < ApplicationController
   end
 
   def likes
-    @likes # 基於測試規格，必須講定變數名稱
+    @likes = @user.likes # 基於測試規格，必須講定變數名稱
+    @tweets = @user.liked_tweets
   end
 
   private
