58d2dda96c59994a8d3b7c20597a3562a114ddca
Brian <finder.chen@gmail.com>
2018-02-09 02:19:23 +0800

    modify user followings, followers, likes order

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,15 +19,15 @@ class UsersController < ApplicationController
   end
 
   def followings
-    @followings = @user.followings# 基於測試規格，必須講定變數名稱
+    @followings = @user.followings.order("followships.created_at desc")# 基於測試規格，必須講定變數名稱
   end
 
   def followers
-    @followers= @user.followers # 基於測試規格，必須講定變數名稱
+    @followers = @user.followers.order("followships.created_at desc") # 基於測試規格，必須講定變數名稱
   end
 
   def likes
-    @likes = @user.liked_tweets# 基於測試規格，必須講定變數名稱
+    @likes = @user.liked_tweets.order("likes.created_at desc")# 基於測試規格，必須講定變數名稱
   end
 
   private
