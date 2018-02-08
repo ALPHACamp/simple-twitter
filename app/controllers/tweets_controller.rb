19bb669d65f2ead01318a2f1a2e36bef09f44500
Kansetsu Kuo <kansetsu4@gmail.com>
2018-02-08 09:35:37 +0800

    Fix bug: tweet_spec.rb:47 by add created_at order to tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,7 +5,7 @@ class TweetsController < ApplicationController
     # @users # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
     @users = User.order('followers_count desc').limit(10)
     # @tweets = Tweet.order('created_at desc').page(params[:page]).per(10)
-    @tweets = Tweet.all
+    @tweets = Tweet.all.order('created_at desc')
     @tweet = Tweet.new
   end
 
