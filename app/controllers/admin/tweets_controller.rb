3c49ac7e8ecf154877c46bae82a0b832e9d18522
sapphirekuo <sapphirekuo@gmail.com>
2018-02-08 10:12:23 +0800

    add admin tweets page.

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -1,7 +1,12 @@
 class Admin::TweetsController < Admin::BaseController
   def index
+    @tweets = Tweet.all.order(created_at: :desc)
   end
 
   def destroy
+    @tweet = Tweet.find(params[:id])
+    @tweet.destroy
+    flash[:notice] = "Tweet was successfully deleted"
+    redirect_to admin_tweets_path
   end
 end
