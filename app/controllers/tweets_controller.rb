2f28420ee8aeede769a76c15d468548c8f14419f
binyaha <yahabin@gmail.com>
2018-02-08 21:26:29 +0800

    修正rspec 99 118

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -20,14 +20,14 @@ class TweetsController < ApplicationController
   def like
     tweet = Tweet.find(params[:id])
     tweet.likes.create!(user: current_user)
-    redirect_back(fallback_location: root_path)
+    redirect_to tweets_path
   end
 
   def unlike
     tweet = Tweet.find(params[:id])
     like = Like.where(user: current_user,tweet: tweet)
     like.destroy_all
-    redirect_back(fallback_location: root_path)
+    redirect_to tweets_path
   end
 
     private
