fee299c0dd0da385bdb5549f2548bd9e3b62ab82
penciler <eraser41@livemail.tw>
2018-02-08 23:43:27 +0800

    add create tweet function in root path

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,9 +4,14 @@ class TweetsController < ApplicationController
   def index
     @users=User.all # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
     @tweets=Tweet.all
+    @tweet=Tweet.new
   end
 
   def create
+    @user = current_user
+    @tweet = @user.tweets.build(tweet_params)
+    @tweet.save!
+    redirect_to root_path
   end
 
   def like
@@ -18,6 +23,12 @@ class TweetsController < ApplicationController
   def replies
     @tweet=Tweet.find(params[:id])
     @replies=Tweet.find(params[:id]).reply
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:description, :user_id)
   end
 
 end
