34f9521357adcb7f01cbeeebe1278f528708e6f4
penciler <eraser41@livemail.tw>
2018-02-08 20:53:43 +0800

    basic style for followings page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,42 +1,43 @@
 class UsersController < ApplicationController
-
+  before_action :set_user, only: [:edit, :update, :followings, :followers, :likes, :tweets]
 
 
 
   def tweets
-    @user = User.find(params[:id])
     @tweets = @user.tweets
   end
 
   def edit
-    @user=User.find(params[:id])
     if @user != current_user
       redirect_to root_path
     end
   end
 
   def update
-    @user=User.find(params[:id])
     @user.update(user_params)
     redirect_to tweets_user_path(@user)
   end
 
   def followings
-    @followings=User.find(params[:id]).followings # 基於測試規格，必須講定變數名稱
+    @followings=@user.followings # 基於測試規格，必須講定變數名稱
   end
 
   def followers
-    @followers=User.find(params[:id]).followers # 基於測試規格，必須講定變數名稱
+    @followers=@user.followers # 基於測試規格，必須講定變數名稱
   end
 
   def likes
-    @likes=User.find(params[:id]).likes # 基於測試規格，必須講定變數名稱
+    @likes=@user.likes # 基於測試規格，必須講定變數名稱
   end
 
   private
 
   def user_params
     params.require(:user).permit(:name, :avatar)
+  end
+
+  def set_user
+    @user=User.find(params[:id])
   end
 
 end
