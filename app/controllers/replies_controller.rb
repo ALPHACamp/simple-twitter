3d013392a737eca6362263bb8e27b9751d9d9ff4
Brian <finder.chen@gmail.com>
2018-02-08 10:42:49 +0800

    add reply index and create action

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,9 +1,26 @@
 class RepliesController < ApplicationController
 
+  before_action :get_tweet, only: [:index, :create]
   def index
+    @replies = @tweet.replies
+    @reply = Reply.new
+    @user = @tweet.user
   end
 
   def create
+    @reply = @tweet.replies.build(reply_params)
+    @reply.user = current_user
+    @reply.save!
+    redirect_to tweet_replies_path(@tweet)
   end
 
+  private
+
+  def get_tweet
+    @tweet = Tweet.find(params[:tweet_id])
+  end
+
+  def reply_params
+    params.require(:reply).permit(:comment)
+  end
 end
