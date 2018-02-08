edd7823800723cb2873c1e5d0d4734d047c0c9a4
KC <weilihmen@gmail.com>
2018-02-08 12:44:20 +0800

    done user likes page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
   end
 
   def likes
-    @likes = @user.likes # 基於測試規格，必須講定變數名稱
+    @likes = @user.like_tweets # 基於測試規格，必須講定變數名稱
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -24,6 +24,9 @@ class User < ApplicationRecord
 
   has_many :tweets, dependent: :destroy
   has_many :replies, dependent: :destroy
+
+  has_many :likes, dependent: :destroy
+  has_many :like_tweets, through: :likes, source: :tweet
   def admin?
     self.role == "admin"
   end
