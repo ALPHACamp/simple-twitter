777d8e574d17ed11153a629873ab71bdbb65af7a
chunchaolee <chunchaolee@gmail.com>
2018-02-08 17:29:33 +0800

    (revise)added tweets_count to users table, and set counter_cache on tweet model

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,7 +2,7 @@ class Tweet < ApplicationRecord
   validates_length_of :description, maximum: 140, :message => "超過字數限制"
 
   # 設定關聯
-  belongs_to :user
+  belongs_to :user, counter_cache: true
   has_many :replies, dependent: :destroy
   has_many :likes, dependent: :destroy
 
