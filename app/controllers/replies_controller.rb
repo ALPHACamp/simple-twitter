3d05b6abec23f58a944ca97fc52e2fd1317222d0
annie chiang <annie100patty@gmail.com>
2018-02-09 13:13:01 +0800

    edit replies_controller based on testing fails

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -12,12 +12,8 @@ class RepliesController < ApplicationController
     if @reply.save
       redirect_back(fallback_location: root_path)
     else
-      if @reply.errors.present?
-        @reply.errors.each do |error, message|
-          flash[:alert] = "#{error.capitalize} #{message}"
-        end
-      else
-        flash[:alert] = "Failed to reply"
+      @reply.errors.each do |error, message|
+        flash[:alert] = "#{error.capitalize} #{message}"
       end
       redirect_to replies_path
     end
