70ce473065df88ab8853a6a92ff144d4341beda4
Wayne <wayne0042@gmail.com>
2018-02-09 10:20:46 +0800

    finished admin can see all tweets

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -1,9 +1,7 @@
 class Admin::TweetsController < Admin::BaseController
 
-  before_action :authenticate_user!
-  before_action :authenticate_admin
-  
   def index
+    @tweets = Tweet.all
   end
 
   def destroy
