fa8f194113d88adcb2537cef8ce3d96aa17caf3f
francishuang <francis3925@gmail.com>
2018-02-09 11:34:20 +0800

    finish replies#index page and enable users to reply tweets

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -7,6 +7,19 @@ class RepliesController < ApplicationController
   end
 
   def create
+    @tweet = Tweet.find(params[:tweet_id])
+    @reply = @tweet.replies.build(reply_params)
+    @reply.user = current_user
+    @reply.save!
+    flash.notice = "回覆推播成功！"
+    redirect_to tweet_replies_path(@tweet)
   end
+
+  private
+
+  def reply_params
+    params.require(:reply).permit(:comment)
+  end
+
 
 end
