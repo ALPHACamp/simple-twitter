a51253ba6e7aead7ba04f25748eeaf8fea5cb79d
binyaha <yahabin@gmail.com>
2018-02-09 22:20:30 +0800

    交卷 稍微優化程式碼 rsepc有一個錯誤 但功能ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,15 @@
 class UsersController < ApplicationController
 
+  before_action :find_user, only: [:tweets, :edit, :update, :followings, :followers, :likes]
+
   def tweets
-    @user = User.find(params[:id])
+    
     @tweets = @user.tweets.order('created_at DESC')
 
   end
 
   def edit
-    @user = User.find(params[:id])
+    
     if current_user != @user
       redirect_to tweets_user_path(@user)
       flash[:alert] = "你不是本人"
@@ -15,28 +17,28 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
+    
     @user.update(user_params)
     @user.save
     redirect_to tweets_user_path(@user)
   end
 
   def followings
-    @user = User.find(params[:id])
+    
     #@followings = @user.followings  # 基於測試規格，必須講定變數名稱
     @followings = User.where('followships.user_id = ?', @user.id).joins(:inverse_followships).merge(Followship.order(created_at: :desc))
   
   end
 
   def followers
-    @user = User.find(params[:id])
+    
     #@followers = @user.followers # 基於測試規格，必須講定變數名稱
     @followers = User.where('followships.following_id = ?', @user.id).joins(:followships).merge(Followship.order(created_at: :desc))
 
   end
 
   def likes
-    @user = User.find(params[:id])
+    
     #@likes = @user.like_tweets  # 基於測試規格，必須講定變數名稱
     @likes = Tweet.where('likes.user_id = ?', @user.id).joins(:likes).merge(Like.order(created_at: :desc))
   end
@@ -46,5 +48,8 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :avatar, :introduction)
   end
 
+  def find_user
+    @user = User.find(params[:id])
+  end
 
 end
