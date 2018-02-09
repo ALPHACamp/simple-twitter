1e765fe4d37091f28215c9808cb20e11b73370eb
sakuraki <a1989198@hotmail.com>
2018-02-09 12:47:57 +0800

    setup admin controller

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -1,7 +1,22 @@
 class Admin::TweetsController < Admin::BaseController
   def index
+     #@tweets = Tweet.page(params[:page]).per(10)
+    @tweets = Tweet.all
   end
+  
+  before_action :set_tweet, only:  [:show, :edit, :update, :destroy]
 
   def destroy
+     @tweet.destroy
+     redirect_to admin_tweets_path
+     flash[:alert] = "tweet was deleted"
+  end
+  
+  
+  
+  private
+  
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
   end
 end
