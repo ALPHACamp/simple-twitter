0cfe98d0f6fb81276ad7c0dcb961b3eb160ef062
beranemo <beranemo@gmail.com>
2018-02-09 15:03:24 +0800

    like/unlike 防呆功能

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -7,5 +7,9 @@ class Tweet < ApplicationRecord
   
   has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
-
+  
+  def is_liked?(user)
+    self.liked_users.include?(user)
+  end
+  
 end
