944327709f9ad4b2740161afdb99a81e0dc01e69
francishuang <francis3925@gmail.com>
2018-02-09 21:32:45 +0800

    enable users to check their followers on users/followers page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,7 +30,8 @@ class UsersController < ApplicationController
   end
 
   def followers
-    @followers 
+    @user = User.find(params[:id])
+    @followers = @user.followers.order(created_at: :desc)
     # 基於測試規格，必須講定變數名稱
   end
 
