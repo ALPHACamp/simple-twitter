b5c73e5f3860ba15422a1b78086324697238eb13
Brian <finder.chen@gmail.com>
2018-02-10 00:07:48 +0800

    try to fix Followship #create when user1 wants to follow user2 can not follow self

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -1,10 +1,8 @@
 class FollowshipsController < ApplicationController
   def create
-    @followship = current_user.followships.build(following_id: params[:following_id])
-    if @followship.user_id == @followships.following_id
-      flash[:alert] = "can't follow yourself"
-      redirect_back(fallback_location: root_path)
-    else
+    if current_user != User.find(params[:following_id])
+      @followship = current_user.followships.build(following_id: params[:following_id])
+
       if @followship.save
         flash[:notice] = "Successfully followed"
         redirect_back(fallback_location: root_path)
