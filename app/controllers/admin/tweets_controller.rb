32a382109bb2155d980edff8ab45ba87711cd10e
Theo Chen <tingyouchen@gmail.com>
2018-02-09 20:28:33 +0800

    modify admin tweets user contoller

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -1,7 +1,11 @@
 class Admin::TweetsController < Admin::BaseController
   def index
+    @tweets = Tweet.all.order(replies_count: :desc)
   end
 
   def destroy
+    @tweet = Tweet.find(params[:id])
+    @tweet.destroy
+    redirect_to(admin_root_path)
   end
 end
