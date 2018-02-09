da4c635c0f6e6ed8e12f4d234f8924bb1f9ecac2
SHLIPH <wontstoplearn@gmail.com>
2018-02-09 20:44:58 +0800

    setup relation among user, tweet and like

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,4 +2,7 @@ class Tweet < ApplicationRecord
   validates_length_of :description, maximum: 140
   belongs_to :user
   has_many :replies
+
+  has_many :likes, dependent: :destroy
+  has_many :liked_tweets, through: :likes, source: :tweet
 end
