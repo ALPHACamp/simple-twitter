fb3066e74ff73c2463675b5cb5b0c6b8ee621122
David <biki3507@gmail.com>
2018-02-10 01:55:48 +0800

    3 - prepare all tweet model & data
    3.使用者能瀏覽所有的推播 (tweet)

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,6 +2,9 @@ class TweetsController < ApplicationController
 
   def index
     @users # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
+    #3.使用者能瀏覽所有的推播 (tweet)
+    allTweet = tweet.all
+    
   end
 
   def create
