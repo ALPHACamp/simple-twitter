d351594193639a05e1d418d8f8246ceaae92f5a0
Theo Chen <tingyouchen@gmail.com>
2018-02-09 19:47:39 +0800

    add followship

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -1,7 +1,17 @@
 class FollowshipsController < ApplicationController
   def create
+    @followship = current_user.followships.build(following_id: params[:following_id])
+
+    if @followship.save
+      redirect_back(fallback_location: root_path)
+    else
+      redirect_back(fallback_location: root_path)
+    end
   end
 
   def destroy
+    @followship = current_user.followships.where(following_id: params[:id]).first
+    @followship.destroy
+    redirect_back(fallback_location: root_path) 
   end
 end
