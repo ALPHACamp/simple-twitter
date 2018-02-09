72f8b61efb0ea035103128a48325714343273445
anna770822 <anna770822@hotmail.com>
2018-02-09 21:07:10 +0800

    desc order for follower and followings

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,18 +19,18 @@ class UsersController < ApplicationController
   def followings
     @user = User.find(params[:id])
     @tweets = Tweet.where(user_id: @user)
-    @followings = @user.followings.order(created_at: :desc)# 基於測試規格，必須講定變數名稱
+    @followings = @user.followings.order('followships.created_at DESC')# 基於測試規格，必須講定變數名稱
   end
 
   def followers
     @user = User.find(params[:id])
     @tweets = Tweet.where(user_id: @user)
-    @followers = @user.followers.order(created_at: :desc) # 基於測試規格，必須講定變數名稱
+    @followers = @user.followers.order('followships.created_at DESC') # 基於測試規格，必須講定變數名稱
   end
 
   def likes
     @user = User.find(params[:id])
-    @likes = @user.liked_tweets.all # 基於測試規格，必須講定變數名稱
+    @likes = @user.liked_tweets.order('likes.created_at DESC') # 基於測試規格，必須講定變數名稱
     @tweets = Tweet.where(user_id: @user)
     @user.likes_count = @user.likes.size
   end
