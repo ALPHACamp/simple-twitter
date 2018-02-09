1b67060eb98d381d529466bb1c5d3fb987499f6d
SHLIPH <wontstoplearn@gmail.com>
2018-02-09 14:59:10 +0800

    setup routes for followings, and add followings view, followings action, then adjust user_filws folloings to link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:edit, :update, :tweets]
+  before_action :set_user, only: [:edit, :update, :tweets,:followings]
 
   def tweets
     @tweets = @user.tweets
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
   end
 
   def followings
-    @followings # 基於測試規格，必須講定變數名稱
+    @followings = @user.followings# 基於測試規格，必須講定變數名稱
   end
 
   def followers
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   resources :users do
     member do
       get :tweets
+      get :followings
     end
   end
   root "tweets#index"
