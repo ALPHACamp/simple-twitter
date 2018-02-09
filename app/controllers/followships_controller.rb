2dc757f6e14a350d8a54eae6261d9022e854aa80
Wayne <wayne0042@gmail.com>
2018-02-09 17:28:13 +0800

    fix followships#create, can't follow self

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -1,13 +1,17 @@
 class FollowshipsController < ApplicationController
   def create
     @followship = current_user.followships.new(following_id: params[:following_id])
-
-    if @followship.save
-      flash[:notice] = "Successfully followed"
+    if @followship.user_id == @followship.following_id
+      flash[:alert] = "can't follow yourself."
       redirect_back(fallback_location: root_path)
     else
-      flash[:alert] = @followship.errors.full_messages.to_sentence
-      redirect_back(fallback_location: root_path)
+      if @followship.save
+        flash[:notice] = "Successfully followed"
+        redirect_back(fallback_location: root_path)
+      else
+        flash[:alert] = @followship.errors.full_messages.to_sentence
+        redirect_back(fallback_location: root_path)
+      end
     end
   end
 
