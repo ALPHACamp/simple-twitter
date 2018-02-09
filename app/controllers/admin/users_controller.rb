3bb00d00d92251d2aa25d9c1602efcba3feeae5f
Adamlin0708 <kuso12610@gmail.com>
2018-02-09 13:29:33 +0800

    admin user lists order by tweets amounts

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,6 @@
 class Admin::UsersController < Admin::BaseController
   def index
-    @users = User.all.page(params[:page]).per(10).order(created_at: :desc)            
+    @users = User.left_outer_joins(:tweets).select('users.*, COUNT(tweets.id) AS tweets_count').group('users.id').page(params[:page]).per(10).order('COUNT(tweets.id) DESC')
     @count = User.all.count
   end
 end
