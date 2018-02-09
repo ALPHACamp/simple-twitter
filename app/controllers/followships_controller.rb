9c09c069306925d8155bd1a8ee2e07619b732b4a
Tony Lin <tonylin1208@gmail.com>
2018-02-09 08:42:31 +0800

    Try user can't follow himself

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -1,18 +1,14 @@
 class FollowshipsController < ApplicationController
   def create
-    @following = Following.find(params[:following_id])
-    unless @following == current_user
-      @followship = @following.followships.build(following_id: params[:following_id])
+    @followship = @following.followships.build(following_id: params[:following_id])
+    
+    if @followship.save
+      flash[:notice] = "Successfully followed #{@followship.following.name}"
+      redirect_back(fallback_location: root_path)
+    else
+      flash[:alert] = @followship.errors.full_messages.to_sentence
+      redirect_back(fallback_location: root_path)
     end
-    # unless @followship.user_id == current_user
-      if @followship.save
-        flash[:notice] = "Successfully followed #{@followship.following.name}"
-        redirect_back(fallback_location: root_path)
-      else
-        flash[:alert] = @followship.errors.full_messages.to_sentence
-        redirect_back(fallback_location: root_path)
-      end
-    # end
   end
 
   def destroy
