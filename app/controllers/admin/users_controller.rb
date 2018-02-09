420639f25f36357327f819b6213c886ff696281e
binyaha <yahabin@gmail.com>
2018-02-09 19:59:04 +0800

    test

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,6 +2,7 @@ class Admin::UsersController < Admin::BaseController
   
   def index
     
-    @users = User.left_joins(:tweets).group(:id).order('COUNT(tweets.id) DESC')
+    #@users = User.left_joins(:tweets).group(:id).order('COUNT(tweets.id) DESC')
+    @users = User.select('users.*, count(tweets.id) as tweets_count').left_joins(:tweets).group(:id).order('tweets_count DESC')
   end
 end
