612c1dedec4c28f32b6cf707ac1c9ccd281031a6
Theo Chen <tingyouchen@gmail.com>
2018-02-09 19:55:04 +0800

    add user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,46 @@
 class UsersController < ApplicationController
 
+  before_action :set_user
+
   def tweets
+    @tweets = @user.tweets.order(created_at: :desc)
   end
 
   def edit
+    unless @user === current_user
+      redirect_to root_path 
+    end
   end
 
   def update
+    if @user.update(user_params)
+      redirect_to tweets_user_path(current_user)
+    else
+      render :edit
+    end
   end
 
   def followings
-    @followings # 基於測試規格，必須講定變數名稱
+    @followings = @user.followings.all.order(created_at: :desc)
   end
 
   def followers
-    @followers # 基於測試規格，必須講定變數名稱
+    @followers = @user.followers.all.order(created_at: :desc)
   end
 
   def likes
-    @likes # 基於測試規格，必須講定變數名稱
+    @tweets = @user.liked_tweets
+    @likes = @tweets.all.order(created_at: :desc)
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :avatar)
   end
 
 end
