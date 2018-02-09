0de340ec2cbc7eee4443169b51ed4e7b76773205
SHLIPH <wontstoplearn@gmail.com>
2018-02-09 23:29:11 +0800

    get tweets, following, follower and like with order

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :tweets, :followings, :followers, :likes]
 
   def tweets
-    @tweets = @user.tweets
+    @tweets = @user.tweets.order(created_at: :desc)
   end
 
   def edit
@@ -24,15 +24,15 @@ class UsersController < ApplicationController
   end
 
   def followings
-    @followings = @user.followings# 基於測試規格，必須講定變數名稱
+    @followings = @user.followings.order(created_at: :desc)# 基於測試規格，必須講定變數名稱
   end
 
   def followers
-    @followers = @user.followers # 基於測試規格，必須講定變數名稱
+    @followers = @user.followers.order(created_at: :desc) # 基於測試規格，必須講定變數名稱
   end
 
   def likes
-    @likes = @user.liked_tweets # 基於測試規格，必須講定變數名稱
+    @likes = @user.liked_tweets.order(created_at: :desc) # 基於測試規格，必須講定變數名稱
   end
 
   def admin?
