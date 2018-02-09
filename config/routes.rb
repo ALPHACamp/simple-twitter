d385e76b44c2b988717adb37d46f467158b10d37
Theo Chen <tingyouchen@gmail.com>
2018-02-09 16:23:29 +0800

    set admin routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
 
   root "tweets#index"
 
+  namespace :admin do 
+    resources :tweets, only: [:index, :destroy]
+    resources :users, only: [:index]
+    root "tweets#index"
+  end
+
   resources :tweets, only: [:index, :create] do
     resources :replies, only: [:index, :create]
     member do
@@ -11,4 +17,22 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :tweets, only: [:index, :create] do
+    resources :replies, only: [:index, :create]
+    member do
+      post :like
+      post :unlike
+    end
+  end
+
+  resources :users, only: [:edit, :update] do 
+    member do 
+      get :tweets
+      get :followings
+      get :followers
+      get :likes
+    end
+  end
+
+  resources :followships, only: [:create, :destroy]
 end
