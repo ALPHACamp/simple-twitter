6a45be0e929f3f3989856ae4a67aa18526039782
victorloo <ila9970@gmail.com>
2018-02-09 20:23:25 +0800

    add create action for followship record

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -2,6 +2,16 @@ class FollowshipsController < ApplicationController
   
   #建立追蹤
   def create
+    @followship = current_user.followships.build(following_id: params[:following_id])
+
+    if @followship.save
+      flash[:notice] = "Successfully followed"
+      redirect_back(fallback_location: root_path)
+    else
+      flash[:alert] = @followship.errors.full_messages.to_sentence
+      redirect_back(fallback_location: root_path)
+    end
+  end
   end
 
   #取消追蹤
