89fa98840f7cdc45aa6b7b9566188a9dd0166c0a
victorloo <ila9970@gmail.com>
2018-02-09 20:19:24 +0800

    tweet index has correct setting

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,8 +4,8 @@ class TweetsController < ApplicationController
     @tweets = Tweet.all
     @tweet = Tweet.new
     @reply = Reply.new
-    @users = User.all
-    #@users # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
+    # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
+    @users = User.order(followers_count: :desc).limit(10)
   end
 
   def create
