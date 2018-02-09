9f580488ffe7219b9e109496ea484c037e69d940
beranemo <beranemo@gmail.com>
2018-02-09 12:48:33 +0800

    使用者可以在 tweets 的 replies 頁面回覆

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,13 +1,24 @@
 class RepliesController < ApplicationController
 
   def index
-    @tweet = Tweet.find(params[:tweet_id]);
-    @replies = Tweet.find(params[:tweet_id]).replies
+    @tweet = Tweet.find(params[:tweet_id])
+    @replies = Tweet.find(params[:tweet_id]).replies.order(created_at: :asc)
     @user = Tweet.find(params[:tweet_id]).user
     @reply = Reply.new
   end
 
   def create
+    @tweet = Tweet.find(params[:tweet_id])
+    @reply = @tweet.replies.build(reply_params)
+    @reply.user = current_user
+    @reply.save!
+    redirect_to tweet_replies_path(@tweet.id)
+  end
+  
+  private
+  
+  def reply_params
+    params.require(:x).permit(:comment)
   end
 
 end
