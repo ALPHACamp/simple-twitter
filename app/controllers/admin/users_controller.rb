cdca26e83b8661c80b751e1283b2708ac7ff23ef
anna770822 <anna770822@hotmail.com>
2018-02-09 18:56:42 +0800

    add left joins to admin users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,7 @@
 class Admin::UsersController < Admin::BaseController
   def index
 
-    @users = User.all
+    @users = User.left_outer_joins(:tweets).group(:id).order('COUNT(tweets.id) DESC')
     
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180201102838) do
+ActiveRecord::Schema.define(version: 20180209095212) do
 
   create_table "followships", force: :cascade do |t|
     t.integer "user_id"
