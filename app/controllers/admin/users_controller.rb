152e36831f33ab8023cb9b64cbdcde5b1648b4ce
beranemo <beranemo@gmail.com>
2018-02-09 19:21:59 +0800

    管理者能進入後者，看到 user 的資料，也可以刪除 user

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,14 @@
 class Admin::UsersController < Admin::BaseController
     
   def index
-    
+    @users = User.all
+  end
+  
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    redirect_to admin_users_path
+    flash[:alert] = "user was delete"
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root "tweets#index"
     resources :tweets, only: [:index, :destroy]
+    resources :users, only: [:index, :destroy]
   end
 
   # 追蹤／取消追蹤
