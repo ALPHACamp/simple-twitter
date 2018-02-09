0c17fd03a3aab1048f1daaec457fb066390c2722
Morrie <morriedog@hotmail.com>
2018-02-10 00:00:12 +0800

    tweet order by reply's created_at

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,8 +1,9 @@
 class TweetsController < ApplicationController
-
+# Home.member.includes(:members).order("members.born asc")
   def index
     @users = User.order(followers_count: :desc).limit(10) # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
-    @tweets = Tweet.order(created_at: :desc).all
+    # @tweets = Tweet.order(created_at: :desc).all
+    @tweets = Tweet.includes(:replies).order("replies.created_at desc")
   end
 
   def create
