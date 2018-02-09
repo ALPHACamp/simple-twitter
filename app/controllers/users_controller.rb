a47aa5d3115bce5cd19903cb1f9bcfb2d369948a
Wayne <wayne0042@gmail.com>
2018-02-09 17:10:09 +0800

    fix user rspec conflict

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,11 +23,11 @@ class UsersController < ApplicationController
   end
 
   def followings
-    @followings = @user.followings.order(created_at: :desc) # 基於測試規格，必須講定變數名稱
+    @followings = @user.followings # 基於測試規格，必須講定變數名稱
   end
 
   def followers
-    @followers = @user.followers.order(created_at: :desc) # 基於測試規格，必須講定變數名稱
+    @followers = @user.followers# 基於測試規格，必須講定變數名稱
   end
 
   def likes
