1237ad85260200cf160bb598a17361f1e4b110f1
Brian <finder.chen@gmail.com>
2018-02-09 23:51:44 +0800

    try to fix Followship #create when user1 wants to follow user2 can not follow self

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -1,6 +1,6 @@
 class FollowshipsController < ApplicationController
   def create
-    if current_user.id != params[:following_id]
+    if current_user != User.find(params[:following_id])
       @followship = current_user.followships.build(following_id: params[:following_id])
 
       if @followship.save
