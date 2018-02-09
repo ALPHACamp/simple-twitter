9b3833bcaa68d5b4e3a3a3c8b9984211f8e3e703
Wayne <wayne0042@gmail.com>
2018-02-09 09:59:40 +0800

    finished user follower relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,6 +20,9 @@ class User < ApplicationRecord
   has_many :followships, dependent: :destroy
   has_many :followings, through: :followships
 
+  has_many :inverse_followships, class_name: "Followship", foreign_key: "following_id"
+  has_many :followers, through: :inverse_followships, source: :user
+
   def admin?
     self.role == "admin"
   end
