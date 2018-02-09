8d2510d1bbf8089b3498e2df2a24141493168d27
KC <weilihmen@gmail.com>
2018-02-09 09:45:11 +0800

    fix

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -2,7 +2,6 @@ class FollowshipsController < ApplicationController
 	def create
     @followship = current_user.followships.new(following_id: params[:following_id])
     @be_followed = User.find(params[:following_id])
-    @be_followed.followers_count!
     @followship.save
     redirect_back(fallback_location: tweets_path)
   end
@@ -10,7 +9,6 @@ class FollowshipsController < ApplicationController
     @followship = current_user.followships.where(following_id: params[:id])
     @followship.destroy_all
     @be_followed = User.find(params[:id])
-    @be_followed.followers_count!
     redirect_back(fallback_location: tweets_path)
   end
 end
