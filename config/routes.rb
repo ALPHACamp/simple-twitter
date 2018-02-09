41c37190c2e116433e4cd06a62c4d64cf336bef0
Theo Chen <tingyouchen@gmail.com>
2018-02-09 15:52:27 +0800

    add tweet index

--- config/routes.rb
+++ config/routes.rb
@@ -3,41 +3,12 @@ Rails.application.routes.draw do
 
   root "tweets#index"
 
-  resources :tweets
+  resources :tweets, only: [:index, :create] do
+    resources :replies, only: [:index, :create]
+    member do
+      post :like
+      post :unlike
+    end
+  end
 
 end
-
-# Rails.application.routes.draw do
-#   devise_for :users
-
-#   root "restaurants#index" 
-
-#   resources :categories, only: :show
-  
-#   resources :users, only: [:index, :show, :edit, :update]
-
-#   resources :followships, only: [:create, :destroy]
-  
-#   namespace :admin do
-#     resources :restaurants
-#     resources :categories
-#     root "restaurants#index"
-#   end
-
-#   resources :restaurants, only: [:index, :show] do
-#     resources :comments, only: [:create, :destroy]
-
-#     collection do
-#       get :feeds
-#     end
-
-#     member do
-#       get :dashboard
-#     end
-
-#     member do
-#       post :favorite
-#       post :unfavorite
-#     end
-#   end  
-# end
