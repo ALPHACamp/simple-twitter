592da233f327b1646d2df78819a6f50719f34aae
annie chiang <annie100patty@gmail.com>
2018-02-09 13:21:47 +0800

    update followship_controller - user can't follow self

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -1,10 +1,16 @@
 class FollowshipsController < ApplicationController
   def create
-    @followship = current_user.followships.build(following_id: params[:following_id])
-    if @followship.save      
-      redirect_back(fallback_location: root_path)
+    @user = User.find(params[:following_id])
+    if current_user != @user
+      @followship = current_user.followships.build(following_id: params[:following_id])
+      if @followship.save      
+        redirect_back(fallback_location: root_path)
+      else
+        flash[:alert] = @followship.errors.full_messages.to_sentence
+        redirect_back(fallback_location: root_path)
+      end
     else
-      flash[:alert] = @followship.errors.full_messages.to_sentence
+      flash[:alert] = "You can't follow yourself!"
       redirect_back(fallback_location: root_path)
     end
   end
