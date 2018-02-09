b34dd5fb5d9ffb7322dbba912b6004296771df84
francishuang <francis3925@gmail.com>
2018-02-10 00:27:06 +0800

    fix routes and users/followings layout

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -12,20 +12,20 @@ class TweetsController < ApplicationController
     @tweet.user = current_user
     @tweet.save!
     flash.notice = "新增推播成功！"
-    redirect_to root_path
+    redirect_to tweets_path
   end
 
   def like
     @tweet = Tweet.find(params[:id])
     @tweet.likes.create!(user: current_user)
-    redirect_back fallback_location: root_path
+    redirect_back fallback_location: tweets_path
   end
 
   def unlike
     @tweet = Tweet.find(params[:id])
     likes = Like.where(tweet: @tweet, user: current_user)
     likes.destroy_all
-    redirect_back fallback_location: root_path
+    redirect_back fallback_location: tweets_path
   end
 
   private
