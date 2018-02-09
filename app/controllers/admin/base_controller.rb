08f8eb7a9f70c542bc323e04ce63c2b7ae16fabf
Theo Chen <tingyouchen@gmail.com>
2018-02-09 20:27:03 +0800

    modify admin base contoller

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,3 +1,12 @@
 class Admin::BaseController < ApplicationController
+  before_action :authenticate_admin
 
+  private
+  
+  def authenticate_admin
+    unless current_user.admin?
+      flash[:alert] = "admin only"
+      redirect_to root_path
+    end
+  end
 end
