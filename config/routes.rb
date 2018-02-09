03bdd5822e45d4bf5d9c5f899f59fdca3e2bbf22
David <biki3507@gmail.com>
2018-02-10 01:46:40 +0800

    add all routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,26 @@ Rails.application.routes.draw do
   # 請依照專案指定規格來設定路由
   root 'tweets#index'
   resources :users, only: [:index, :show, :edit, :update]
+  resources :tweets, only: [:index, :create, :like, :unlike] do
+    member do
+      post :like
+    end
+  end
+
+  resources :replies, only: [:index, :create]
+  resources :users, only: [:index, :show] do
+    collection do
+      # 瀏覽所有餐廳的最新動態
+      # GET users/tweets
+      get :tweets
+      get :followings
+      get :followers
+    end
+    member do
+      get :like
+      #post :unfavorite
+    end
+  end
+  resources :followships, only: [:create, :destroy]
 end
+
