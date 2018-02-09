ae9b16fe25d7ea71851e10c1e4918b30129ab92f
MarkWeng <bskin0330@gmail.com>
2018-02-09 23:02:26 +0800

    fix liked botton

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,7 @@
 class TweetsController < ApplicationController
   def index
     @tweet = Tweet.new
-    @tweets = Tweet.all.order(created_at: :desc).limit(10)
+    @tweets = Tweet.all.order(created_at: :desc)
     @users = User.all.order(created_at: :desc).limit(10)
     # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
   end
@@ -26,8 +26,8 @@ class TweetsController < ApplicationController
   end
 
   def unlike
-    @unlike = current_user.likes.where(tweet_id: params[:id]).first
-    @unlike.destroy
+    @like = current_user.likes.where(tweet_id: params[:id]).first
+    @like.destroy
     flash[:notice] = "unliked"
     redirect_back(fallback_location: root_path)
   end
