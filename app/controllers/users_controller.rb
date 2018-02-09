a3ad1be800cbfb5c2608084348d9ffbab71e7c6c
wusunghan   <diadora1166@gmail.com>
2018-02-09 21:26:23 +0800

    Following：該使用者的關注清單，排序依照追蹤紀錄成立的時間，愈新的在愈前面 Follower：該使用者的跟隨者清單，排序依照追蹤紀錄成立的時間，愈新的在愈前面

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,36 +1,35 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:tweets, :edit, :update]
+  before_action :set_user, only: [ :tweets,:edit, :update, :followings, :followers]
   def tweets
     @tweets = Tweet.all
-
   end
 
   def edit
-     @user = current_user
   end
 
   def update
-    @user = current_user
+
     if @user.update(user_params)
       redirect_to root_path
     else
       render :edit
     end
   end
-
   def followings
-    @followings # 基於測試規格，必須講定變數名稱
+    @followings = @user.followings.order("followships.created_at desc") # 基於測試規格，必須講定變數名稱
   end
 
   def followers
-    @followers # 基於測試規格，必須講定變數名稱
+    @followers = @user.followers.order("followships.created_at desc") # 基於測試規格，必須講定變數名稱
   end
 
   def likes
     @likes # 基於測試規格，必須講定變數名稱
   end
 
-  private
+
+
+private
   def set_user
     @user = User.find(params[:id])
   end
@@ -38,5 +37,4 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :introduction, :avatar)
   end
-
 end
