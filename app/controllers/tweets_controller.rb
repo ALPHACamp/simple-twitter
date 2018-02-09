76d7770d073eb265d6ae08a27b8c38222dcceb69
LanceRabbit <lancerabbit@gmail.com>
2018-02-09 18:06:08 +0800

    after click like/unlike action will redirect to that tweet

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -21,13 +21,13 @@ class TweetsController < ApplicationController
 
   def like
     @tweet.likes.create!(user: current_user)
-    redirect_back(fallback_location: root_path)  # 導回上一頁
+    redirect_to tweets_path
   end
 
   def unlike
     likes = Like.where(tweet: @tweet, user: current_user)
     likes.destroy_all
-    redirect_back(fallback_location: root_path)
+    redirect_to tweets_path
   end
 
   private
