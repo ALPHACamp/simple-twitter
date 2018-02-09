863a64fc14c882dacc31bbd207cb6f26393e272f
Zarek <simen1127@gmail.com>
2018-02-09 21:25:44 +0800

    edit partial view

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -1,8 +1,12 @@
 class Admin::TweetsController < Admin::BaseController
   def index
-    
+    @tweets = Tweet.all
   end
 
   def destroy
+    @tweet = Tweet.find(params[:id])
+    @tweet.destroy
+    flash[:notice] = "tweet was deleted!"
+    redirect_to admin_root_path  
   end
 end
