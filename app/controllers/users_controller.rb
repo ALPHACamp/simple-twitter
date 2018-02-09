048532bdf9fd08c8f4edbc20fe885ff3fe9d1253
kokotsai <kokotsai@gmail.com>
2018-02-09 15:57:43 +0800

    edit user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,22 @@
 class UsersController < ApplicationController
-
+  before_action :set_user, only: [:edit, :update]
   def tweets
     @user = User.find(params[:id])
     @tweets = @user.tweets.all.order(created_at: :desc)
   end
 
-  def edit
-  end
 
   def update
+
+       if @user.update(user_params)
+      redirect_to root_path, notice: "Profile Updated "
+    else
+      flash[:alert] = @user.errors.full_messages.to_sentence
+
+      render :edit
+    end
   end
+
 
   def followings
     @followings # 基於測試規格，必須講定變數名稱
@@ -21,6 +28,17 @@ class UsersController < ApplicationController
 
   def likes
     @likes # 基於測試規格，必須講定變數名稱
+  end
+
+
+
+ private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :avatar)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,15 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  devise_for :users
-
-  # 請依照專案指定規格來設定路由
-
-  resources :tweets, only: [:index, :create]
-
-  resources :tweets, only: [:edit, :update] do
-    member do
-      get :tweets
-    end
-  end
-  
-  root "tweets#index"
-
-
-    namespace :admin do
-    resources :tweets, only: [:index , :destroy]
-    resources :users
-    root "tweets#index"
-
-  end
-
-end
+ Rails.application.routes.draw do
+   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+   devise_for :users
+ 
+   # 請依照專案指定規格來設定路由
+   namespace :admin do
+     resources :tweets, only: [:index, :create]
+ 
+     root "tweets#index"
+   end
+ 
+   resources :tweets, only: [:index, :destroy]
+   resources :users, only: [:edit, :update]
+   root "tweets#index"
+ end
