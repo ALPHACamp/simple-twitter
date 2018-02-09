f8b9576cfe61cb1da3b7be47a099fa81363943d6
Theo Chen <tingyouchen@gmail.com>
2018-02-09 23:12:21 +0800

    fix follow self bug

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -2,7 +2,7 @@ class FollowshipsController < ApplicationController
   def create
     @followship = current_user.followships.build(following_id: params[:following_id])
 
-    if @followship.save
+    if current_user.id != params[:following_id].to_i and @followship.save
       redirect_back(fallback_location: root_path)
     else
       redirect_back(fallback_location: root_path)
