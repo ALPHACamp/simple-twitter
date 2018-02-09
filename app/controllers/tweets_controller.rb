baa63c973008498ba189147345721886a694b52a
SHLIPH <wontstoplearn@gmail.com>
2018-02-09 11:48:59 +0800

    update views and show all tweets at root page

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -7,7 +7,7 @@ class TweetsController < ApplicationController
     @user = current_user
     @users = User.all# 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
     @tweet = Tweet.new
-    @tweets = @user.tweets
+    @tweets = Tweet.all
   end
 
   def create
