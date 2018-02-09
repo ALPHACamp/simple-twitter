16d0232bd6eb166041fe526f88234168d38c94e4
Theo Chen <tingyouchen@gmail.com>
2018-02-09 23:28:55 +0800

    fix tweets ordered by created desc bug

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -1,6 +1,6 @@
 class Admin::TweetsController < Admin::BaseController
   def index
-    @tweets = Tweet.all.includes(:replies, :user).order(replies_count: :desc)
+    @tweets = Tweet.all.order(created_at: :desc)
   end
 
   def destroy
