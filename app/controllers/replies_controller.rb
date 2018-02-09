d0f9c36ab03484a5a2d956aa5191d05ef7d8559d
badbug wne <sweewen45@gmail.com>
2018-02-09 14:03:21 +0800

    add reply#index/create action and view. remove @ from partial:user_info for local variable

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,9 +1,25 @@
 class RepliesController < ApplicationController
-
+  before_action :set_tweet
   def index
+    # set tweet
+    @replies = @tweet.replies.order(created_at: :desc)
+    @reply = Reply.new
   end
 
   def create
+    # seet tweet
+    @reply = @tweet.replies.build(reply_params)
+    @reply.user = current_user
+    @reply.save!
+    redirect_to tweet_replies_path(@tweet)
   end
 
+  private
+  def set_tweet
+    @tweet = Tweet.find_by(params[:id])
+  end
+
+  def reply_params
+    params.require(:reply).permit(:comment)
+  end
 end
