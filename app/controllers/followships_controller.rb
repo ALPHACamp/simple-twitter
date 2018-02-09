5296b72476e02e912146cfc336b68e2af89322e8
badbug wne <sweewen45@gmail.com>
2018-02-09 08:45:03 +0800

    add count_follower in follow/unfollow actions. Use user.followers_count replace user.followers.size

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -2,6 +2,7 @@ class FollowshipsController < ApplicationController
   def create
     @followship = current_user.followships.build(following_id: params[:following_id])
     if @followship.save
+      User.find_by(id: params[:following_id]).count_followers
       redirect_back(fallback_location: root_path, notice: "您已將 #{User.find_by(id: params[:following_id]).name} 加入追蹤名單!" )   
     else
       flash[:alert] = @followship.errors.full_messages.to_sentence
@@ -12,6 +13,7 @@ class FollowshipsController < ApplicationController
   def destroy
     @followship = current_user.followships.where(following_id: params[:id])
     @followship.destroy_all
+    User.find_by(id: params[:id]).count_followers
     flash[:alert] = "已取消追蹤#{User.find_by(id: params[:id]).name} !"
     redirect_back(fallback_location: root_path)
   end
