fe99a673aae5c7e2a9ab0e2e90277d6e65df7a38
Chad <xmen11034@gmail.com>
2018-02-09 21:07:28 +0800

    followships controller fix minor bug

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -2,7 +2,7 @@ class FollowshipsController < ApplicationController
   def create
     @followship = current_user.followships.build(following_id: params[:following_id])
     if @followship.save
-      flash[:notice] = "成功追蹤#{User.find_by(id: params[:following_id]).name}！"
+      flash[:alert] = "成功追蹤#{User.find_by(id: params[:following_id]).name}！"
       redirect_back(fallback_location: tweets_path)
     else
       flash[:alert] = @followship.errors.full_messages.to_sentence
@@ -15,6 +15,5 @@ class FollowshipsController < ApplicationController
     @followship.destroy
     flash[:alert] = "取消追蹤#{User.find_by(id: params[:id]).name}！"
     redirect_back(fallback_location: tweets_path)
-  end
   end
 end
