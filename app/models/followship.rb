57c8849953fa7ed61d3b516dd803f84fb2f34d41
KC <weilihmen@gmail.com>
2018-02-09 09:41:45 +0800

    try following validation

--- app/models/followship.rb
+++ app/models/followship.rb
@@ -5,4 +5,12 @@ class Followship < ApplicationRecord
   belongs_to :following, class_name: "User" 
   #followship裡面有個following id，但沒有following這個model，所以要用class_name把這個id指向USER model
 
+  after_destroy :update_to_user
+  after_save :update_to_user
+ 
+  def update_to_user
+    self.following.followers_count!
+  end
+
+
 end
