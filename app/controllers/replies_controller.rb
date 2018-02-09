068738c95ba8148943cdc8c157d03b298daf5080
imwayneliao <imwayneliao@gmail.com>
2018-02-09 16:58:46 +0800

    user can reply a tweet on its reply_index page

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -4,9 +4,20 @@ class RepliesController < ApplicationController
     @tweet = Tweet.find(params[:tweet_id])
     @replies = @tweet.replies
     @user = @tweet.user
+    @reply = Reply.new
   end
 
   def create
+    @tweet = Tweet.find(params[:tweet_id])
+    @reply = @tweet.replies.build(reply_params)
+    @reply.user = current_user
+    @reply.save!
+    redirect_to tweet_replies_path(@tweet)
+  end
+
+  private
+  def reply_params
+    params.require(:reply).permit(:comment)
   end
 
 end
