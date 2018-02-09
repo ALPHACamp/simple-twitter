4379a5427947ec4d4fd811ed519d80b75a88ae10
beranemo <beranemo@gmail.com>
2018-02-09 12:19:09 +0800

    版面美化：tweet 首頁留言框

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -2,6 +2,7 @@ class RepliesController < ApplicationController
 
   def index
     @replies = Tweet.find(params[:tweet_id]).replies
+    @user = Tweet.find(params[:tweet_id]).user
   end
 
   def create
