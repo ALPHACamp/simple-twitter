df14adcc880c5051a922f304fe19ad5bb7c4cf40
Wayne <wayne0042@gmail.com>
2018-02-09 09:55:00 +0800

    finished following html

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
   end
 
   def followings
-    @followings # 基於測試規格，必須講定變數名稱
+    @followings = @user.followings.order(created_at: :desc) # 基於測試規格，必須講定變數名稱
   end
 
   def followers
