909c9b11458f13f299b666242f362a025d8ee6ee
LanceRabbit <lancerabbit@gmail.com>
2018-02-09 12:51:55 +0800

    show top 10 users on tweets page

--- app/models/followship.rb
+++ app/models/followship.rb
@@ -1,5 +1,8 @@
 class Followship < ApplicationRecord
   validates :following_id, uniqueness: { scope: :user_id }
   belongs_to :user
-  belongs_to :following, class_name: "User"
+  # counter_cache 可以自動回傳 size 方法結果，計算關聯物件的數量
+  # 必須在關聯 Model 上加一個欄位，慣例命名為 {table_name}_count
+  # 將counter指定給user的欄位followers_count
+  belongs_to :following, class_name: "User", counter_cache: :followers_count
 end
