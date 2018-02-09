02fe69e8a4793254ec1207c6cb582deac510aa56
francishuang <francis3925@gmail.com>
2018-02-09 14:26:43 +0800

    finish like/unlike function

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,7 +3,7 @@ class TweetsController < ApplicationController
   def index
     @popular_users = User.where.not(role: "admin").order(followers_count: :asc).limit(10)
     # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
-    @tweets = Tweet.order(created_at: :desc)
+    @tweets = Tweet.includes(:user).order(created_at: :desc)
     @tweet = Tweet.new
   end
 
@@ -16,9 +16,16 @@ class TweetsController < ApplicationController
   end
 
   def like
+    @tweet = Tweet.find(params[:id])
+    @tweet.likes.create!(user: current_user)
+    redirect_back fallback_location: root_path
   end
 
   def unlike
+    @tweet = Tweet.find(params[:id])
+    likes = Like.where(tweet: @tweet, user: current_user)
+    likes.destroy_all
+    redirect_back fallback_location: root_path
   end
 
   private
