c30eac87d917b480640b09776e8a10f9b7ec9b8f
Theo Chen <tingyouchen@gmail.com>
2018-02-09 14:06:55 +0800

    add layout set route set seed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,43 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
 
-  # 請依照專案指定規格來設定路由
+  root "tweets#index"
+
+  resources :tweets
 
 end
+
+# Rails.application.routes.draw do
+#   devise_for :users
+
+#   root "restaurants#index" 
+
+#   resources :categories, only: :show
+  
+#   resources :users, only: [:index, :show, :edit, :update]
+
+#   resources :followships, only: [:create, :destroy]
+  
+#   namespace :admin do
+#     resources :restaurants
+#     resources :categories
+#     root "restaurants#index"
+#   end
+
+#   resources :restaurants, only: [:index, :show] do
+#     resources :comments, only: [:create, :destroy]
+
+#     collection do
+#       get :feeds
+#     end
+
+#     member do
+#       get :dashboard
+#     end
+
+#     member do
+#       post :favorite
+#       post :unfavorite
+#     end
+#   end  
+# end
