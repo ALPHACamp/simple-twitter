6ed518ea80a3a4f43422e6d90195c5601c2b2798
beranemo <beranemo@gmail.com>
2018-02-10 01:32:19 +0800

    heroku fix

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -2,10 +2,9 @@ class FollowshipsController < ApplicationController
   
   def create
     @followship = current_user.followships.build(following_id: params[:following_id])
-    current_user.count_followers
-    
     if @followship.save
       flash[:notice] = "追蹤成功"
+      current_user.count_followers
       redirect_back(fallback_location: root_path)  # 導回上一頁
     else
       flash[:alert] = @followship.errors.full_messages.to_sentence
@@ -15,9 +14,10 @@ class FollowshipsController < ApplicationController
 
   def destroy
     @followship = current_user.followships.find_by(following_id: params[:id])
-    current_user.count_followers
+    
     if @followship
       @followship.destroy
+      current_user.count_followers
       flash[:notice] = "追蹤已取消"
       redirect_back(fallback_location: root_path)  # 導回上一頁
     end    
