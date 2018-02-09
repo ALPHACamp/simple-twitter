e05c4e8e98ac8e7baa7ca36b93b5a07e8da708c7
David <biki3507@gmail.com>
2018-02-10 03:23:45 +0800

    6 - add submit form  at index page for add tweet
    6.使用者能新增推播（限制在 140 字裡）

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,16 +6,26 @@ class TweetsController < ApplicationController
      # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
     #3.使用者能瀏覽所有的推播 (tweet)
     @allTweets = Tweet.all
-    
+    @user = User.find(current_user.id)
+    @tweet = Tweet.new
   end
 
   def create
+    @user = User.find(current_user.id)
+    @tweet = @user.tweets.build(tweet_params)
+    @tweet.user = current_user
+    @tweet.save!
+    redirect_back(fallback_location: root_path)
   end
 
   def like
   end
 
   def unlike
+  end
+
+  def tweet_params
+    params.require(:tweet).permit(:description)
   end
 
 end
