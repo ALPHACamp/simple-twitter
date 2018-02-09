bb6045b328a614b9b4a404b3373eb8f4a3c84e93
annie chiang <annie100patty@gmail.com>
2018-02-09 22:47:16 +0800

    update reply routes

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,21 +1,21 @@
 class RepliesController < ApplicationController
 
   def index
-    @tweet = Tweet.find(params[:id])
+    @tweet = Tweet.find(params[:tweet_id])
     @reply = Reply.new
   end
 
   def create
-    @tweet = Tweet.find(params[:id])
+    @tweet = Tweet.find(params[:tweet_id])
     @reply = current_user.replies.build(reply_params)
     @reply.tweet = @tweet
     if @reply.save
-      redirect_back(fallback_location: replies_path(@tweet))
+      redirect_back(fallback_location: tweet_replies_path(@tweet))
     else
       @reply.errors.each do |error, message|
         flash[:alert] = "#{error.capitalize} #{message}"
       end
-      redirect_to replies_path(@tweet)
+      redirect_to tweet_replies_path(@tweet)
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,8 @@ Rails.application.routes.draw do
   # 請依照專案指定規格來設定路由
 
   resources :tweets, only: [:index, :create] do
+    resources :replies, only: [:index, :create]
     member do
-      resources :replies, only: [:index, :create]
-
       post :like
       post :unlike
     end
