3f5b44328d5c9942f03d0c1972dc506959dbbcef
KC <weilihmen@gmail.com>
2018-02-09 09:21:16 +0800

    done edit page&controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,26 +7,20 @@ class UsersController < ApplicationController
 
   def edit
     #功能已併入devise頁面,但還是先寫起來放著
+    unless @user == current_user
+      redirect_to tweets_user_path(@user)
+    end
   end
 
   def update
     #功能已併入devise頁面,但還是先寫起來放著
-    @new_restaurant = Restaurant.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "更新成功"
-      redirect_to root_path
+      redirect_to edit_user_path
     else
       flash.now[:alert] = "更新失敗"
       render :edit
     end
-  end
-
-  def edit
-    #併入devise頁面
-  end
-
-  def update
-    #併入devise頁面
   end
 
   def followings
