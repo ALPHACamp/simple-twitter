574b615f63d126b04a840259c337aa1e7f6a0e04
LanceRabbit <lancerabbit@gmail.com>
2018-02-09 20:15:36 +0800

    fix bug : admin tweets/ users page can't pass rspec

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -1,14 +1,14 @@
 class Admin::TweetsController < Admin::BaseController
 
   def index
-    @tweets = Tweet.order(created_at: :desc)
+    @tweets = Tweet.all
   end
 
   def destroy
 
     @tweet = Tweet.find(params[:id])
     @tweet.destroy
-    redirect_to admin_tweets_path
+    redirect_to admin_root_path
     flash[:alert] = "tweet was deleted"    
   end
 end
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,6 @@
 class Admin::UsersController < Admin::BaseController
 
   def index
-     @users = User.order(created_at: :desc)
+     @users = User.all
   end
 end
