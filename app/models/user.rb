618252c5f00394afa21fc7ebf429c3a04a7e35cb
David <biki3507@gmail.com>
2018-02-10 23:00:04 +0800

    8 - temp add followering data model
    8.使用者可以追蹤/取消追蹤其他使用者（不能追蹤自己）

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
 
   has_many :tweets, dependent: :restrict_with_error
   has_many :replies, dependent: :restrict_with_error
+  has_many :followships, dependent: :restrict_with_error
   # 需要 app/views/devise 裡找到樣板，加上 name 屬性
   # 並參考 Devise 文件自訂表單後通過 Strong Parameters 的方法
   validates_presence_of :name
