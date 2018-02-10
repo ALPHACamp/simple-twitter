2cb75a35c3056b770ef7e37ee68438cfec82f34b
finile <wu.nile@gmail.com>
2018-02-10 11:41:04 +0800

    correct back-end user page order by tweets no by adding column tweets_count in db

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,5 @@
 class Admin::UsersController < Admin::BaseController
   def index
-    @users = User.all.includes(:tweets, :likes, :followships)
+    @users = User.all.includes(:tweets, :likes, :followships).order(tweets_count: :desc)
   end
 end
