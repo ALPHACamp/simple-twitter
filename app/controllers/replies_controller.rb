89026e7da45d8a10714b857b3d45962f902a3dba
Adamlin0708 <kuso12610@gmail.com>
2018-02-10 18:31:15 +0800

    fix tweet_replies

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,7 +1,7 @@
 class RepliesController < ApplicationController
 
   def index
-    @tweet = Tweet.find(params[:id])
+    @tweet = Tweet.find(params[:tweet_id])
     @user = User.find(@tweet.user_id)
     @replies = Reply.where(:tweet_id => @tweet.id)
   end
@@ -9,10 +9,10 @@ class RepliesController < ApplicationController
   def create
     @reply = Reply.new(reply_params)
     @reply.user_id = current_user.id
-    @reply.tweet_id = params[:id]
+    @reply.tweet_id = params[:tweet_id]
     if @reply.save      
       flash[:notice] = "reply was successfully created"
-      redirect_to replies_tweet_path
+      redirect_to tweet_replies_path
     else
       flash.now[:alert] = "tweet was failed to create"
       render :index
