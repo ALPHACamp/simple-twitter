4b907771bea694c519f1e5545694223ac19ccb96
finile <wu.nile@gmail.com>
2018-02-10 12:40:13 +0800

    fix testing error for self followship

--- app/models/followship.rb
+++ app/models/followship.rb
@@ -1,6 +1,13 @@
 class Followship < ApplicationRecord
   validates :following_id, uniqueness: { scope: :user_id }
+  validates :check_self_following
   belongs_to :user 
   belongs_to :following, class_name: "User", counter_cache: :followers_count
+
+  def check_self_following
+    if user_id == following_id
+      errors[:following_id]<<"can't follow yourself"
+    end
+  end
 
 end
