33b9759fcd89488ee6f6e0b7c2086d5a89a79872
David <biki3507@gmail.com>
2018-02-10 16:49:07 +0800

    7 - add create and reply and user releationship
    7.使用者能回覆別人的推播

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -4,6 +4,15 @@ class RepliesController < ApplicationController
   end
 
   def create
+    @tweet = Tweet.find(:id)
+    @user = User.find(current_user.id)
+    @replies = @tweet.comment.build(replie_params)
+    @replies.user = current_user
+    @replies.save!
+    redirect_back(fallback_location: root_path)
   end
 
+  def replie_params
+    params.require(:replies).permit(:comment)
+  end
 end
