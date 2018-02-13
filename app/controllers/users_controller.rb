5b601a962acd163d28229113e8e274a3809961ec
laibuc <ljyuan5193@gmail.com>
2018-02-13 09:12:36 +0800

    github rspec*8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,8 +40,7 @@ class UsersController < ApplicationController
   def likes
     @user_id = User.find(params[:id])
     @like = Like.order(created_at: :desc).limit(10)
-    @likes = Like.order(created_at: :desc)
-    @users =Like.order(created_at: :desc).includes(:tweet)
+    @likes = Like.order(created_at: :desc).includes(:tweet)
   end
 
   private
