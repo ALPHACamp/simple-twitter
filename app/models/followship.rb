b0ae9a6e3b3fd0e3d419b64a0f63008298d61ae9
finile <wu.nile@gmail.com>
2018-02-13 21:20:30 +0800

    fix follower/following problem

--- app/models/followship.rb
+++ app/models/followship.rb
@@ -2,7 +2,7 @@ class Followship < ApplicationRecord
   validates :following_id, uniqueness: { scope: :user_id }
   validate :check_self_following
   belongs_to :user 
-  belongs_to :following, class_name: "User", counter_cache: :followers_count
+  belongs_to :following, class_name: "User"
 
   def check_self_following
     if user_id == following_id
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ApplicationRecord
   validates :tweet_id, uniqueness: { scope: :user_id }
-  belongs_to :user, counter_cache: true
+  belongs_to :user
   belongs_to :tweet, counter_cache: true
 
 end
