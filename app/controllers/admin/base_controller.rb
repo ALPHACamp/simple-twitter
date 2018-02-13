4883a4f3231011406780b5acdd6c4ea5a19255d7
badbug wne <sweewen45@gmail.com>
2018-02-13 10:38:35 +0800

    Move admin authentication from application to admin::base

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,4 +1,13 @@
 class Admin::BaseController < ApplicationController
   before_action :authenticate_user! # 登入才能使用網站
   before_action :authenticate_admin # admin才能進入後台
+
+  private
+  def authenticate_admin
+    unless current_user.admin?
+      flash[:alert] = "Not allow"
+      redirect_to root_path
+    end  
+  end
+
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user! # 登入才能使用網站
-
   
-
-  def authenticate_admin
-    unless current_user.admin?
-      flash[:alert] = "Not allow"
-      redirect_to root_path
-    end  
-  end
   
   # 請參考 Devise 文件自訂表單後通過 Strong Parameters 的方法
   # https://github.com/plataformatec/devise#strong-parameters
