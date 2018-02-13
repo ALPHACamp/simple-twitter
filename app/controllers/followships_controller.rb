28eb685c7c81bff70e956e2ba35d5d62a209ee43
badbug wne <sweewen45@gmail.com>
2018-02-13 13:34:44 +0800

    user can not follow self

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -1,14 +1,16 @@
 class FollowshipsController < ApplicationController
   def create
-    @followship = current_user.followships.build(following_id: params[:following_id])
-    if @followship.save
-      User.find_by(id: params[:following_id]).count_followers
-      redirect_back(fallback_location: root_path, notice: "您已將 #{User.find_by(id: params[:following_id]).name} 加入追蹤名單!" )   
-    else
-      flash[:alert] = @followship.errors.full_messages.to_sentence
-      redirect_back(fallback_location: root_path)
-    end  
-  end
+    if current_user != @user
+      @followship = current_user.followships.build(following_id: params[:following_id])
+      if @followship.save
+        User.find_by(id: params[:following_id]).count_followers
+        redirect_back(fallback_location: root_path, notice: "您已將 #{User.find_by(id: params[:following_id]).name} 加入追蹤名單!" )   
+      else
+        flash[:alert] = @followship.errors.full_messages.to_sentence
+        redirect_back(fallback_location: root_path)
+      end  
+    end
+  end  
 
   def destroy
     @followship = current_user.followships.where(following_id: params[:id])
