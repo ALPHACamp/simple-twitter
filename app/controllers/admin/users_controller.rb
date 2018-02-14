1a8fc1a271953f96d7b0c013b10b0025bb6a713e
victorloo <ila9970@gmail.com>
2018-02-15 06:40:31 +0800

    user order by tweets_count on admin page

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,12 +2,7 @@ class Admin::UsersController < Admin::BaseController
   
   #顯示所有的使用者
   def index
-    @users = User.all
+    @users = User.order(tweets_count: :desc)
   end
 
-  private
-
-  def set_user
-    @user = User.find(params[:id])
-  end
 end
