78e867119374c51587610dc9f040575eda6f2500
victorloo <ila9970@gmail.com>
2018-02-16 02:13:27 +0800

    admin can understand each user's likes

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,6 +3,8 @@ class Admin::UsersController < Admin::BaseController
   #顯示所有的使用者
   def index
     @users = User.order(tweets_count: :desc)
+    #@tweets = Tweet.find(params[:id])
+    #@likes = Tweet.where(:user_id => @tweets.user).pluck(:likes_count).sum
   end
 
 end
