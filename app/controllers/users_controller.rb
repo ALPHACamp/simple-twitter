e08bc5c95cc8074a6b8f99e47a854d2587583c22
Penciler <eraser41@livemail.tw>
2018-02-17 20:10:14 +0800

    add order to like list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
   end
 
   def likes
-    @likes=@user.likes # 基於測試規格，必須講定變數名稱
+    @likes=@user.likes.order(created_at: :desc) # 基於測試規格，必須講定變數名稱
   end
 
   private
