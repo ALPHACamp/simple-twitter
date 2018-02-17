d4a7487485b55fe1f7a1ef0f59ac8c3849e1528b
Penciler <eraser41@livemail.tw>
2018-02-17 16:19:14 +0800

    add function to update followers_count automatically when follow/unfollow

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -4,6 +4,9 @@ class FollowshipsController < ApplicationController
 
     if @followship.save
       flash[:notice] = "Successfully followed"
+      @following = User.find(params[:following_id])
+      @following.update(:followers_count => @following.followers.count)
+      @following.save!
       redirect_back(fallback_location: root_path)
     else
       flash[:alert] = @followship.errors.full_messages.to_sentence
@@ -14,6 +17,9 @@ class FollowshipsController < ApplicationController
   def destroy
     @followship = current_user.followships.where(following_id: params[:id]).first
     @followship.destroy
+    @following = User.find(params[:id])
+    @following.update(:followers_count => @following.followers.count)
+    @following.save!
     flash[:alert] = "Followship destroyed"
     redirect_back(fallback_location: root_path)
   end
