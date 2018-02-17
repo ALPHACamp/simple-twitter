2ca356173f283f9202f426d31a01d232fbc09f08
Penciler <eraser41@livemail.tw>
2018-02-17 16:25:13 +0800

    list tweet according to create_at

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,7 +3,7 @@ class TweetsController < ApplicationController
 
   def index
     @users=User.all # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
-    @tweets=Tweet.all
+    @tweets=Tweet.order(created_at: :desc)
     @tweet=Tweet.new
   end
 
