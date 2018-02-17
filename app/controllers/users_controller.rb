e05793a349e15b0a38dd8cdbde9db8b28c7ba1f5
Penciler <eraser41@livemail.tw>
2018-02-17 20:09:08 +0800

    add order to follower list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
   end
 
   def followers
-    @followers=@user.followers # 基於測試規格，必須講定變數名稱
+    @followers=@user.followers.order(created_at: :desc) # 基於測試規格，必須講定變數名稱
   end
 
   def likes
