9c2538504fb1a57136c909660e3ca865514c13d0
vita <dragic.hsieh@gmail.com>
2018-02-20 00:17:34 +0800

    Modify move authenticate_user! to application_controller.rb

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -1,8 +1,4 @@
 class Admin::TweetsController < Admin::BaseController
-  
-  # 建立使用者登入流程
-  before_action :authenticate_user!
-
   # 建立管理者認證
   before_action :authenticate_admin
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  # 建立使用者登入流程
+  before_action :authenticate_user!
+
   # 請參考 Devise 文件自訂表單後通過 Strong Parameters 的方法
   # https://github.com/plataformatec/devise#strong-parameters
   # 注意有 sign_up 和 account_update 兩種參數要處理
