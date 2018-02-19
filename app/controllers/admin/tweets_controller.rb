4642b449996705e02f1be1de88b4a73b49841a4d
vita <dragic.hsieh@gmail.com>
2018-02-19 18:59:29 +0800

    Done add user name to registration form

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -1,4 +1,11 @@
 class Admin::TweetsController < Admin::BaseController
+  
+  # 建立使用者登入流程
+  before_action :authenticate_user!
+
+  # 建立管理者認證
+  before_action :authenticate_admin
+
   def index
   end
 
