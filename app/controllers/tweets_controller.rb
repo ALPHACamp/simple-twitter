1b7ec7145bf8e023b007afcbe8a292e3a8a9a84e
vita <dragic.hsieh@gmail.com>
2018-02-22 00:54:55 +0800

    Done add is_liked? method to tweet model for checking like/unlike button

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,12 +1,14 @@
 class TweetsController < ApplicationController
 
+  before_action :set_tweet, only: [:like, :unlike]
+
   def index
     @users # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
     @tweets = Tweet.all
     @tweet = Tweet.new # 把新增的動作放在index頁面
   end
 
- def create # 日後確認，為何這樣寫也
+  def create # 日後確認，為何這樣寫也
     @tweet = Tweet.new(tweet_params)
     @tweet.user = current_user
     if @tweet.save
@@ -19,15 +21,21 @@ class TweetsController < ApplicationController
   end
 
   def like
-    @tweet = Tweet.find(params[:id])
     @tweet.likes.create!(user: current_user)
     redirect_back(fallback_location: root_path) # 導回上一頁
   end
 
   def unlike
+    likes = Like.where(tweet: @tweet, user: current_user)
+    likes.destroy_all
+    redirect_back(fallback_location: root_path) # 導回上一頁
   end
 
   private
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
+  end
 
   # Strong Parameters
   def tweet_params
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -15,4 +15,9 @@ class Tweet < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
 
+  # 檢查喜歡紀錄是否存在
+  def is_liked?(user)
+    self.liked_users.include?(user)
+  end  
+
 end
