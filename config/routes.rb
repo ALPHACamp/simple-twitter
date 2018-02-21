6739336c60a87eb1a5dac47312ff776eb48048d5
vita <dragic.hsieh@gmail.com>
2018-02-21 12:16:53 +0800

    Done add table style for admin user/index

--- config/routes.rb
+++ config/routes.rb
@@ -6,21 +6,23 @@ Rails.application.routes.draw do
   # 設定前台首頁路由
     root "tweets#index"
 
-  # 設定前台路由 / 設定回覆路由
+  # 設定前台路由，首頁及新增
+  # 設定在特定的推播下，可看見推播及新增(回覆)
   	resources :tweets, only: [:index, :create] do
       resources :replies, only: [:index, :create]
     end
  
-  # 設定 user 的相關路由
+  # 設定點擊使用者名字時，可以看見推播牆及編輯自己的資料路由
     resources :users, only: [:edit, :update] do
       member do
         get :tweets
       end
     end    
 
-  # 設定後台首頁路由
+  # 設定後台首頁路由 & 設定在admin之下可看所有使用者清單
   	namespace :admin do
   		resources :tweets, only: [:index, :destroy]
+      resources :users, only: [:index]
   		root "tweets#index"
   	end	
 
