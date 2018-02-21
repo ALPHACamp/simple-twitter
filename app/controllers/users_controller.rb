579342a86df359875091ec34ab9352d1c1ca81bb
vita <dragic.hsieh@gmail.com>
2018-02-21 16:41:12 +0800

    Done add reply.count and css style on index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:tweets, :edit, :update]
 
   def tweets
-    @tweets = @user.tweets.all
+    @tweets = @user.tweets
   end
 
   def edit
