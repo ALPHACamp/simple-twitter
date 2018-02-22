7d03947d329901d7bb581921c6fb9dedf5466913
Duncan <loxa10077@gmail.com>
2018-02-23 07:10:01 +0800

    public index only show ten user

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,7 +2,7 @@ class TweetsController < ApplicationController
 
   def index
     @tweets = Tweet.all.order(created_at: :desc).limit(10)
-    @users = User.all  # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
+    @users = User.all.order(created_at: :desc).limit(10) # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
     @tweet = Tweet.new
   end
 
