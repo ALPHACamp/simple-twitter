7e7788814af174d002219767c3a4b514afb84af8
Wells <chawkeye@gmail.com>
2018-02-22 09:45:57 +0800

    added kaminari

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'jquery-rails'
 gem 'ffaker'
 gem 'rest-client'
 gem 'filestack-rails', '~> 3.1.0'
+gem 'kaminari'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.4'
 # Use sqlite3 as the database for Active Record
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,7 +2,7 @@ class TweetsController < ApplicationController
   before_action :set_tweet, only: [:like, :unlike]
   def index
     @users = User.order(followers_count: :desc).limit(10) # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
-    @tweets = Tweet.all.includes(:likes, :user, :like_users).order(created_at: :desc)
+    @tweets = Tweet.includes(:likes, :user, :like_users).order(created_at: :desc).page(params[:page]).per(15)
     @tweet = Tweet.new
   end
 
