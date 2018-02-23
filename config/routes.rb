1aa623d1c43105cc20cd093d49c6bc562f1b7d3d
vita <dragic.hsieh@gmail.com>
2018-02-23 16:58:53 +0800

    Done setup for followings and followers page, follow / unfollow function

--- config/routes.rb
+++ config/routes.rb
@@ -4,31 +4,36 @@ Rails.application.routes.draw do
 
   # 請依照專案指定規格來設定路由
   # 設定前台首頁路由
-    root "tweets#index"
+  root "tweets#index"
 
   # 設定前台路由，首頁及新增
   # 設定在特定的推播下，可看見推播及新增(回覆)
-  	resources :tweets, only: [:index, :create] do
-      resources :replies, only: [:index, :create]
+  resources :tweets, only: [:index, :create] do
+    resources :replies, only: [:index, :create]
       member do
         post :like
         post :unlike
       end  
-    end
+  end
  
   # 設定點擊使用者名字時，可以看見推播牆及編輯自己的資料路由
-    resources :users, only: [:edit, :update] do
-      member do
+  resources :users, only: [:edit, :update] do
+    member do
         get :tweets
         get :likes # 可看該使用者 like 的推播清單
-      end
-    end    
+        get :followings # 看見某一使用者正在關注的使用者
+        get :followers # 看見某一使用者的跟隨者
+    end
+  end
+
+  # 追蹤 / 取消追蹤路由
+  resources :followships, only: [:create, :destroy]
 
   # 設定後台首頁路由 & 設定在admin之下可看所有使用者清單
-  	namespace :admin do
-  		resources :tweets, only: [:index, :destroy]
-      resources :users, only: [:index]
-  		root "tweets#index"
-  	end	
+  namespace :admin do
+  	resources :tweets, only: [:index, :destroy]
+    resources :users, only: [:index]
+  	root "tweets#index"
+  end	
 
 end
