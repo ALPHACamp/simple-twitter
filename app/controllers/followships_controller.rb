a1f352a7bea4980fa65387739bee674c5394dc96
vita <dragic.hsieh@gmail.com>
2018-02-23 17:31:25 +0800

    Done setup uniqueness method and add create action for followship record

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -1,6 +1,17 @@
 class FollowshipsController < ApplicationController
   def create
-  end
+  	# 需要設定前端的 link_to ，在發出請求時送進 following_id
+  	@followship = current_user.followships.build(following_id: params[:following_id])
+  	if @followship.save
+  		flash[:notice] = "成功追蹤該名使用者"
+  		redirect_back(fallback_location: root_path)
+  	else
+  		# 驗證失敗時，Model 將錯誤訊息放在 errors 裡回傳
+  		# 使用 errors.full_messages 取出完成訊息集合 (Array) ，串接 to_sentence 將 Array 組合成 String
+  		flash[:alert] = @followship.errors.full_messages.to_sentence
+  		redirect_back(fallback_location: root_path)
+  	end
+  end	
 
   def destroy
   end
--- app/models/followship.rb
+++ app/models/followship.rb
@@ -1,4 +1,5 @@
 class Followship < ApplicationRecord
+  # 加入 Model 驗證，每個 User 只能追蹤另一個 User 一次
   validates :following_id, uniqueness: { scope: :user_id }
 
   belongs_to :user # 每一筆追蹤紀錄，屬於發出追蹤的使用者 (User)
