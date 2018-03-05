83efedf963748c6926b219d68a087add274407c8
Shih_Kuan <cliff1304@gmail.com>
2018-03-05 21:31:54 +0800

    User must login before browse web site.

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,3 +1,4 @@
 class Admin::BaseController < ApplicationController
-
+  # 加入認證程序，使用者需登入才能瀏覽網站
+  before_action :authenticate_user!
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  # 加入認證程序，使用者需登入才能瀏覽網站
+  before_action :authenticate_user!
+
   # 請參考 Devise 文件自訂表單後通過 Strong Parameters 的方法
   # https://github.com/plataformatec/devise#strong-parameters
   # 注意有 sign_up 和 account_update 兩種參數要處理
