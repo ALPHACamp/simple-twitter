dab179276795919a09c82fc4157af46edaff5156
Shih_Kuan <cliff1304@gmail.com>
2018-03-05 22:07:25 +0800

    User cannot use same name to sign up. Create tweets index page. Create

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,13 @@ class User < ApplicationRecord
 
   # 需要 app/views/devise 裡找到樣板，加上 name 屬性
   # 並參考 Devise 文件自訂表單後通過 Strong Parameters 的方法
+  # 讓 name 爲必填欄位
   validates_presence_of :name
   # 加上驗證 name 不能重覆 (關鍵字提示: uniqueness)
+  # 這樣似乎是能讓網頁跳出 error，但是似乎不太理想
+  # validates :name, uniqueness: {scope: :id}
+  # 這個是到 api.rubyonrails.org 查到的
+  validates_uniqueness_of :name
 
   # 檢查是否爲網站管理員
   def is_admin?
