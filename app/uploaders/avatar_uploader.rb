c8e077cd0225832e9f379de292c13e1b31d4f277
victorloo <ila9970@gmail.com>
2018-03-16 22:05:58 +0800

    close auto=image by uploader

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -22,12 +22,12 @@ class AvatarUploader < CarrierWave::Uploader::Base
   #end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  def default_url(*args)
+  #def default_url(*args)
   # #   # For Rails 3.1+ asset pipeline compatibility:
-    ActionController::Base.helpers.asset_path("fallback/" +["default-avatar.png"].compact.join('_'))
+   # ActionController::Base.helpers.asset_path("fallback/" +["default-avatar.png"].compact.join('_'))
   # #
   #  "/assets/images/" + [version_name, "default.png"].compact.join('_')
-  end
+  #end
   #
   #
   # version :medium do
