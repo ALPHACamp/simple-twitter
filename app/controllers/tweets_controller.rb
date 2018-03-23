6ef163852aa9f2f901465034aee98fc929be9cb9
davincilchen <doki2099@gmail.com>
2018-03-23 16:38:43 +0800

    add form for tweet in tweets index page and process params of create action

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,9 +2,12 @@ class TweetsController < ApplicationController
 
   def index
     @users # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
+    @tweet = Tweet.new
   end
 
   def create
+    puts " !!!!!!!!!!!~~~~~~~~~"
+    redirect_back(fallback_location: root_path)
   end
 
   def like
@@ -13,4 +16,10 @@ class TweetsController < ApplicationController
   def unlike
   end
 
+
+ private
+
+  def tweet_params
+    params.require(:tweet).permit(:description)
+  end
 end
