21ed6db028dc0672ea15d76fb5cb3ebe6db0907f
davincilchen <doki2099@gmail.com>
2018-03-25 15:27:58 +0800

    modify like and unlike function in controller tweet

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,5 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, only:[:like, :unlike]
 
   def index
     @users # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
@@ -13,9 +14,14 @@ class TweetsController < ApplicationController
   end
 
   def like
+    @tweet.likes.create(user: current_user)
+    redirect_back(fallback_location: root_path)
   end
 
   def unlike
+    @likes = Like.where(user: current_user, tweet: @tweet )
+    @likes.destroy_all
+    redirect_back(fallback_location: root_path)
   end
 
 
@@ -23,5 +29,9 @@ class TweetsController < ApplicationController
 
   def tweet_params
     params.require(:tweet).permit(:description)
+  end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
   end
 end
