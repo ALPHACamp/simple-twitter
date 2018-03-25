df819b35804945517fb219ee52ebc9beea4041c6
davincilchen <doki2099@gmail.com>
2018-03-25 22:05:32 +0800

    setup relation for follower in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,9 @@ class User < ApplicationRecord
   has_many :followships, dependent: :destroy
   has_many :followings, through: :followships
 
+  has_many :inverse_followships, class_name: "Followship", foreign_key: "following_id"
+  has_many :followers, through: :inverse_followships, source: :user
+
 
   # admin? 判斷單個user是否有 admin 角色，列如：current_user.admin?
   def admin?
