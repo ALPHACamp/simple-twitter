72f2d6018fdaeee4cb4f83b33eb65a867cf8b119
davincilchen <doki2099@gmail.com>
2018-03-25 22:37:51 +0800

    add popular on tweets#index view

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,7 +2,7 @@ class TweetsController < ApplicationController
   before_action :set_tweet, only:[:like, :unlike]
 
   def index
-    @users # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
+    @users = User.order(followers_count: :desc).limit(10)# 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
     @tweet = Tweet.new
     @tweets = Tweet.all.order(created_at: :desc)
   end
