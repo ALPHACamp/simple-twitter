307c7d2106c557620c15aae370b2ffc372159c1a
davincilchen <doki2099@gmail.com>
2018-03-26 21:20:42 +0800

    admin can delete tweet

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -4,5 +4,13 @@ class Admin::TweetsController < Admin::BaseController
   end
 
   def destroy
+    @tweet = Tweet.find(params[:id])
+    @tweet.destroy
+    if @tweet.errors.any?
+      flash[:alert] = @tweet.errors.full_messages.to_sentence
+    else
+      flash[:alert] = "Tweet was successfully deleted"
+    end
+    redirect_to admin_tweets_path
   end
 end
