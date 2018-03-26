3842c0c0adfe84d9c59a7af84c6518f5abf0fe3b
davincilchen <doki2099@gmail.com>
2018-03-26 12:04:25 +0800

    setup routes for replies#index and replies#create action

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
   end
 
   resources :tweets, only: [:index, :create] do
+    resources :replies, only: [:index, :create]
+
     member do
       post :like
       post :unlike
