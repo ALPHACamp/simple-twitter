0e05e6cac0ab7e09e33b7e701911be00902dcd69
Duncan <loxa10077@gmail.com>
2018-03-26 21:07:11 +0800

    user can edit profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,29 @@
 class UsersController < ApplicationController
 
   def tweets
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
     unless @user == current_user
       redirect_to tweets_user_path(current_user), alert: "Can't edit other's profile"
     end
   end
 
   def update
+    if @user == current_user
+      if @user.update(user_params)
+        flash[:notice] = "Profile updated"
+        redirect_to root_path
+      else
+        flash[:alert] = @user.errors.full_messages.to_sentence
+        render :edit
+      end
+    else
+      flash[:alert] = "Can't edit other's profile"
+      redirect_to root_path
+    end
   end
 
   def followings
@@ -22,6 +36,12 @@ class UsersController < ApplicationController
 
   def likes
     @likes # 基於測試規格，必須講定變數名稱
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :avatar)
   end
 
 end
