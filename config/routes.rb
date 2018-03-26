e2eef3afa7792e964bec5a0ffb487ff839599ba9
Duncan <loxa10077@gmail.com>
2018-03-26 10:33:59 +0800

    add routes for user and admin and public page

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,18 @@ Rails.application.routes.draw do
   devise_for :users
 
   # 請依照專案指定規格來設定路由
+  
+  resources :tweets
+  root "tweets#index"
+
+  resources :users, only: [:edit, :update]
+
+  namespace :admin do
+    root "tweets#index"
+    resources :tweets, only: [:index, :destroy]
+  end
+
+
+
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+file = File.open("#{Rails.root}/public/avatar/user#{rand(1..20)}.jpg")
+ User.create(
+   email: "root@example.com",
+   password: "12345678",
+   name: "Duncan",
+   introduction: "Administrator",
+   avatar: file,
+   role: "admin"
+ )
+ puts "admin has created"
