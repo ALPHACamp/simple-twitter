c6ad8c80a49c53ac8a3c948fbc6fca4c4ebe8943
Ace <waynn7788@gmail.com>
2018-03-26 22:14:42 +0800

    set up routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,30 @@ Rails.application.routes.draw do
   devise_for :users
 
   # 請依照專案指定規格來設定路由
+  resources :tweets, only: [:index, :create] do
+    resources :replies, only: [:index, :create]
+    member do
+      get :like
+      post :unlike
+    end
+  end
 
+  root "tweets#index"
+
+  resources :uers, only: [:edit] do
+    member do
+      get :likes
+      get :tweets
+      get :followings
+      get :followers
+    end
+  end
+
+  resources :followships, only: [:create, :destroy]
+
+  namespace :admin do
+    resources :tweets, only: [:index, :destroy]
+    resources :users, only: [:index]
+    root "tweets#index"
+  end
 end
