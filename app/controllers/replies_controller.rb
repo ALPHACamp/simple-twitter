661b0ee77bdb0c3d7a19d8951afe110118104dc4
davincilchen <doki2099@gmail.com>
2018-03-26 18:22:06 +0800

    user can add reply on replies index page

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,14 +1,25 @@
 class RepliesController < ApplicationController
-  before_action :set_tweet, only:[:index]
+  before_action :set_tweet, only:[:index, :create]
 
   def index
+    @reply = Reply.new
     @user = @tweet.user
   end
 
   def create
+    @reply = @tweet.replies.build(reply_params)
+    @reply.user = current_user
+    if !@reply.save
+      flash[:alert] = @reply.errors.full_messages.to_sentence
+    end
+    redirect_back(fallback_location: root_path)
   end
 
  private
+
+  def reply_params
+    params.require(:reply).permit(:comment)
+  end
 
   def set_tweet
     @tweet = Tweet.find(params[:tweet_id])
