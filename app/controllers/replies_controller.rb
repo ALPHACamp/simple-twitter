0ddb137aecc6168e08f7bacd87d69fdb462d30b7
davincilchen <doki2099@gmail.com>
2018-03-26 13:16:33 +0800

    add user info and main tweet to reply page

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,9 +1,17 @@
 class RepliesController < ApplicationController
+  before_action :set_tweet, only:[:index]
 
   def index
+    @user = @tweet.user
   end
 
   def create
+  end
+
+ private
+
+  def set_tweet
+    @tweet = Tweet.find(params[:tweet_id])
   end
 
 end
