7400ad07a078345a185b52c8cbd6c7176c9c35e5
davincilchen <doki2099@gmail.com>
2018-03-28 19:44:52 +0800

    add test for heroku

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -11,17 +11,18 @@ class Admin::UsersController < Admin::BaseController
     #@users = User.joins('LEFT JOIN tweets ON tweets.user_id = users.id')
     #.group('tweets.user_id')
     #.order('COUNT(tweets.user_id) DESC')
-
+    puts "~~~~~~~~~~~~1"
     @users = Array.new
+    puts "~~~~~~~~~~~~2"
     @some_users= User.joins('INNER JOIN tweets ON tweets.user_id = users.id')
     .group('tweets.user_id')
     .order('COUNT(tweets.user_id) DESC')
 
-
+    puts "~~~~~~~~~~~~3"
     @some_users.each do |user|
       @users.push(user)
     end
-
+    puts "~~~~~~~~~~~~4"
     tmp_user = User.all
     tmp_user.each do |user|
       if user.tweets.count == 0
