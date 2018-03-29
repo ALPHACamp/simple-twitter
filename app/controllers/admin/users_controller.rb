e3193f27f85ce88ea2252e1d215f71056fb73ab7
Riemann <dr.mactt@gmail.com>
2018-03-30 02:43:32 +0800

    add order setting to admin users controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,8 +2,8 @@ class Admin::UsersController < Admin::BaseController
 
   # S16L1U2 有最近十筆排序 參考一下
   def index
-    @users = User.all
-    # @users = User.all.order(user.tweets.count :desc) # 無效版
+    @users = User.all.order(tweets_count: :desc)
+    # @users = User.all
     # @user = User.left_joins(:tweets).group(:id).order('COUNT(tweets.id) DESC') # 依然無效，剩加欄一途。
     
   end
