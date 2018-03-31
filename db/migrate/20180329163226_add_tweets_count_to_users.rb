5f8b8647ac14ae70cc0dc178647acd16cd7f047e
Riemann <dr.mactt@gmail.com>
2018-03-31 11:53:44 +0800

    migrate error

--- db/migrate/20180329163226_add_tweets_count_to_users.rb
+++ db/migrate/20180329163226_add_tweets_count_to_users.rb
@@ -1,5 +1,5 @@
 class AddTweetsCountToUsers < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :tweets_count, :intger, default: 0
+    add_column :users, :tweets_count, :integer, default: 0
   end
 end
