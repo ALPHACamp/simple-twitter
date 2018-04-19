b82c55edc7d7385d4bd8b9e4cc4c5c7f6e20e6d7
Penciler <eraser41@livemail.tw>
2018-04-20 06:46:23 +0800

    let user to be able to update introduction

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,7 +33,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :avatar)
+    params.require(:user).permit(:name, :avatar, :introduction)
   end
 
   def set_user
