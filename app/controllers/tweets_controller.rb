26992f1db9b860a947934abe856ac843d678f907
shushu <shushu0212@gami.com>
2018-04-26 08:41:39 +0800

    sign in before index and Add a name field to the registration page

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,5 @@
 class TweetsController < ApplicationController
+  before_action :authenticate_user!  #devise內建的方法，導到登入頁面
 
   def index
     @users # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
-
-  # 請依照專案指定規格來設定路由
+  resources :users, only: [:show, :edit, :update]
+  
 
   root "tweets#index"
 
