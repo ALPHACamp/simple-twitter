e8666e7fc9e01a2f295ab59595e387f2c21c8f38
shushu <shushu0212@gami.com>
2018-04-27 23:18:59 +0800

    Link from navbar_avatar to current_user's tweets

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:show, :edit, :update] do
+    member do
+      get :tweets
+    end
+  end
+
   
 
   root "tweets#index"
