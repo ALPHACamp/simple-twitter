d3a0e9ca555419c310525c3a26d6d1ea5bc5265f
shushu <shushu0212@gami.com>
2018-04-27 23:35:42 +0800

    Add edit profile button on current user's tweets page and link to edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,14 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
 
   def tweets
   end
 
   def edit
+    unless @user == current_user
+      flash[:alert] = "非本人，無法編輯!"
+      redirect_to root_path
+    end
   end
 
   def update
@@ -20,5 +25,17 @@ class UsersController < ApplicationController
   def likes
     @likes # 基於測試規格，必須講定變數名稱
   end
+
+
+  private
+
+  def set_user
+      @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :avatar)
+  end
+
 
 end
