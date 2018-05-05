eab246a4e646ee54eb1993b25839562f8e8b1d14
SamChang045 <willpower03@gmail.com>
2018-05-05 20:11:56 +0800

    Finish the public user controller of followships replies，users and tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,16 +1,43 @@
 class TweetsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_tweet, only: [:like, :unlike]
 
   def index
-    @users # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
+    @tweet = Tweet.new
+    @tweets = Tweet.all.order(created_at: :desc)
+    @users = User.all.order(follower_count: :desc).limit(10)
   end
-
+  
   def create
+    @tweet = Tweet.new(tweet_params)
+    @tweet.user = current_user
+    if @tweet.save
+      flash[:notice] = "tweet was successfully created"
+      redirect_to tweets_path
+    else
+      flash.now[:alert] = "tweet was failed to create"
+      render :action => "index"
+    end    
   end
 
   def like
+    @tweet.likes.create!(user: current_user)
+    redirect_back(fallback_location: root_path)  # 導回上一頁
   end
 
   def unlike
+    likes = Like.where(tweet: @tweet, user: current_user)
+    likes.destroy_all
+    redirect_back(fallback_location: root_path)
   end
 
+  private
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
+  end
+ 
+  def tweet_params
+    params.require(:tweet).permit(:name, :description)
+  end  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,45 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :tweets, :followings, :followers, :likes]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def edit
+    unless @user == current_user
+      redirect_to user_path(@user)
+    end
+  end
+
+  def update
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
 
   def tweets
   end
 
-  def edit
-  end
-
-  def update
-  end
-
   def followings
-    @followings # 基於測試規格，必須講定變數名稱
+    @followings = @user.followings
   end
 
   def followers
-    @followers # 基於測試規格，必須講定變數名稱
+    @followers = @user.followers
   end
 
   def likes
-    @likes # 基於測試規格，必須講定變數名稱
   end
 
+  private
+     
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :avatar)
+  end    
 end
