00c9e90f591a1fc197a1978d863e3b510795afba
SamChang045 <willpower03@gmail.com>
2018-05-06 16:13:46 +0800

    Fixed after deleting the user tweet, admin should go to admin root page

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -10,7 +10,7 @@ class Admin::TweetsController < Admin::BaseController
   def destroy
     @tweet = Tweet.find(params[:id])
     @tweet.destroy
-    redirect_to admin_tweets_url
+    redirect_to admin_root_path
   end
 
   private
