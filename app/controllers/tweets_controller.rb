c4ff7d96190317a177bbcc156ef2d75e79ea1611
frank402 <frank40202@gmail.com>
2018-05-07 19:03:19 +0800

    fix navbar and tweet

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,13 +1,36 @@
 class TweetsController < ApplicationController
+  
   def index
     @users = User.order(followers_count: :desc).limit(10) # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
     @tweets = Tweet.all
     @tweet = Tweet.new
   end
 
-  def create; end
+  def create
+    @tweet = Tweet.new(tweet_params)
+    @tweet.user = current_user
+    if @tweet.save
+      flash[:notice] = 'tweet was successfully created'
+      redirect_to tweets_path
+    else
+      flash.now[:alert] = 'tweet was failed to create'
+      render :index
+    end
+  end
 
-  def like; end
+  def like 
+  end
 
-  def unlike; end
+  def unlike
+  end
+
+  private
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def tweet_params
+    params.require(:tweet).permit(:description)
+  end
 end
