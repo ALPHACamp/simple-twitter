a92b9bff42291c893fffb453a5020c5881f9adba
shushu <shushu0212@gami.com>
2018-05-13 10:52:08 +0800

    add reply funtion (Content can be stored in the database and calculated)

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,9 +1,25 @@
 class RepliesController < ApplicationController
+  before_action :set_tweet
 
   def index
+    @reply = Reply.new
   end
 
   def create
+    @reply = @tweet.replies.build(reply_params)
+    @reply.user = current_user
+    @reply.save!
+    redirect_to tweet_replies_path(@tweet)
   end
+
+  private
+
+  def set_tweet
+    @tweet = Tweet.find(params[:tweet_id])
+  end
+
+  def reply_params
+    params.require(:reply).permit(:comment)
+  end 
 
 end
