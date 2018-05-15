828105ed174ca98313743d2b59fcdd7dd329f41c
shushu <shushu0212@gami.com>
2018-05-15 08:29:23 +0800

    set up admin authentication

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -1,6 +1,4 @@
 class Admin::TweetsController < Admin::BaseController
-  before_action :authenticate_admin  #驗證身份是否為admin
-  
   def index
   end
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,12 +13,4 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 
-  # 驗證是否為admin
-  def authenticate_admin
-    unless current_user.admin?
-      flash[:alert] = "Not allow!"
-      redirect_to root_path
-    end
-  end
-
 end
