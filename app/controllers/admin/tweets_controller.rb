5e787438296034fc5f95aac07326a488f6db4931
shushu <shushu0212@gami.com>
2018-05-16 15:04:25 +0800

    admin index tweet and delete

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -1,7 +1,21 @@
 class Admin::TweetsController < Admin::BaseController
+  before_action :set_tweet, only: [:destroy]
+
   def index
+    @tweets = Tweet.order(created_at: :desc)
   end
 
   def destroy
+    @tweet.destroy
+    redirect_to admin_tweets_path
+    flash[:alert] = "tweet was deleted"
   end
+
+  private
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
+  end
+
 end
+
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -5,7 +5,7 @@ class Tweet < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
 
-  has_many :replies
+  has_many :replies, dependent: :destroy
 
   def is_liked?(user)
     self.liked_users.include?(user) 
