b78192e70a921c89d40a2b0113ad5e67f150f475
sinash <pine0113@gmail.com>
2018-05-29 18:50:49 +0800

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,42 @@ Rails.application.routes.draw do
 
   # 請依照專案指定規格來設定路由
 
+   root "tweet#index"
+
+
+  namespace :admin do
+    resources :tweets, only: [:index, :destroy]
+    resources :users, only: [:index]
+    root "admin/tweets#index"
+  end
+
+  resources :tweets, only: [:index, :show] do
+    resources :replies, only: [:index, :create]
+
+    member do
+     
+      post :like
+      post :unlike
+    end
+
+  end
+
+
+
+  resources :users, only: [:index, :show, :edit, :update] do
+  
+    member do 
+       get :likes
+       get :tweets
+       get :followings
+       get :followers
+     end
+
+  end
+
+
+  
+  resources :followships, only: [:create, :destroy]
+
+
 end
