2eb390039cca9f8eb3a93e82da6d75aafb707ade
sinash <pine0113@gmail.com>
2018-05-31 19:57:03 +0800

    add top 10 feature

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -6,6 +6,8 @@ class FollowshipsController < ApplicationController
 
     if @followship.save
       flash[:notice] = "Successfully followed"
+      @followship.following.followers_count+=1
+      @followship.following.save
       redirect_back(fallback_location: root_path)
     else
       flash[:alert] = @followship.errors.full_messages.to_sentence
@@ -15,6 +17,8 @@ class FollowshipsController < ApplicationController
 
   def destroy
     @followship = current_user.followships.where(following_id: params[:id]).first
+    @followship.following.followers_count-=1
+    @followship.following.save
     @followship.destroy
     flash[:alert] = "Followship destroyed"
     redirect_back(fallback_location: root_path)
