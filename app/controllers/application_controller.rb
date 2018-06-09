c19e797f9442ded6daa3e5d95f954b6bd9311017
Revi <s1492e034@gmail.com>
2018-06-09 10:42:59 +0800

    authenticate user for using the website

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,6 @@ class ApplicationController < ActionController::Base
   # 請參考 Devise 文件自訂表單後通過 Strong Parameters 的方法
   # https://github.com/plataformatec/devise#strong-parameters
   # 注意有 sign_up 和 account_update 兩種參數要處理
+
+  before_action :authenticate_user! #使用者一定要登入才能使用網站
 end
