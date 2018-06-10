5420b27c661e68c486f23c1be2278a081d95c37f
Revi <s1492e034@gmail.com>
2018-06-10 21:59:26 +0800

    add comment to followship model

--- app/models/followship.rb
+++ app/models/followship.rb
@@ -1,6 +1,7 @@
 class Followship < ApplicationRecord
   validates :following_id, uniqueness: { scope: :user_id }
 
+  #counter_cache 指南 https://rails.ruby.tw/association_basics.html
   belongs_to :user, counter_cache: :followers_count
   belongs_to :following, class_name: "User"
 end
