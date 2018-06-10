bc3a25919251ec380667d27aec2be9d1efcc8b03
Revi <s1492e034@gmail.com>
2018-06-10 22:42:20 +0800

    setup routes to users#tweets page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :tweets]
 
   def tweets
   end
--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,13 @@ Rails.application.routes.draw do
   # 請依照專案指定規格來設定路由
   root "tweets#index" #前台首頁
   resources :tweets
-  resources :users, only: [:show, :edit, :update]
   resources :followships, only: [:create, :destroy]
 
+
+  resources :users, only: [:show, :edit, :update] do
+    get :tweets
+  end
+  
 
   namespace :admin do
     root "tweets#index" #後台首頁
