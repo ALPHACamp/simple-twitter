acfdcf0500af0d6f7e6b9ee6b854b364a7481fd2
Jimmy <du20783@gmail.com>
2018-06-10 18:50:21 +0800

    add Kaminari and bootstrap3

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -2,8 +2,8 @@ class Admin::TweetsController < Admin::BaseController
     before_action :authenticate_user!
     before_action :authenticate_admin
   def index
-    @tweets = Tweet.all
-    @users = User.all
+    @tweets = Tweet.page(params[:page]).per(10)
+    @users = User.page(params[:page]).per(10)
   end
 
   def destroy
