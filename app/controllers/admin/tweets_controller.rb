939816040b55126d27a674aa9caa029d51d6352e
Jimmy <du20783@gmail.com>
2018-06-10 16:35:37 +0800

    add user data in admin index

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -3,6 +3,7 @@ class Admin::TweetsController < Admin::BaseController
     before_action :authenticate_admin
   def index
     @tweets = Tweet.all
+    @users = User.all
   end
 
   def destroy
@@ -10,5 +11,14 @@ class Admin::TweetsController < Admin::BaseController
     @tweet.destroy
     redirect_to admin_root_path
     flash[:alert] = "tweet was deleted"
+  end
+
+  private
+
+  def authenticate_admin
+    unless current_user.admin?
+      flash[:alert] = "Not allow"
+      redirect_to root_path
+    end
   end
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -15,12 +15,5 @@ class TweetsController < ApplicationController
   def unlike
   end
 
-  private
 
-  def authenticate_admin
-    unless current_user.admin?
-      flash[:alert] = "Not allow"
-      redirect_to root_path
-    end
-  end
 end
