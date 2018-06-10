940a6bfc25ab61fb16c75a4d6f4b7258d21f6dc1
Revi <s1492e034@gmail.com>
2018-06-10 15:06:11 +0800

    add tweets create action and view

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,15 +2,30 @@ class TweetsController < ApplicationController
 
   def index
     @users # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
+    @tweet = Tweet.new
   end
 
   def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      flash[:notice] = "tweet was successfully created"
+      redirtect_to root_path
+    else
+      flash.now[:alert] = "tweet was failed to create"
+      render :index
+    end
   end
 
   def like
   end
 
   def unlike
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:description)
   end
 
 end
