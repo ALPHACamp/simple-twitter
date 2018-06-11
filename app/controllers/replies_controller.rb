4450c14b162f3e4302582c779db833b8e08c0d91
Revi <s1492e034@gmail.com>
2018-06-11 11:43:31 +0800

    add form for reply in replies#index page

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,6 +1,9 @@
 class RepliesController < ApplicationController
 
   def index
+    @tweet = Tweet.find(params[:tweet_id]) #確認推播物件
+    @user = @tweet.user
+    @reply = Reply.new
   end
 
   def create
