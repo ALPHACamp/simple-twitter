7f41fcb9986a12868533b914272ef5a34fb53744
Revi <s1492e034@gmail.com>
2018-06-12 11:48:15 +0800

    fix followers_count in followship model

--- app/models/followship.rb
+++ app/models/followship.rb
@@ -2,6 +2,6 @@ class Followship < ApplicationRecord
   validates :following_id, uniqueness: { scope: :user_id }
 
   #counter_cache 指南 https://rails.ruby.tw/association_basics.html
-  belongs_to :user, counter_cache: :followers_count
-  belongs_to :following, class_name: "User"
+  belongs_to :user
+  belongs_to :following, class_name: "User", counter_cache: :followers_count
 end
