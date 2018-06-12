79bfa349a70f1b905a1d9d47b2925204ebb3dc00
Revi <s1492e034@gmail.com>
2018-06-12 11:01:01 +0800

    setup relations between like, tweet, and user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,10 @@ class User < ApplicationRecord
   has_many :inverse_followships, class_name: "followship", foreign_key: "following_id"
   has_many :followers, through: :inverse_followships, source: :user
 
-  has_many :replies, dependent: :destroy #使用者有很多reply記錄
+  has_many :replies, dependent: :destroy
+
+  has_many :likes, dependent: :destroy
+  has_many :liked_tweets, through: :likes, source: :tweet
   
 
   def admin?
