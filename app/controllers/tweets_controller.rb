c545d8e2aee80603b55c6178493f51e190c66c6f
Jimmy <du20783@gmail.com>
2018-06-12 11:16:46 +0800

    add tweet create action

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,11 +3,17 @@ class TweetsController < ApplicationController
 
 
   def index
-    @tweets = Tweet.page(params[:page]).per(10)
+    #@tweets = Tweet.page(params[:page]).per(10)
+    @tweets = Tweet.order(created_at: :desc).page(params[:page]).per(10)
+    @tweet = Tweet.new
     #@users # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
   end
 
   def create
+    @user = current_user
+    @tweet = @user.tweets.build(tweet_params)
+    @tweet.save!
+    redirect_to root_path
   end
 
   def like
@@ -16,5 +22,9 @@ class TweetsController < ApplicationController
   def unlike
   end
 
+  private
 
+  def tweet_params
+    params.require(:tweet).permit(:description)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,11 @@ class User < ApplicationRecord
 
   mount_uploader :avatar, AvatarUploader
 
-  has_many :tweets
+  has_many :tweets, dependent: :destroy
+
+  validates_presence_of :name
   # 需要 app/views/devise 裡找到樣板，加上 name 屬性
   # 並參考 Devise 文件自訂表單後通過 Strong Parameters 的方法
-  validates_presence_of :name
   # 加上驗證 name 不能重覆 (關鍵字提示: uniqueness)
   def admin?
     self.role == "admin"
