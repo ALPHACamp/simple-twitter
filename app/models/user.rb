0a698d4d6c779eddccb05fda66c3eca0d614a73c
Jimmy <du20783@gmail.com>
2018-06-13 10:59:27 +0800

    add reply dependent destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   mount_uploader :avatar, AvatarUploader
 
   has_many :tweets, dependent: :destroy
-  has_many :replies
+  has_many :replies, dependent: :destroy
 
   validates_presence_of :name
   # 需要 app/views/devise 裡找到樣板，加上 name 屬性
