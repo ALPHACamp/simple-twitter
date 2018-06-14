e8aa268c7c6316bdb299f19bd359f105329fd1c9
liuchunyueh <liuchunyueh@gmail.com>
2018-06-14 23:26:33 +0800

    add login process and correct url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,21 @@
 class ApplicationController < ActionController::Base
+
+
+  before_action :authenticate_user!
+
   protect_from_forgery with: :exception
 
   # 請參考 Devise 文件自訂表單後通過 Strong Parameters 的方法
   # https://github.com/plataformatec/devise#strong-parameters
   # 注意有 sign_up 和 account_update 兩種參數要處理
+
+  private
+
+  def authenticate_admin
+    unless current_user.admin?
+      flash[:alert] = "Not allow!"
+      redirect_to_root_path
+    end
+  end
+
 end
