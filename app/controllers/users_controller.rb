9dd82a0efa89133e994b48eefc7bdbb4f52798f7
Revi <s1492e034@gmail.com>
2018-06-14 10:55:08 +0800

    add followings and followers actions to user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :followings, :followers]
 
   def tweets
     @user = User.find(params[:user_id])
@@ -26,11 +26,11 @@ class UsersController < ApplicationController
   end
 
   def followings
-    @followings # 基於測試規格，必須講定變數名稱
+    @followings = @user.followings.order(created_at: :desc) # 基於測試規格，必須講定變數名稱
   end
 
   def followers
-    @followers # 基於測試規格，必須講定變數名稱
+    @followers = @user.followers.order(created_at: :desc) # 基於測試規格，必須講定變數名稱
   end
 
   def likes
