7418ef5f155ec8b8f26f3dbfcfb9c70ed862352b
Revi <s1492e034@gmail.com>
2018-06-14 11:08:35 +0800

    fix followers setting in user model and add views of followings & followers, link from users#tweets page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :followings, :followers]
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user_id, only: [:tweets, :followings, :followers]
 
   def tweets
-    @user = User.find(params[:user_id])
+    #@user = User.find(params[:user_id])
     @tweets = @user.tweets.order(created_at: :desc)
   end
 
@@ -26,10 +27,12 @@ class UsersController < ApplicationController
   end
 
   def followings
+    #@user = User.find(params[:user_id])
     @followings = @user.followings.order(created_at: :desc) # 基於測試規格，必須講定變數名稱
   end
 
   def followers
+    #@user = User.find(params[:user_id])
     @followers = @user.followers.order(created_at: :desc) # 基於測試規格，必須講定變數名稱
   end
 
@@ -42,6 +45,10 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def set_user_id
+    @user = User.find(params[:user_id])
   end
 
   def user_params
