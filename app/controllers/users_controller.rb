67b0b305184f70d475048592abdcfcb341e258ab
Jimmy <du20783@gmail.com>
2018-06-16 15:05:04 +0800

    add user create update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,23 @@
 class UsersController < ApplicationController
-
+  before_action :set_user, only:[:tweets, :edit, :update]
   def tweets
-    @user = User.find(params[:id])
     @tweets = @user.tweets.order(created_at: :desc).page(params[:page]).per(10)
   end
 
   def edit
+    unless @user == current_user
+      redirect_to tweets_user_path(@user)
+    end
   end
 
   def update
+    if @user.update(user_params)
+      flash[:notice] = "successfull updated"
+      redirect_to tweets_user_path(@user)
+    else
+      flash[:alert] = "update failed"
+      render :edit
+    end
   end
 
   def followings
@@ -23,4 +32,13 @@ class UsersController < ApplicationController
     @likes # 基於測試規格，必須講定變數名稱
   end
 
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:avatar ,:name, :introduction)
+  end
 end
