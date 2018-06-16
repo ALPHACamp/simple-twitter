746751f247ef29cac3e592d2b38df2715317919e
Jimmy <du20783@gmail.com>
2018-06-16 16:30:13 +0800

    add user and followship relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,9 @@ class User < ApplicationRecord
   has_many :tweets, dependent: :destroy
   has_many :replies, dependent: :destroy
 
+  has_many :followships, dependent: :destroy
+  has_many :followings, through: :followships
+
   validates_presence_of :name
   # 需要 app/views/devise 裡找到樣板，加上 name 屬性
   # 並參考 Devise 文件自訂表單後通過 Strong Parameters 的方法
