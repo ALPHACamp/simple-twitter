aaf1fd91e5dfc5480f224644c530e6f47c0d232c
Revi <s1492e034@gmail.com>
2018-06-17 15:56:42 +0800

    show all users in tweets_count number

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,7 +2,7 @@ class Tweet < ApplicationRecord
   validates_presence_of :description
   validates_length_of :description, maximum: 140
 
-  belongs_to :user
+  belongs_to :user, counter_cache: true
 
   has_many :replies, dependent: :destroy
 
