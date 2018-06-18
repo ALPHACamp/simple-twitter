f05e8922428804b37bd177600d4b501c8428e2a3
Revi <s1492e034@gmail.com>
2018-06-18 15:19:53 +0800

    fix followship create action: user cannot follow self

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -1,14 +1,17 @@
 class FollowshipsController < ApplicationController
   def create #follow
-    @followship = current_user.followships.build(following_id: params[:following_id])
+    following = User.find(params[:following_id])
 
-    if @followship.save
-      flash[:notice] = "Successfully followed"
-      redirect_back(fallback_location: root_path)
-    else
-      flash[:alert] = @followship.errors.full_messages.to_sentence
-      redirect_back(fallback_location: root_path)
+    if current_user != following
+      @followship = current_user.followships.build(following_id: params[:following_id])
+
+      if @followship.save
+        flash[:notice] = "Successfully followed"
+      else
+        flash[:alert] = @followship.errors.full_messages.to_sentence
+      end
     end
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy #unfollow
