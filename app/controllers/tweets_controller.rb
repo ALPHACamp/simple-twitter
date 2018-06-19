e4377229579462bb656b5cdb8f00b5aa4a9bc81a
xs1523c <xs1523c@gmail.com>
2018-06-20 00:20:06 +0800

    bootstrap index page

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,18 +2,18 @@ class TweetsController < ApplicationController
 
   def index
     @users # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
-    @tweets = Tweet.all
+    @tweets = Tweet.order(created_at: :desc )
     @tweet = Tweet.new
   end
 
   def create
-    @tweet = Tweet.create!(tweet_params)
+    @tweet = Tweet.create(tweet_params)
     if @tweet.save
       flash[:notice]="您已經成功發佈一篇推特"
       redirect_to root_path
     else
-      flash[:alert]="推特發佈失敗"
-      render :new
+      flash[:alert]="推特內容不能空白"
+      redirect_to root_path
     end
   end
 
