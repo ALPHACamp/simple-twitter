502c0e2097808978b2a21a4b71817c4469c69e24
Jimmy <du20783@gmail.com>
2018-06-19 13:05:33 +0800

    setup css

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -2,11 +2,26 @@ class RepliesController < ApplicationController
 
   def index
     @tweet = Tweet.find(params[:tweet_id])
+    @replies = @tweet.replies
     @reply = Reply.new
     @user = current_user
   end
 
   def create
+    @tweet = Tweet.find(params[:tweet_id])
+    @reply = @tweet.replies.build(reply_params)
+    @reply.user = current_user
+    if @reply.save
+      flash[:notice] = "successfully created reply"
+    else
+      flash[:alert] = @reply.errors.full_messages.to_sentence
+    end
+    redirect_back(fallback_location: root_path)
   end
 
+  private
+
+  def reply_params
+    params.require(:reply).permit(:comment)
+  end
 end
