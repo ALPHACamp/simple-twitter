d3eb6ffffd7cfb883176cca32d47344182c107da
xs1523c <xs1523c@gmail.com>
2018-06-20 17:18:22 +0800

    beautify user_tweet user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,21 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to root_path
+    end
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice]="資料成功被編輯"
+      redirect_to tweets_user_path(current_user)
+    else
+      flash[:alert]="未編輯成功，名字不能為空白"
+      render :edit
+    end
   end
 
   def followings
@@ -21,6 +33,11 @@ class UsersController < ApplicationController
 
   def likes
     @likes # 基於測試規格，必須講定變數名稱
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:avatar,:introduction,:name)
   end
 
 end
