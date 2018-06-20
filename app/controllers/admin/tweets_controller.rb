294db15477dbdb6bdfcdcc30802d35b97d4daf3c
Jimmy <du20783@gmail.com>
2018-06-20 23:12:46 +0800

    add Admin::BaseController and move authenticate_admin here

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -1,8 +1,7 @@
 class Admin::TweetsController < Admin::BaseController
-    before_action :authenticate_user!
-    before_action :authenticate_admin
+
   def index
-    @tweets = Tweet.page(params[:page]).per(10)    
+    @tweets = Tweet.page(params[:page]).per(10)
   end
 
   def allusers
@@ -16,12 +15,5 @@ class Admin::TweetsController < Admin::BaseController
     flash[:alert] = "tweet was deleted"
   end
 
-  private
 
-  def authenticate_admin
-    unless current_user.admin?
-      flash[:alert] = "Not allow"
-      redirect_to root_path
-    end
-  end
 end
