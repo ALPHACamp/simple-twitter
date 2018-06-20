0ff465239f1d3d39ee39ad92c36152b05cffa367
Jimmy <du20783@gmail.com>
2018-06-20 23:03:10 +0800

    add user name to registration form

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :liked_tweets, through: :likes, source: :tweet
 
-  validates_presence_of :name
+  validates_presence_of  :name, presnece: true, uniqueness: true
   # 需要 app/views/devise 裡找到樣板，加上 name 屬性
   # 並參考 Devise 文件自訂表單後通過 Strong Parameters 的方法
   # 加上驗證 name 不能重覆 (關鍵字提示: uniqueness)
