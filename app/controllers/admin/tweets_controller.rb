05b8b0b9c1b9ea800f1117bac887de50992dc85a
liuchunyueh <liuchunyueh@gmail.com>
2018-06-24 20:17:59 +0800

    link user and tweet model

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -4,5 +4,14 @@ class Admin::TweetsController < Admin::BaseController
   end
 
   def destroy
+    @recent_tweet.destroy 
+
+    if @recent_tweet.error.any?
+      flash[:alert] = @recent_tweet.errors.full_messages.to_sentense
+    else
+      flash[:notice] = "tweet was successfully deleted!!!"
+    end
+    
+  redirect_to admin_tweets_path      
   end
 end
