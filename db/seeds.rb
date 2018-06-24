9a2b2456e997d33f7cb2c0bb885c45f865beb5f8
Chiehyun <jealic@gmail.com>
2018-06-24 10:28:26 +0800

    set up db:seed for an admin and a commoner & manually add views/tweets and views/admin/tweets&users folder

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(email: "admin@ccc.com", password: "123456", role: "admin", name: "Admin is Me!", introduction: "I love coding, and coding loves me!!")
+puts "Default admin created."
+
+User.create(email: "jess@ccc.com", password: "123456", name: "Jess", introduction: "My name is Jess. I want to be a model when I grow up. I brush my teeth twice a day.")
+puts "Default commoner created."
