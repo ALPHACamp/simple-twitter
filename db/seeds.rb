7307a5c1c642d77c591fbce46da4405d6ec2979f
Stanley <Stanley741025@gmail.com>
2018-06-29 12:32:08 +0800

    Add admin user in seed.rb and remove fake user destroy action

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+user = User.create(
+  name: "Admin",
+  email: "admin@admin.com",
+  password: "111111",
+  introduction: FFaker::Lorem::sentence(30),
+  avatar: file
+)
+puts "#{user.name} created"
