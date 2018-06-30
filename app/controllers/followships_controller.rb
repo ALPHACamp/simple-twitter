99b8e166be44c56efe8c3722391265cb197fbc35
Stanley <Stanley741025@gmail.com>
2018-06-30 14:32:56 +0800

    setup create and destroy action in followships controller and modify follow/unfollow btn in tweets.html.erb

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -1,7 +1,22 @@
 class FollowshipsController < ApplicationController
+
   def create
+      @followship = current_user.followships.new(following_id: params[:following_id])
+
+      if @followship.save
+        flash[:notice] = "Successfully followed"
+        redirect_back(fallback_location: root_path)
+      else
+        flash[:alert] = @followship.errors.full_messages.to_sentence
+        redirect_back(fallback_location: root_path)
+      end
   end
 
   def destroy
+    # 當使用 where 方法時，無論資料有幾筆，他都會回傳一組物件集合（也就是陣列）
+    @followship =current_user.followships.where(following_id: params[:id])
+    @followship.destroy_all
+    flash[:alert] = "followship destroyed"
+    redirect_back(fallback_location: root_path)
   end
 end
