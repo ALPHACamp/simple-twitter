9116f966793fc033bc01abb8eb23e833eababf7f
Chiehyun <jealic@gmail.com>
2018-07-01 11:45:04 +0800

    fixed the tweets/index page and show replies/index page, not yet the reply textarea

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,9 +1,19 @@
 class RepliesController < ApplicationController
 
   def index
+    @tweet = Tweet.find(params[:tweet_id])
+    @replies = @tweet.replies
+    @user = @tweet.user
+    
   end
 
   def create
+  end
+
+  private
+
+  def reply_params
+    params.require(:reply).permit(:comment)
   end
 
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,18 +3,19 @@ class TweetsController < ApplicationController
   def index
     @users # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
     @users = User.order(:name).limit(10)
-    @tweets = Tweet.order(created_at: :desc).page(params[:page]).per(10)
+    @tweets = Tweet.order(created_at: :desc).page(params[:id]).per(10)
     @tweet = Tweet.new
   end
 
   def create
-    @tweet = current_user.tweets.new(tweet_params)
+    @tweet = Tweet.new(tweet_params)
+    @tweet.user = current_user
     if @tweet.save
-      flash[:notice] = "You've updated with a tweet."
+      flash[:notice] = "Tweet was successfully added."
       redirect_to tweets_path
     else
-      @tweets = Tweet.all
-      @users = User.all
+      @tweet = Tweet.all
+
       render :index
     end
   end
