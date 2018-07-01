c42660662181ef38da331f36ce7af3f4c3885702
Chiehyun <jealic@gmail.com>
2018-07-01 14:20:30 +0800

    add comment textarea in replies index page

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -2,12 +2,22 @@ class RepliesController < ApplicationController
 
   def index
     @tweet = Tweet.find(params[:tweet_id])
-    @replies = @tweet.replies
+    @reply = Reply.new
+    @replies = @tweet.replies.order(created_at: :desc)
     @user = @tweet.user
     
   end
 
   def create
+    @tweet = Tweet.find(params[:tweet_id])
+    @reply = @tweet.replies.new(reply_params)
+    @reply.user = current_user
+    if @reply.save
+      redirect_to tweet_replies_path(@tweet)
+    else
+      flash[:alert] = "Comments can't be blank!"
+      redirect_back(fallback_location: request.referrer)
+    end
   end
 
   private
