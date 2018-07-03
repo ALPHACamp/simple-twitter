f1c9df00b743eca9f38689ab08a3e65d00a26b58
Stanley <Stanley741025@gmail.com>
2018-07-03 14:47:32 +0800

    modify replies controller and setup replies index page. The current tweet do not get yet in index page

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,23 +1,25 @@
 class RepliesController < ApplicationController
 
   def index
+    @user = current_user
     @tweet = Tweet.find(params[:tweet_id])
-    @replies = @tweet.replaies.all.order(created_at: :desc)
+    @reply = Reply.new
+    @replies = @tweet.replies.all.order(created_at: :desc)
   end
 
   def create
     @tweet = Tweet.find(params[:tweet_id])
-    @reply = @tweet.replies.new(reply_params)
+    @reply = @tweet.replies.build(reply_params)
     @reply.user = current_user
     @reply.save!
-    redirect_to tweet_replies_path(@reply)
+    redirect_to tweet_replies_path(@tweet)
   end
 
 
-private
+  private
 
   def reply_params
-    params.requires(:reply).permit(:comment)
+    params.require(:reply).permit(:comment)
   end
 
 
