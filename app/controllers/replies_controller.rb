e13ed085c1d97b0d5cd29cd878692d6cc5aab8cc
Stanley <Stanley741025@gmail.com>
2018-07-03 12:49:56 +0800

    setup replies controller

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,9 +1,24 @@
 class RepliesController < ApplicationController
 
   def index
+    @tweet = Tweet.find(params[:tweet_id])
+    @replies = @tweet.replaies.all.order(created_at: :desc)
   end
 
   def create
+    @tweet = Tweet.find(params[:tweet_id])
+    @reply = @tweet.replies.new(reply_params)
+    @reply.user = current_user
+    @reply.save!
+    redirect_to tweet_replies_path(@reply)
   end
+
+
+private
+
+  def reply_params
+    params.requires(:reply).permit(:comment)
+  end
+
 
 end
