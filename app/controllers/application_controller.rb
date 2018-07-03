ef3c7d2cfbecead30f6b4f4ae0c5647a2134fe4e
liuchunyueh <liuchunyueh@gmail.com>
2018-07-03 23:03:59 +0800

    set before_action :authenticate_user! in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
 
 
   # 請參考 Devise 文件自訂表單後通過 Strong Parameters 的方法
