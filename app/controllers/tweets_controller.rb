66dfb0e57de394ac081aef2950427271942fa038
Stanley <Stanley741025@gmail.com>
2018-07-04 17:54:30 +0800

    Add .order action in tweet index page. And remove .order action in tweets controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,7 +2,9 @@ class TweetsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @users = User.order(followers_count: :desc).limit(10)# 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
+    #把order(followers_count: :desc)放在 index page 這樣就能同步更新。當在首頁按下follow的時候。
+    @users = User.limit(10)# 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
+
     @tweet = Tweet.new
     @tweets = Tweet.all.order(created_at: :desc)
 
