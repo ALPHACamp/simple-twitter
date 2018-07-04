7ae6df4e8aaa64b4e54b75ac01aa2bbc9622bbd4
yeddachuang <YeddaChuang@gmail.com>
2018-07-04 22:13:12 +0800

    add before_action authenticate_user! and authenticate_admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,4 +19,9 @@ class User < ApplicationRecord
   has_many :inverse_followships, class_name: "Followship", foreign_key: "following_id"
   has_many :followers, through: :inverse_followships, source: :user
   has_many :likes
+
+  def admin?
+    self.role == "admin"
+  end
+  
 end
