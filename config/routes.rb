408b029b03e66958f772032612054cfb017ea4cd
Yedda <YeddaChuang@gmail.com>
2018-07-04 20:26:59 +0800

    set routes for front-end and back-end

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,41 @@ Rails.application.routes.draw do
   devise_for :users
 
   # 請依照專案指定規格來設定路由
+  resources :tweets, only: [:index, :create] do
+    # tweets#index
+    # tweets#create
 
+    member do
+      post :like   # tweets#like
+      post :unlike # tweets#unlike
+    end
+
+    resources :replies, only: [:index, :create]
+    # replies#index
+    # replies#create
+    
+  end
+
+  resources :users, only: [:edit] do
+    member do
+      get :tweets     # users#tweets
+      get :followings # users#followings
+      get :followers  # users#followers
+      get :likes      # users#likes
+      # get :edit       # users#edit
+    end
+  end
+
+  resources :followships, only: [:create, :destroy]
+  # followships#create
+  # followships#destroy
+
+  root "tweets#index"
+
+  namespace :admin do
+    resources :tweets, only: [:index, :destroy]
+    resources :users, only: [:index]
+
+    root "tweets#index"
+  end
 end
