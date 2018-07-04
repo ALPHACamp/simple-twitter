4689ef762c8df5a7085e14fb799279f1a0199351
yeddachuang <YeddaChuang@gmail.com>
2018-07-05 00:11:56 +0800

    add name column for user signup and edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
   
   # 請參考 Devise 文件自訂表單後通過 Strong Parameters 的方法
@@ -12,4 +13,12 @@ class ApplicationController < ActionController::Base
       redirect_to root_path
     end
   end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
 end
