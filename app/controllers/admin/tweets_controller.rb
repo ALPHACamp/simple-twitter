8ae3368903b5a3efd2a888d3a3f775c61d0e4393
Stanley <Stanley741025@gmail.com>
2018-07-05 11:19:28 +0800

    Add destroy method in tweets_controller.rb

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -16,5 +16,9 @@ class Admin::TweetsController < Admin::BaseController
   end
 
   def destroy
+    @tweet = Tweet.find(params[:id])
+    @tweet.destroy
+    redirect_to admin_root_path
+    flash[:alert] = "tweet was deleted"
   end
 end
