abaa817ecd8b59e119e37ebd0cdac85e6881d1c0
Stanley <Stanley741025@gmail.com>
2018-07-05 10:42:50 +0800

    move authenticate_admin to Admin::BaseController

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -1,6 +1,4 @@
 class Admin::TweetsController < Admin::BaseController
-  before_action :authenticate_user!
-  before_action :authenticate_admin
 
 
   def index
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,11 +13,5 @@ class ApplicationController < ActionController::Base
 
   end
 
-  private
-    def authenticate_admin
-      unless current_user.admin?
-        flash[:alert] = "Not allow!!"
-        redirect_to root_path
-      end
-    end
+  
 end
