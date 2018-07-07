14acd2e228c2fba4666e721cc054817aed917047
Yun Ting Hung <s9923554@gmail.com>
2018-07-07 15:48:55 +0800

    update replies controller.rb

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,9 +1,22 @@
 class RepliesController < ApplicationController
 
   def index
+    @tweet = Tweet.find(params[:tweet_id])
+    @replies = @tweet.replies.order(created_at: :desc)
+    @reply = Reply.new
   end
 
   def create
+    @tweet = Tweet.find(params[:tweet_id])
+    @reply = @tweet.replies.build(reply_params)
+    @reply.user = current_user
+    @reply.save!
+    redirect_back(fallback_location: root_path)
+  end
+
+  private
+  def reply_params
+    params.require(:reply).permit(:comment)
   end
 
 end
