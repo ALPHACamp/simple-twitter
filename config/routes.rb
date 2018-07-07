75e2757e5d66406d900b1ebe307ab2de99497ce2
Yun Ting Hung <s9923554@gmail.com>
2018-07-07 16:00:59 +0800

    update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,16 @@ Rails.application.routes.draw do
 
   # 請依照專案指定規格來設定路由
 
+  resources :tweets, only: [:index, :create] do
+    resources :replies, only: [:index, :create]
+  end
+
+  resources :users, only: [:edit, :update, :tweets]
+
+  root "tweets#index"
+
+  namespace :admin do
+    resources :tweets, only: [:index, :destroy]
+    resources :users, only: [:index]
+  end
 end
