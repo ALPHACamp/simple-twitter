b5716365d11361f4ba2b074bb899f3fc529cfcde
Yun Ting Hung <s9923554@gmail.com>
2018-07-07 15:50:30 +0800

    update tweet controller.rb

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,16 +1,31 @@
 class TweetsController < ApplicationController
-
+  before_action :authenticate_user!
   def index
-    @users # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
+    @tweets = Tweet.order(created_at: :desc)
+    @tweet = Tweet.new
+    @users = User.all.order(follower_count: :desc).limit(10)
   end
 
   def create
+    @user = current_user
+    @tweet = @user.tweets.build(tweet_params)
+    if @tweet.save
+      redirect_to root_path
+    else
+      render :index
+      flash[:alert] = "not able to tweet"
+    end
   end
 
   def like
   end
 
   def unlike
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:description)
   end
 
 end
