99d2abd62991b945f9995ef370c0e5d2a511f272
liuchunyueh <liuchunyueh@gmail.com>
2018-07-08 13:47:11 +0800

    user can post tweet

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,15 +1,26 @@
 class TweetsController < ApplicationController
   def index
    # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
+   @tweet = Tweet.new
   end
 
-  def create
+  def create 
+    @tweet = current_user.tweets.build(description_params)
+    @tweet.save!
+    redirect_to root_path
   end
 
   def like
   end
 
   def unlike
+  end
+
+  private
+
+  def description_params
+    params.require(:tweet).permit(:description)
+    
   end
 
 end
