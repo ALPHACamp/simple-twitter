d99adc033a0730744d27f489326ab4788e244a01
yeddachuang <YeddaChuang@gmail.com>
2018-07-10 00:45:14 +0800

    add create method in tweets_controllers

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -7,6 +7,17 @@ class TweetsController < ApplicationController
   end
 
   def create
+    @users = User.all.order(followers_count: :desc).limit(10) # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
+    @tweets = Tweet.all.order(created_at: :desc)
+    @tweet = current_user.tweets.new(tweet_params)
+
+    if @tweet.save
+      flash[:notice] = "tweet was successfully created"
+      redirect_to tweets_path
+    else
+      # @tweet = Category.all
+      render :index
+    end
   end
 
   def like
@@ -15,4 +26,9 @@ class TweetsController < ApplicationController
   def unlike
   end
 
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:description)
+  end
 end
