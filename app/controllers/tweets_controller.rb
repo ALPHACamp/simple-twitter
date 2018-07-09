621f1ebe9e639409bf406dec613df19082d13ad0
yeddachuang <YeddaChuang@gmail.com>
2018-07-10 00:38:12 +0800

    add page /tweets for all users

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,9 @@
 class TweetsController < ApplicationController
 
   def index
-    @users # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
+    @users = User.all.order(followers_count: :desc).limit(10) # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
+    @tweets = Tweet.all.order(created_at: :desc)
+    @tweet = current_user.tweets.new()
   end
 
   def create
