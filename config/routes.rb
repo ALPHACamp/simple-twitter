04a4f3644615c98255b4f38a3a29ef2b25879c50
Yun Ting Hung <s9923554@gmail.com>
2018-07-09 20:44:05 +0800

    add users#likes, tweets#like, tweets#unlike routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
 
   resources :tweets, only: [:index, :create] do
     resources :replies, only: [:index, :create]
+    member do
+      post 'like'
+      post 'unlike'
+    end
   end
 
   resources :users, only: [:edit, :update] do
@@ -13,6 +17,7 @@ Rails.application.routes.draw do
       get 'tweets'
       get 'followers'
       get 'followings'
+      get 'likes'
     end
   end
 
