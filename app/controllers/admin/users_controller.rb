69b56168752db8da80b99e1256c4ad3f1c15d3f8
Penciler <eraser41@livemail.tw>
2018-07-11 07:40:22 +0800

    change admin user index for debug

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,7 +2,8 @@ class Admin::UsersController < Admin::BaseController
   before_action :authenticate_user!
   before_action :authenticate_admin
   def index
-    @users=User.order(tweets_count: :desc)
+    #@users=User.order(tweets_count: :desc)
+    @users=User.all
     @user=current_user
   end
 end
