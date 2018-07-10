19c056fe99389d17d962a0cb645247e1f5a6a479
yeddachuang <YeddaChuang@gmail.com>
2018-07-10 22:56:16 +0800

    add followships#follow and followships#destroy method

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -1,7 +1,24 @@
 class FollowshipsController < ApplicationController
   def create
+    @followship = current_user.followships.build(following_id: params[:following_id])
+
+    if @followship.save
+      flash[:notice] = "Successfully followed"
+    else
+      flash[:alert] = @followship.errors.full_messages.to_sentence
+    end
+
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
+    @followship = current_user.followships.where(following_id: params[:id]).first
+    if @followship
+      @followship.destroy
+      flash[:alert] = "Followship destroyed"
+    else
+      flash[:alert] = "No followship"
+    end
+    redirect_back(fallback_location: root_path)
   end
 end
--- app/models/followship.rb
+++ app/models/followship.rb
@@ -1,7 +1,14 @@
 class Followship < ApplicationRecord
   validates :following_id, uniqueness: { scope: :user_id }
+  validate :cannot_follow_itself
 
   belongs_to :user, counter_cache: :followers_count
   belongs_to :following, class_name: "User", counter_cache: :followings_count
   # belongs_to :follower, class_name: "User", counter_cache: :followers_count
+
+  def cannot_follow_itself
+    if user_id == following_id
+      errors.add(:user_id, "cannot follow itself")
+    end
+  end
 end
