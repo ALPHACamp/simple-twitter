4f0619d0855b6110dc6f73607549b957a6b2ca0c
gary80212@gmail.com <gary80212@gmail.com>
2018-07-11 00:02:09 +0800

    add tweet index and view and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
 
-  # 請依照專案指定規格來設定路由
-
+  resources :tweets
+  root "tweets#index"
 end
