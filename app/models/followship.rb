70c1dc938c7b7678e24cf531b7b7b61464233395
yeddachuang <YeddaChuang@gmail.com>
2018-07-11 17:58:40 +0800

    modify for counter_cache followings_count and followers_count

--- app/models/followship.rb
+++ app/models/followship.rb
@@ -2,10 +2,9 @@ class Followship < ApplicationRecord
   validates :following_id, uniqueness: { scope: :user_id }
   validate :cannot_follow_itself
 
-  belongs_to :user, counter_cache: :followers_count
-  belongs_to :following, class_name: "User", counter_cache: :followings_count
-  # belongs_to :follower, class_name: "User", counter_cache: :followers_count
-
+  belongs_to :user, counter_cache: :followings_count
+  belongs_to :following, class_name: "User", counter_cache: :followers_count
+  
   def cannot_follow_itself
     if user_id == following_id
       errors.add(:user_id, "cannot follow itself")
