b200c4c164561f675334117ab4aec8cc4cc8bdb8
Yun Ting Hung <s9923554@gmail.com>
2018-07-11 10:49:29 +0800

    def likes method in users controller and create likes.html.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,7 +32,8 @@ class UsersController < ApplicationController
   end
 
   def likes
-    @likes # 基於測試規格，必須講定變數名稱
+    @user = User.find(params[:id])
+    @likes = @user.liked_tweets.all
   end
 
   private
