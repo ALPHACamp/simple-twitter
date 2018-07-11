6c07be71185ba2b723f3cd40d245f26c7acef196
Yun Ting Hung <s9923554@gmail.com>
2018-07-11 17:23:27 +0800

    add tweets_count to user model and order by tweets_count on admin/users index page

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,7 +3,7 @@ class Admin::UsersController < Admin::BaseController
   before_action :authenticate_admin
   
   def index
-    @users = User.all
+    @users = User.order(tweets_count: :desc)
   end
 
   private 
