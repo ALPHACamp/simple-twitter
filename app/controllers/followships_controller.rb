e2263f5410578ec46fd2450a1829a2b5d338de77
liuchunyueh <liuchunyueh@gmail.com>
2018-07-12 00:48:26 +0800

    add create action for followship record

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -1,5 +1,14 @@
 class FollowshipsController < ApplicationController
   def create
+    @followship = current_user.followships.build(following_id: params[:following_id])
+
+    if @followship.save
+      flash[:notice] = "Successfully followed"
+      redirect_back(fallback_location: root_path)
+    else
+      flash[:alter] = @followship.errors.full_messages.to_sentence
+      redirect_back(fallback_location: root_path)  
+    end  
   end
 
   def destroy
