0769f0a13485a5755b7a17ae4591a18aefd95327
Yun Ting Hung <s9923554@gmail.com>
2018-07-11 20:59:47 +0800

    correct error which user shall not able to follow itself

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -1,10 +1,11 @@
 class FollowshipsController < ApplicationController
   def create
     @followship = current_user.followships.build(following_id: params[:following_id])
-    if @followship.save
+    if @followship.user_id != @followship.following_id
+      @followship.save
       flash[:notice] = "Successfully followed"
       redirect_back(fallback_location: root_path)
-    else
+    else 
       flash[:alert] = @followship.errors.full_messages.to_sentence
       redirect_back(fallback_location: root_path)
     end
