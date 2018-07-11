945cd24552c0cf1f672385ce96ad7a5745f19c6c
yeddachuang <YeddaChuang@gmail.com>
2018-07-11 17:09:13 +0800

    modify tweets#create in tweets_controller to pass redirect_to tests

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,34 +3,31 @@ class TweetsController < ApplicationController
   def index
     @users = User.all.order(followers_count: :desc).limit(10) # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
     @tweets = Tweet.all.order(created_at: :desc)
-    @tweet = current_user.tweets.new()
+    @tweet = current_user.tweets.new
   end
 
   def create
-    @users = User.all.order(followers_count: :desc).limit(10) # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
-    @tweets = Tweet.all.order(created_at: :desc)
     @tweet = current_user.tweets.new(tweet_params)
 
     if @tweet.save
       flash[:notice] = "tweet was successfully created"
-      redirect_to tweets_path
     else
-      # @tweet = Category.all
-      render :index
+      flash[:alert] = "tweet should be less than 140 words"
     end
+    redirect_to tweets_path
   end
 
   def like
     tweet = Tweet.find(params[:id])
     tweet.likes.create!(user: current_user)
-    redirect_back(fallback_location: root_path)
+    redirect_back(fallback_location: tweets_path)
   end
 
   def unlike
     tweet = Tweet.find(params[:id])
     likes = Like.where(tweet: tweet, user: current_user)
     likes.destroy_all
-    redirect_back(fallback_location: root_path)
+    redirect_back(fallback_location: tweets_path)
   end
 
   private
