6a541722acd086e377534d7ab3be99ef52f04bfe
liuchunyueh <liuchunyueh@gmail.com>
2018-07-11 22:30:19 +0800

    add tweet#index tweets content

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,9 @@
 class TweetsController < ApplicationController
   def index
-   # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
+   @users # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
    @tweet = Tweet.new
+   @index_post = Tweet.order(created_at: :desc).limit(15)
+
   end
 
   def create 
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,16 @@ Rails.application.routes.draw do
   devise_for :users
 
   # 請依照專案指定規格來設定路由
-  resources :tweets, only: [:index, :create]
+  resources :tweets, only: [:index, :create] do
+    member do 
+      get :replies
+    end 
+  end
+
   root "tweets#index"
 
   namespace :admin do
-    resources :tweets, only: [:index, :destroy]  #index看見站內所有推播、destroy刪除其他使用者的推文
+    resources :tweets, only: [:index, :destroy]  #index看見站內所有推播、destroy刪除其他使用者的推文 
     resources :users, only: [:index] # user#index 看見站內所有使用者
   end 
 
