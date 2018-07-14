fc7f05ebd680e7938ba06235bb6bba3fa430e3ef
Penciler <eraser41@livemail.tw>
2018-07-14 13:51:56 +0800

    rank user

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,8 +2,8 @@ class Admin::UsersController < Admin::BaseController
   before_action :authenticate_user!
   before_action :authenticate_admin
   def index
-    #@users=User.order(tweets_count: :desc)
-    @users=User.all
+    @users=User.order(tweets_count: :desc)
+    #@users=User.all
     @user=current_user
   end
 end
