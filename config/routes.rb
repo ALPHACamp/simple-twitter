c842b9fb876cf95261a8de646eebe20d92924eab
gary80212@gmail.com <gary80212@gmail.com>
2018-07-16 00:24:42 +0800

    setup reply routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   namespace :admin do
+    resources :tweets, only: [:index, :destroy]
     root "tweets#index"
   end
-  namespace :admin do
-    resources :tweets
-    root "tweets#index"
+  resources :tweets do
+    resources :replys, only: [:create]
   end
-  resources :tweets
   root "tweets#index"
 end
