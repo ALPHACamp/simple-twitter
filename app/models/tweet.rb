6d7f592d85003265245ff96b8c9bf967fa12537e
liuchunyueh <liuchunyueh@gmail.com>
2018-07-16 22:02:41 +0800

    add relation amoung user & like & tweet in tweet model

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -4,7 +4,9 @@ class Tweet < ApplicationRecord
   belongs_to :user
   has_many :replies, dependent: :destroy
 
+  # 「tweet被很多使用者喜歡」的多對多關係
   has_many :likes, dependent: :destroy
+  has_many :liked_users, through: :likes, source: :user
 
   def is_liked?(user)
     self.likes.include?(user)
