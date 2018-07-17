c576469bc6e33ed194ccef7c8b62633ed08e0c3c
tsaichungyi <hata.s520@gmail.com>
2018-07-17 17:15:57 +0800

    add user edit html

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,21 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to root_path
+    end
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice]="Profiles edit succes"
+      redirect_to tweets_user_path(current_user)
+    else
+      flash[:alert]="Profiles edit error"
+      render :edit
+    end
   end
 
   def followings
@@ -23,4 +35,9 @@ class UsersController < ApplicationController
     @likes # 基於測試規格，必須講定變數名稱
   end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:avatar,:introduction,:name)
+  end
 end
