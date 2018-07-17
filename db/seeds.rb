f8edd6202a31fc6105e73e2acefd82ddfb3a2207
Wendy <wendy.hj.wu@gmail.com>
2018-07-17 18:27:46 +0800

    setup seed data for default admin user and test user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#Default admin
+User.create(email: "admin@test.com", name: "Admin", password: "123456", role: "admin")
+puts "Default admin created!"
+
+#Default user
+User.create(email: "test@test.com", name: "Tester", password: "123456")
+puts "Default test account created"
