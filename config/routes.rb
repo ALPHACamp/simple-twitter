613c6eb49e1e94d0d8559d5c799f561c77583755
Henry <henry81325@gmail.com>
2018-07-17 13:09:02 +0800

    add twitter index action and view for admin

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
 
-  # 請依照專案指定規格來設定路由
+ # 請依照專案指定規格來設定路由
+
+  root "tweets#index"
+
+
+  namespace :admin do
+    resources :tweets
+    root "tweets#index"
+  end 
 
 end
