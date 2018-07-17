546d9ed3c46270796581836f2bcc6f33834d26d5
tsaichungyi <hata.s520@gmail.com>
2018-07-17 16:12:37 +0800

    add style.scss and add tweets index create action method,add tweet index of user

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,10 +2,22 @@ class TweetsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @users # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
+    @tweets = Tweet.all
+    @tweet = Tweet.new
+    # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
+    @users = User.all
   end
 
   def create
+    @tweet = Tweet.new(tweet_params)
+    @tweet.user = current_user
+    if @tweet.save
+      flash[:notice] = "tweet was successfully created"
+      redirect_to root_path
+    else
+      flash[:notice] = "tweet content can't blank"
+      render :index
+    end
   end
 
   def like
@@ -14,4 +26,9 @@ class TweetsController < ApplicationController
   def unlike
   end
 
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:description)
+  end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,4 +3,6 @@ class Tweet < ApplicationRecord
 
   belongs_to :user
 
+  validates_presence_of :description
+
 end
