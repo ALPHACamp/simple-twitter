f71c0a415742afaf3e982695c4f0eb21aa5bbc97
tsaichungyi <hata.s520@gmail.com>
2018-07-17 22:27:14 +0800

    add tweets of admin and fix page

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -1,7 +1,15 @@
 class Admin::TweetsController < Admin::BaseController
+  before_action :authenticate_user!
+  before_action :authenticate_admin
+
   def index
+    @tweets = Tweet.all
   end
 
   def destroy
+    @tweet = Tweet.find(params[:id])
+    @tweet.destroy
+    flash[:alert] = "Tweet success delete"
+    redirect_back(fallback_location: root_path)
   end
 end
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,5 @@
 class Admin::UsersController < Admin::BaseController
   def index
+    @user = User.all
   end
 end
