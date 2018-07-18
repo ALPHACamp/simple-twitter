ab194d622f4d016bb3a4b45564ff2a373e8a0674
Wendy <wendy.hj.wu@gmail.com>
2018-07-18 14:53:11 +0800

    add like/unlike action on Tweet Controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -19,9 +19,16 @@ class TweetsController < ApplicationController
   end
 
   def like
+    @tweet = Tweet.find(params[:id])
+    @tweet.likes.create!(user: current_user)
+    redirect_back(fallback_location: root_path)
   end
 
   def unlike
+    @tweet = Tweet.find(params[:id])
+    likes = Like.where(tweet: @tweet, user: current_user)
+    likes.destroy_all
+    redirect_back(fallback_location: root_path)
   end
 
   private
