56a5f0f2f8c81498681146b886d937e32b94b44e
Kafka Tzeng <freefish0308@gmail.com>
2018-07-19 13:31:02 +0800

    add tweets index action and view for public users

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
 
+  root "tweets#index"
+
   # 請依照專案指定規格來設定路由
 
 end
