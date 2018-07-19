e6fea364e0630fabe4cb2ee0eaaaaf5f8a4e87e9
Wendy <wendy.hj.wu@gmail.com>
2018-07-20 00:12:12 +0800

    add reply page, user can add reply in reply page

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,9 +1,22 @@
 class RepliesController < ApplicationController
 
   def index
+    @tweet = Tweet.find(params[:tweet_id])
+    @user = current_user
+    @reply = Reply.new
   end
 
   def create
+    @tweet = Tweet.find(params[:tweet_id])
+    @reply = @tweet.replies.build(reply_params)
+    @reply.user = current_user
+    @reply.save!
+    redirect_back(fallback_location: tweet_replies_path)
   end
 
+  private
+
+  def reply_params
+    params.require(:reply).permit(:comment)
+  end
 end
