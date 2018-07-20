ee8e42175b18bd9c41d8c77572dcd64e94e55ce8
Henry <henry81325@gmail.com>
2018-07-20 11:22:33 +0800

    set tweet model/controller(依時間排序的顯示&限140字)

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,7 @@
 class TweetsController < ApplicationController
 
   def index # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
-    @users = User.order(followers_count: :desc).limit(10) 
+    @tweets = Tweet.order(updated_at: :desc).page(params[:page]).per(15)
     @tweet = Tweet.new
   end
 
@@ -18,5 +18,12 @@ class TweetsController < ApplicationController
 
   def unlike
   end
+
+  private 
+
+  def tweet_params
+    params.require(:tweet).permit(:description)
+  end
+
 
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,5 @@
 class Tweet < ApplicationRecord
-  validates_length_of :description, maximum: 140
+  validates_length_of :description, maximum: 140,:message => "限字140"
   belongs_to :user
 
 end
