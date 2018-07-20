3febaff8a8c9833401148ab7263a32cabb128eda
Cynthia <ex09190919@yahoo.com.tw>
2018-07-20 17:21:05 +0800

    set up routes and index page for admin and public user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
-
   # 請依照專案指定規格來設定路由
+  # 設定前台路由
+  root "tweets#index"
 
+  # 設定後台路由
+  namespace :admin do
+    root "tweets#index"
+  end
 end
