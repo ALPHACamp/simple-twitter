c7b0c5422e9e7a4f7995477d612e09de89275a5c
Allie <zxp800310@gmail.com>
2018-07-20 22:09:19 +0800

    setup all routes for simple-twitter

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,30 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
 
+  resources :tweets, only:[:index, :create] do
+    resources :replies, only:[:index, :create]
+    member do
+      post :like
+      post :unlike
+    end
+  end
+
+  resources :users, only:[:edit] do
+    member do
+      get :tweets
+      get :followings
+      get :followers
+      get :likes
+    end
+  end
+
+  resources :followships, only:[:create, :destroy]
+
   root "tweets#index"
 
   namespace :admin do
+    resources :tweets, only:[:index, :destroy]
+    resources :users, only:[:index]
     root "tweets#index"
   end
 
