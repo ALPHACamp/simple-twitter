6ecfa39dacd004e957b576640266d1dc5486d66f
yungcheng <yungcheng111023@gmail.com>
2018-07-22 09:41:00 +0800

    add user name to registration form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 
 
   # 請參考 Devise 文件自訂表單後通過 Strong Parameters 的方法
@@ -15,6 +24,5 @@ class ApplicationController < ActionController::Base
       redirect_to root_path
     end
   end
-
 
 end
