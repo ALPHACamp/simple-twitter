46bcfc080debabbedf4d6342a5c9c61c191048e4
yungcheng <yungcheng111023@gmail.com>
2018-07-23 05:38:09 +0800

    add input tweet box to index page

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,17 +5,27 @@ class TweetsController < ApplicationController
   def index
     @users = Tweet.page(params[:page]).per(10)
     # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
+    @tweet = Tweet.new
   end
 
   def create
-    @restaurant = Restaurant.find(params[:id])
-    
+    @tweet = Tweet.find(params[:user_id])
+    @tweet = @tweets.build(tweet_params)
+    @tweet.user = current_user
+    @tweet.save!
+    redirect_to tweet_path(@tweet)
   end
 
   def like
   end
 
   def unlike
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:content)
   end
 
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,7 @@
 class Tweet < ApplicationRecord
   validates_length_of :description, maximum: 140
+
+  belongs_to :user, optional: true
   
 
 end
