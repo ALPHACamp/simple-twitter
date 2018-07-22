1206f6f784d0cff608ec6a4618892e8c2507a10b
Zion <bone2057@gmail.com>
2018-07-23 07:00:10 +0800

    add user # followings action and using partial template for follow function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,18 @@ class UsersController < ApplicationController
   def tweets
    @user = User.find(params[:id])
    @tweet = Tweet.new
+   @tweets = @user.tweets.all
   end
 
 
 
   def edit
     @user = User.find(params[:id])
+
+    unless user = current_user
+      redirect_to user_path(@user)
+    end
+
   end
 
   def update
@@ -19,7 +25,8 @@ class UsersController < ApplicationController
   end
 
   def followings
-    @followings # 基於測試規格，必須講定變數名稱
+    @user = User.find(params[:id])
+    @followings = @user.followings.page(params[:page]).per(6) # 基於測試規格，必須講定變數名稱
   end
 
   def followers
