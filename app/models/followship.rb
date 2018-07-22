4233fe07f283e6b4a346c5d1bd31af803aa40e59
Wendy <wendy.hj.wu@gmail.com>
2018-07-22 13:39:28 +0800

    add validate to followship.rb to disallow follow self

--- app/models/followship.rb
+++ app/models/followship.rb
@@ -1,5 +1,17 @@
 class Followship < ApplicationRecord
   validates :following_id, uniqueness: { scope: :user_id }
+  validate :disallow_self_follow
+
   belongs_to :user
   belongs_to :following, class_name: "User"
+
+  private
+
+  # 自訂驗證：避免follow到自己
+  def disallow_self_follow
+    if user_id == following_id
+      errors[:following_id] << 'cannot follow self'
+    end
+  end
+
 end
