03687e9626ba2b8f79936a173527fc1c099762e8
yungcheng <yungcheng111023@gmail.com>
2018-07-22 19:21:40 +0800

    user can update the profile details

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,21 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update] 
 
   def tweets
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   def edit
     unless @user == current_user
-          redirect_to user_path(@user)
+      redirect_to user_path(@user)
     end
   end
 
   def update
+    @user.update(user_params)
+    redirect_to user_path(@user)
   end
 
   def followings
@@ -26,6 +28,16 @@ class UsersController < ApplicationController
 
   def likes
     @likes # 基於測試規格，必須講定變數名稱
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :avatar )
   end
 
 end
