31cfd08ba788e233af4791471a536ceca21334d9
Zion <bone2057@gmail.com>
2018-07-24 03:54:06 +0800

    add index, destroy action, and admin index page for admin

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -1,7 +1,17 @@
 class Admin::TweetsController < Admin::BaseController
   def index
+    @tweets = Tweet.all
   end
 
   def destroy
+    @tweet = Tweet.find(params[:id])
+    @tweet.destroy
+    redirect_to admin_tweets_path
+    flash[:alert] = "tweet has destroyed"
+
+  end
+  private
+  def tweet_params
+    params.require(:tweet).permit(:description, :user_id)
   end
 end
