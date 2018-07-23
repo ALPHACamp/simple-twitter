53fd230a96ad21bfdfa4c4b7f311edef304097f1
Wendy <wendy.hj.wu@gmail.com>
2018-07-23 15:10:45 +0800

    add before action authenticate_admin to admin base controller, execute authenticate_admin in any admin controller

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -1,5 +1,4 @@
 class Admin::TweetsController < Admin::BaseController
-  before_action :authenticate_admin
 
   def index
     @tweets = Tweet.all.order(created_at: :desc).page(params[:page]).per(10)
