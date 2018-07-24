c32b6bc2b9637d0432a0fca50aa3bc10991d3330
liuchunyueh <liuchunyueh@gmail.com>
2018-07-24 19:40:37 +0800

    edit the UI of IDreplies

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -2,9 +2,13 @@ class RepliesController < ApplicationController
 
   def index
     @reply = Reply.find(params[:tweet_id])
+    @replies = @reply.tweet.replies
+    @tweet = Tweet.find(params[:tweet_id])
+    @newReply = Reply.new
   end
 
   def create
+    
   end
 
 end
