a54d49f364563d91bce5244fdac6b883c08b77ae
liuchunyueh <liuchunyueh@gmail.com>
2018-07-25 06:13:45 +0800

    user can add reply on tweet#index page

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -2,13 +2,23 @@ class RepliesController < ApplicationController
 
   def index
     @reply = Reply.find(params[:tweet_id])
-    @replies = @reply.tweet.replies
+    @replies = @reply.tweet.replies.order(created_at: :desc)
     @tweet = Tweet.find(params[:tweet_id])
     @newReply = Reply.new
   end
 
   def create
-    
+    @tweet = Tweet.find(params[:tweet_id])
+    @reply = @tweet.replies.build(reply_params)
+    @reply.user = current_user
+    @reply.save!
+    redirect_to tweet_replies_path(@tweet)
+  end
+
+  private
+
+  def reply_params
+    params.require(:reply).permit(:comment)
   end
 
 end
