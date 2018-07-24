e93ac76cfab0fe659d985cb575bceac814ee7739
Henry <henry81325@gmail.com>
2018-07-24 11:42:00 +0800

    add like/unlike button & add tweets counnt on user model

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,10 +1,10 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:like, :unlike]
 
   def index
     @users  # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
     @tweet = Tweet.new
     @tweets = Tweet.all.order(created_at: :desc)
-
   end
 
   def create
@@ -17,13 +17,22 @@ class TweetsController < ApplicationController
     end
   end
 
-  def like
+ def like
+    @tweet.likes.create!(user: current_user)
+    redirect_back fallback_location: tweets_path
   end
 
   def unlike
+    like = Like.where(tweet: @tweet, user: current_user)
+    like.destroy_all
+    redirect_back fallback_location: tweets_path
   end
 
   private
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
+  end
 
   def tweet_params
     params.require(:tweet).permit(:description)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,8 @@ class UsersController < ApplicationController
   end
 
   def likes
-    @likes # 基於測試規格，必須講定變數名稱
+    @likes = @user.liked_tweets.order('likes.created_at desc')
+     # 基於測試規格，必須講定變數名稱
   end
 
   private
