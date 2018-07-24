3b9d3590f52f41ae58a4ec4eb81f2885650af30c
Zion <bone2057@gmail.com>
2018-07-25 05:57:46 +0800

    add control flow for users can't follow themselves

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -1,14 +1,19 @@
 class FollowshipsController < ApplicationController
 
   def create
-    @followship = current_user.followships.build(following_id: params[:following_id])
-      if @followship.save
-        flash[:notice] = "followed sucessfully"
-        redirect_back(fallback_location: root_path)
-      else
-        flash[:alert] = @followship.errors.full_messages.to_sentence
-        redirect_back(fallback_location: root_path)
-      end   
+    @user = User.find(params[:following_id])
+    if @user == current_user
+      flash[:alert] = "can't follow yourself"
+    else
+      @followship = current_user.followships.build(following_id: params[:following_id])
+        if @followship.save
+          flash[:notice] = "followed sucessfully"
+          redirect_back(fallback_location: root_path)
+        else
+          flash[:alert] = @followship.errors.full_messages.to_sentence
+          redirect_back(fallback_location: root_path)
+        end 
+    end  
   end
 
   def destroy
