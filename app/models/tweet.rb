246812b0f0d3303cddeb7182489445abf45a4544
Cynthia <ex09190919@yahoo.com.tw>
2018-07-25 09:15:45 +0800

    add limit the number of words (140) in tweet and cannot be blank

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,4 +2,8 @@ class Tweet < ApplicationRecord
   validates_length_of :description, maximum: 140
 # 建立user跟tweet的關聯，一個tweet只會有一個user
   belongs_to :user
+# tweet限制字數及空白
+  validates :user_id, presence: true
+  validates :description, presence: true, length: {maximum: 140}
+
 end
