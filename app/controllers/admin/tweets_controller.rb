47c81db86c4d2d2ea674b49ce1aa5007dda5927d
Kafka Tzeng <freefish0308@gmail.com>
2018-07-26 00:31:42 +0800

    controller/admin/index action & destroy action

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -2,8 +2,13 @@ class Admin::TweetsController < Admin::BaseController
   before_action :authenticate_user!
 
   def index
+    @tweets = Tweet.order(created_at: :desc)
   end
 
   def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+    flash[:alert] = "tweets delete!"
+    redirect_to admin_root_path
   end
 end
