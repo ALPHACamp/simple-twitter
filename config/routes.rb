f317017a565965fd2a4add73c4c4f77f4295a963
yungcheng <yungcheng111023@gmail.com>
2018-07-25 22:31:49 +0800

    setup routes for like/unlike

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,10 @@ Rails.application.routes.draw do
 
   resources :tweets, only: [:index, :create] do
     resources :replies, only:[:index, :create]
+      member do
+        post :like
+        post :unlike
+      end
   end
 
   resources :users, only: [:edit, :update]
