340d9bc93464e57297a742088d163db46caab79c
Zion <bone2057@gmail.com>
2018-07-27 06:49:01 +0800

    add index action to controller tweets for admin

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -1,17 +1,20 @@
 class Admin::TweetsController < Admin::BaseController
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.all.order(created_at: :desc)
   end
 
   def destroy
+    
     @tweet = Tweet.find(params[:id])
+    
     @tweet.destroy
-    redirect_to admin_tweets_path
+    redirect_to admin_root_path
     flash[:alert] = "tweet has destroyed"
 
   end
   private
   def tweet_params
-    params.require(:tweet).permit(:description, :user_id)
+    params.require(:tweet).permit(:description)
   end
+  
 end
