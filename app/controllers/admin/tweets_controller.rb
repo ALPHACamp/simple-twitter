d721007ed1ab7753c22d2b9b4cf195dd0234dcec
Henry <henry81325@gmail.com>
2018-07-26 11:47:42 +0800

    update tweet/user index page for admin

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -3,15 +3,13 @@ class Admin::TweetsController < Admin::BaseController
   before_action :authenticate_admin
 
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.all.order(created_at: :desc)
   end
 
-   def destroy
+  def destroy
     @tweet = Tweet.find(params[:id])
     @tweet.destroy
     flash[:alert] = "Tweet was deleted!"
-    redirect_to admin_root_path(t)
+    redirect_to admin_root_path
   end
-
-
 end
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,7 @@
 class Admin::UsersController < Admin::BaseController
 
   def index
-    @users = User.all
+    @users = User.order(tweets_count: :desc)
   end
 
 end
