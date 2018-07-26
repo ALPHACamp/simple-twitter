de134fbf5b74230b7addb0ed8a8840e83739cc28
Cynthia <ex09190919@yahoo.com.tw>
2018-07-26 22:17:37 +0800

    setup self-referential relationships within users table through followships

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,10 @@ class User < ApplicationRecord
   # 設定user跟tweet關聯，一個user可以有很多tweets
   has_many :tweets
 
+  # 設定追蹤關聯紀錄
+  has_many :followships, dependent: :destroy
+  has_many :followings, through: :followships
+
   # 需要 app/views/devise 裡找到樣板，加上 name 屬性
   # 並參考 Devise 文件自訂表單後通過 Strong Parameters 的方法
   # 加上驗證 name 不能重覆 (關鍵字提示: uniqueness)
