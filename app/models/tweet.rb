b3340a6b06417d5436377a803f7830887a337717
Henry <henry81325@gmail.com>
2018-07-27 10:34:14 +0800

    update tweet index page for user

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,6 @@
 class Tweet < ApplicationRecord
   validates_length_of :description, maximum: 140,:message => "限字140"
+  
   belongs_to :user, counter_cache: true
 
   has_many :replies, dependent: :destroy
