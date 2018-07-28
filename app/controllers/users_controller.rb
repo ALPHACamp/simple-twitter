ed553d6c7765f2ddde03160b1ae0d07ca23a4589
Kafka Tzeng <freefish0308@gmail.com>
2018-07-28 15:01:56 +0800

    profit#edit and page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,9 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = current_user
+     #redirect_to tweets_user_path(@user)
+    
   end
 
   def update
@@ -20,5 +23,10 @@ class UsersController < ApplicationController
   def likes
     @likes # 基於測試規格，必須講定變數名稱
   end
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :avatar)
+  end
+
 
 end
