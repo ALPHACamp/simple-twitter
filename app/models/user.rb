3d5ab530275ee5957a215ad0d9540e5106b06cc0
Cynthia <ex09190919@yahoo.com.tw>
2018-07-28 13:36:02 +0800

    setup user and follower relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,10 @@ class User < ApplicationRecord
   has_many :followships, dependent: :destroy
   has_many :followings, through: :followships
 
+  # 設定追蹤者關聯
+  has_many :inverse_followships, class_name: "Followship", foreign_key: "following_id"
+  has_many :followers, through: :inverse_followships, source: :user
+
   # 需要 app/views/devise 裡找到樣板，加上 name 屬性
   # 並參考 Devise 文件自訂表單後通過 Strong Parameters 的方法
   # 加上驗證 name 不能重覆 (關鍵字提示: uniqueness)
