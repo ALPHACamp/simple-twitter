fe631079a1f91f6bcfcc0f13b1d0ed1a2fe5efa5
Kafka Tzeng <freefish0308@gmail.com>
2018-07-28 15:48:20 +0800

    profit#create and set bootstrap

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :tweets, :followings, :followers, :likes]
 
   def tweets
   end
@@ -10,6 +11,8 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user.update(user_params)
+    #redirect_to tweets_user_path(@user)
   end
 
   def followings
@@ -22,6 +25,12 @@ class UsersController < ApplicationController
 
   def likes
     @likes # 基於測試規格，必須講定變數名稱
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
   def user_params
