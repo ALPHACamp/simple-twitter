de673b2f32827d35ac4cead0038418d1b9129d77
yvesen <l.sen.t@hotmail.com>
2018-07-28 18:27:12 +0800

    add Kaminari

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -3,6 +3,7 @@ class Admin::TweetsController < Admin::BaseController
   before_action :set_tweet, only:  [:show, :destroy]
   def index
     @tweets = Tweet.order(created_at: :desc)
+    @tweets = Tweet.page(params[:page]).per(10)
   end
 
   def destroy
