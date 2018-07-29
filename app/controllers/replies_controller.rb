ff37fac0cf159196448fea23512aa5af7078cef3
yungcheng <yungcheng111023@gmail.com>
2018-07-29 15:40:50 +0800

    delete reply_item page to fix bug

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -3,16 +3,16 @@ class RepliesController < ApplicationController
   def index
     @tweet = Tweet.find(params[:tweet_id])
     @user = @tweet.user
-    @replies = @tweet.replies.order(update_at: :desc)
     @reply = Reply.new
+    @replies = Reply.where('tweet_id' => params[:tweet_id])
   end
 
   def create
-    tweet = Tweet.find(params[:tweet_id])
-    reply = tweet.replies.create(reply_params)
-    reply.user = current_user
-    repy.save
-    redirect_to tweet_replies_path(tweet)
+    @tweet = Tweet.find(params[:tweet_id])
+    @reply = @tweet.replies.create(reply_params)
+    @reply.user = current_user
+    @reply.save!
+    redirect_back(fallback_location: root_path)
   end
 
   private
