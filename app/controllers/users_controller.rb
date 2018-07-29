295b0e7c49e92e65b7c538393f94b397b922d02b
yungcheng <yungcheng111023@gmail.com>
2018-07-29 17:28:21 +0800

    add link_to followings/followers/likes page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,17 +24,19 @@ class UsersController < ApplicationController
   end
 
   def followings
-    @followings # 基於測試規格，必須講定變數名稱
-    @followers.count_likes
+    @user = User.find(params[:id])
+    @followings = @user.followings.order(created_at: :desc)# 基於測試規格，必須講定變數名稱
   end
 
   def followers
-    @followers # 基於測試規格，必須講定變數名稱
-    @followers.count_likes
+    @user = User.find(params[:id])
+    @followers = @user.followers.order(created_at: :desc)# 基於測試規格，必須講定變數名稱
+ 
   end
 
   def likes
-    @likes # 基於測試規格，必須講定變數名稱
+    @user = User.find(params[:id])
+    @likes = @user.likes.order(created_at: :desc)# 基於測試規格，必須講定變數名稱
   end
 
   private
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,5 @@
 class Like < ApplicationRecord
 
-   belongs_to :user, optional: true
+   belongs_to :user
    belongs_to :tweet, optional: true
 end
