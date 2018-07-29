b2c496dd6fca60e6ef3b55855abc807e9938cbc0
Ryan814 <rytg156@gmail.com>
2018-07-29 21:19:22 +0800

    add login process and update view with login messages

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -1,5 +1,4 @@
 class Admin::TweetsController < ApplicationController
-
   def index
     
   end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,6 @@
 class TweetsController < ApplicationController
-
+  before_action :authenticate_user!
+  
   def index
     
   end
