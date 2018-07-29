0c32641532cf48fb8698e6f5636700cfe1c8a5a8
yungcheng <yungcheng111023@gmail.com>
2018-07-29 08:08:27 +0800

    add _tweet_item page

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,12 +1,25 @@
 class RepliesController < ApplicationController
 
   def index
-
+    @tweet = Tweet.find(params[:tweet_id])
+    @user = @tweet.user
+    @replies = @tweet.replies.order(update_at: :desc)
+    @reply = Reply.new
   end
 
   def create
-
-
+    tweet = Tweet.find(params[:tweet_id])
+    reply = tweet.replies.create(reply_params)
+    reply.user = current_user
+    repy.save
+    redirect_to tweet_replies_path(tweet)
   end
+
+  private
+
+  def reply_params
+    params.require(:reply).permit(:comment)
+  end
+  
 
 end
