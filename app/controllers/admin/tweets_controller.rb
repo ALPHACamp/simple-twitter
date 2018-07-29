14d2e8372b4b41b0e782c752044564bc780110cd
huier <andychen522.ambit@gmail.com>
2018-07-29 18:31:13 +0800

    add index and sestroy action for admin

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -3,8 +3,20 @@ class Admin::TweetsController < Admin::BaseController
   before_action :authenticate_admin
   
   def index
+    # 顯示全部 tweets 的資料
+    @tweets = Tweet.all
   end
 
   def destroy
+    @tweets = Tweet.find(params[:id])
+    @tweets.destroy
+    redirect_to admin_tweets_path
+    falsh[:alert] = "tweet was delete"
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:description)
   end
 end
