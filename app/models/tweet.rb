23d8b21ae55bdb363031153edd077565ecd87f9d
Cynthia <ex09190919@yahoo.com.tw>
2018-07-29 22:22:29 +0800

    add is_liked? method to tweet model for checking like/unlike button

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -11,4 +11,8 @@ class Tweet < ApplicationRecord
 # 使用者可以喜歡很多評論
   has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
+
+  def is_liked?(user)
+    self.liked_users.include?(user)
+  end
 end
