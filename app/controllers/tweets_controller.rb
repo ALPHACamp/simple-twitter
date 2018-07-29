4d72980f709ea61318b8be16e6445c46a2f25de4
Cynthia <ex09190919@yahoo.com.tw>
2018-07-29 21:57:33 +0800

    add user like and unlike tweet action

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -20,9 +20,16 @@ before_action :authenticate_user!
   end
 
   def like
+    @tweet = Tweet.find(params[:id])
+    @tweet.likes.create!(user: current_user)
+    redirect_back(fallback_location: root_path) #導回上一頁
   end
 
   def unlike
+    @tweet = Tweet.find(params[:id])
+    likes = Like.where(tweet: @tweet, user: current_user)
+    likes.destroy_all
+    redirect_back(fallback_location: root_path)
   end
 
   private
