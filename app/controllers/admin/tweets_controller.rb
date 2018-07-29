62c762e20e713725fb15cd805a3a9a0c06147a3c
Ryan814 <rytg156@gmail.com>
2018-07-29 21:35:28 +0800

    set up admin authenticate

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -1,5 +1,17 @@
 class Admin::TweetsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :authenticate_admin
+
   def index
     
+  end
+
+  private
+
+    def authenticate_admin
+      unless current_user.admin?
+        flash[:alert] = "非工作人員請勿進入!"
+        redirect_to root_path
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+  
+  def admin?
+    self.role == "admin"
+  end
 end
