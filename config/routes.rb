ffc3ab36042437ee97f3ca760a408aa7bcf41b71
Ryan814 <rytg156@gmail.com>
2018-07-29 11:39:25 +0800

    create user model using devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :tweets, only: [:index, :create ]
 
@@ -6,5 +7,6 @@ Rails.application.routes.draw do
 
   namespace :admin do 
     resources :tweets, only: [:index, :destroy]
+    resources :users, only: [:index]
   end
 end
