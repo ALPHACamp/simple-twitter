470a257dfe6521f169b0bc1810e70298cd73f191
Cynthia <ex09190919@yahoo.com.tw>
2018-07-31 05:59:37 +0800

    add find user tweets order created time desc and modify another contorller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
-before_action :set_user, only: [:edit, :update, :followings, :followers]
+before_action :set_user, only: [:edit, :update, :followings, :followers, :likes, :tweets]
   def tweets
     @user = User.find(params[:id])
+    @tweets = @user.tweets.includes(:user).order("tweets.created_at desc")
   end
-
-
+  
   def edit
     unless @user == current_user
       redirect_to edit_user_path(@user)
@@ -26,7 +26,6 @@ before_action :set_user, only: [:edit, :update, :followings, :followers]
   end
 
   def likes
-    @user = User.find(params[:id])
     @likes = @user.liked_tweets.includes(:likes).order("likes.created_at desc")
     # 基於測試規格，必須講定變數名稱
   end
