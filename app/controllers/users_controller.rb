de3a906824a243bba6b8b4c4c176ac4d5edbe960
huier <andychen522.ambit@gmail.com>
2018-07-31 01:51:01 +0800

    add update action on users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:tweets, :edit, :update, :followings, :followers]
 
   def tweets
   end
@@ -7,6 +8,13 @@ class UsersController < ApplicationController
   end
 
   def update
+    if @user.update(user_params)
+      flash[:notice] = "Profile was successfully updated"
+      redirect_to tweets_user_path(@user)
+    else
+      flash[:alert] = "Profile was failed to update"
+      render :edit
+    end
   end
 
   def followings
@@ -19,6 +27,16 @@ class UsersController < ApplicationController
 
   def likes
     @likes # 基於測試規格，必須講定變數名稱
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :avatar, :introduction)
   end
 
 end
