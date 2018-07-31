4b3e8025f16c14df033cdb0c4349d6870673ee8d
Allie <zxp800310@gmail.com>
2018-07-31 19:12:28 +0800

    admin can see tweets_count on admin panel

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,5 @@
 class Admin::UsersController < Admin::BaseController
   def index
-    @users = User.all.order(created_at: :desc)
+    @users = User.all.order(tweets_count: :desc)
   end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,7 +1,8 @@
 class Tweet < ApplicationRecord
   validates_length_of :description, maximum: 140
 
-  belongs_to :user
+  belongs_to :user, counter_cache: true
+  
   has_many :replies, dependent: :destroy
   has_many :replied_users, through: :replies, source: :user
 
