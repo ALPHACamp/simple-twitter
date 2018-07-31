bc2579f7bde824464b282f3255bcdf3ec4526c91
Cynthia <ex09190919@yahoo.com.tw>
2018-07-31 13:23:34 +0800

    transfer admin authenticate to admin base controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,13 +13,4 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 
-  private
-
-  def authenticate_admin
-    unless current_user.admin? #admin?要另外在user model定義
-      flash[:alert] = "Not allow!"
-      redirect_to root_path
-    end
-  end
-
 end
