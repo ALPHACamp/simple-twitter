151a31500fe70e2c56a9b84705e8bc6408e14cc1
Ryan814 <rytg156@gmail.com>
2018-07-31 11:31:02 +0800

    add Admin::BaseController and move authenticate_admin here

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -1,5 +1,4 @@
-class Admin::TweetsController < ApplicationController
-  before_action :authenticate_admin
+class Admin::TweetsController < Admin::BaseController
 
   def index
     @tweets = Tweet.page(params[:page]).per(10)
@@ -12,12 +11,4 @@ class Admin::TweetsController < ApplicationController
     flash[:alert] = "推文已刪除"
   end
 
-  private
-
-    def authenticate_admin
-      unless current_user.admin?
-        flash[:alert] = "非工作人員請勿進入!"
-        redirect_to root_path
-    end
-  end
 end
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,4 @@
-class Admin::UsersController < ApplicationController
+class Admin::UsersController < Admin::BaseController
   before_action :authenticate_admin
   
   def index
