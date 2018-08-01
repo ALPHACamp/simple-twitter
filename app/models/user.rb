19f187270ccbc385e565bbfbd93fd738d84ce25b
Cynthia <ex09190919@yahoo.com.tw>
2018-08-01 09:24:40 +0800

    modify user and replies relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,8 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :liked_tweets, through: :likes, source: :tweet
 
+  # 設定使用者會回復很多tweets
+  has_many :replies, dependent: :destroy
   # 需要 app/views/devise 裡找到樣板，加上 name 屬性
   # 並參考 Devise 文件自訂表單後通過 Strong Parameters 的方法
   # 加上驗證 name 不能重覆 (關鍵字提示: uniqueness)
