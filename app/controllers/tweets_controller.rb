3d2379f744e962f6ed6efcea0d93f461a277c5c5
Ryan814 <rytg156@gmail.com>
2018-08-01 21:53:40 +0800

    create and styled index page for public

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,6 +2,8 @@ class TweetsController < ApplicationController
   
   
   def index
-    
+    @tweets = Tweet.all
+    @users = User.all.order(follower_count: :desc).limit(10)
+    @tweet = Tweet.new
   end
 end
