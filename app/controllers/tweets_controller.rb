e3072d9bf525a16609bceeb7d4d5b87a4c5ca5d9
Cynthia <ex09190919@yahoo.com.tw>
2018-08-02 17:12:44 +0800

    modify   tweet #index user log in tweets behaviour can see all tweets instance

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,10 +2,10 @@ class TweetsController < ApplicationController
 # 登入認證
 before_action :authenticate_user!
   def index
+    @tweets = Tweet.all.order(created_at: :desc)
+    @tweet = Tweet.new #在index建立tweet容器來建立短文
     # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
     @users = User.order(followers_count: :desc).limit(10)
-    @tweets = Tweet.all
-    @tweet = Tweet.new #在index建立tweet容器來建立短文
   end
 
   def create
