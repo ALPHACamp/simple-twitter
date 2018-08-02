10e9356c93c3e5da9f214b8f009c4c4725fffabd
Cynthia <ex09190919@yahoo.com.tw>
2018-08-02 09:44:01 +0800

    modify user validation should validate that :name cannot be empty/falsy

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-  # user大頭照照片上傳器
+# user大頭照照片上傳器
   mount_uploader :avatar, AvatarUploader
-
+  
+# 加上驗證 name 不能重覆 (關鍵字提示: uniqueness)
+  #validates_presence_of :name, :uniqueness => {:case_sensitive => true}
+  validates_uniqueness_of :name
+  validates_presence_of :name
   # 設定user跟tweet關聯，一個user可以有很多tweets
   has_many :tweets, dependent: :destroy
   # 設定追蹤關聯紀錄
@@ -25,8 +28,8 @@ class User < ApplicationRecord
   has_many :replies, dependent: :destroy
   # 需要 app/views/devise 裡找到樣板，加上 name 屬性
   # 並參考 Devise 文件自訂表單後通過 Strong Parameters 的方法
-  # 加上驗證 name 不能重覆 (關鍵字提示: uniqueness)
-  validates_presence_of :name, :uniqueness => {:case_sensitive => false}
+
+  
 
 def admin?
   self.role == "admin"
