c93d22e8de659851f3d530feb2cc15a04ba39861
Cynthia <ex09190919@yahoo.com.tw>
2018-08-02 16:18:45 +0800

    modify admin::tweet go to admin root page if admin user log in can delete other users tweet

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -3,13 +3,13 @@ class Admin::TweetsController < Admin::BaseController
 before_action :authenticate_user!
 before_action :authenticate_admin
   def index
-    @tweets = Tweet.all #管理者瀏覽所有tweet資料
+    @tweets = Tweet.all.order(created_at: :desc) #管理者瀏覽所有tweet資料
   end
 
   def destroy
     @tweets = Tweet.find(params[:id])
     @tweets.destroy
-    redirect_to admin_tweets_path
+    redirect_to admin_root_path
     flash[:alert] = "tweet was deleted"
   end
 end
