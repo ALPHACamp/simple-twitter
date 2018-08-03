9aa21826b8cb9da10b35e013731fa44a19a055fb
Cynthia <ex09190919@yahoo.com.tw>
2018-08-03 12:25:59 +0800

    modify followship #create when user1 wants to follow user2 can not follow self

--- app/models/followship.rb
+++ app/models/followship.rb
@@ -1,5 +1,14 @@
 class Followship < ApplicationRecord
+  # 限制使用者只能追蹤另一個使用者一次
   validates :following_id, uniqueness: { scope: :user_id }
+  # 限制是否追蹤自己
+  validate :check_self_following
   belongs_to :user
   belongs_to :following, class_name:"User"
+
+  def check_self_following
+    if user_id == following_id
+      errors[:following_id] <<  "Can's follow yourself"
+    end
+  end
 end
