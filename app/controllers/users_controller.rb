b2c2fe6d4663a7fe91e7c2f9626a7a8f830eeeac
liuchunyueh <liuchunyueh@gmail.com>
2018-08-09 01:49:01 +0800

    change the order of likes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,7 +36,7 @@ class UsersController < ApplicationController
 
   def likes
     @user = User.find(params[:id])
-    @likes = @user.likes # 基於測試規格，必須講定變數名稱
+    @likes = @user.likes.order(created_at: :desc) # 基於測試規格，必須講定變數名稱
   end
 
   private
