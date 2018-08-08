398d5d735f193a05db46d342ef0ff58447fc26b1
liuchunyueh <liuchunyueh@gmail.com>
2018-08-09 01:22:24 +0800

    change name of variable

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,7 +2,7 @@ class TweetsController < ApplicationController
   def index
    @users = User.order(followers_count: :desc).limit(10) # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
    @tweet = Tweet.new
-   @index_post = Tweet.order(created_at: :desc).limit(15)
+   @tweets = Tweet.order(created_at: :desc).limit(15)
 
   end
 
