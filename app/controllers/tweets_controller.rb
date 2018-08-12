463aae1d05883d341b925ee8ac78170e08003817
huier <andychen522.ambit@gmail.com>
2018-08-12 15:55:57 +0800

    add index method on tweets_controller for public user

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,6 +2,9 @@ class TweetsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @tweet = Tweet.new
+    # 每頁10則tweet，按時間排序
+    @tweets = Tweet.order(updated_at: :desc).page(params[:page]).per(10)
     @users # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
   end
 
