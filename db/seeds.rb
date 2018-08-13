3897065ffce529949429870933c27e56d66e7b1d
huier <andychen522.ambit@gmail.com>
2018-08-13 20:55:40 +0800

    add admin user data on seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(email: "admin@example.co", password: "root12345", role: "admin", name: "huihui")
+puts "Default admin created!"
