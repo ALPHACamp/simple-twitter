99b293c9c4f90183d4ac21aff0045c1aaf48ad40
Ryan814 <rytg156@gmail.com>
2018-08-15 21:17:10 +0800

    to fit the assignment's require

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -6,6 +6,7 @@ class Admin::TweetsController < Admin::BaseController
 
   def destroy
     @tweet = Tweet.find(params[:id])
+
     if current_user.admin?
       @tweet.destroy
       redirect_to admin_tweets_path
--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -2,12 +2,13 @@ class FollowshipsController < ApplicationController
 
   def create
     @followship = current_user.followships.build(following_id: params[:following_id])
-
-    if @followship.save
-      flash[:notice] = "Successfully followed"
+    @user = User.find(params[:following_id])
+    if @user == current_user
+      flash[:alert] = @followship.errors.full_messages.to_sentence
       redirect_back(fallback_location: root_path)
     else
-      flash[:alert] = @followship.errors.full_messages.to_sentence
+      @followship.save
+      flash[:notice] = "Successfully followed"
       redirect_back(fallback_location: root_path)
     end
   end
