2e9c5bd199bf31a7dc7fe4fbdcb67aad2ec3d63f
huier <andychen522.ambit@gmail.com>
2018-08-18 15:53:17 +0800

    add Reply link to replies#index on tweets#index page

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,5 +1,5 @@
 class Reply < ApplicationRecord
   # counter_cache 可以自動回傳 size 方法結果，計算關聯物件數量
-  belongs_to :user, counter_cache: true
+  belongs_to :user
   belongs_to :tweet, counter_cache: true
 end
