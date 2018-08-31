c893fc3959218e6bbb90540f8356189d4d6a4b03
allenpan <panjau@gmail.com>
2018-08-31 11:29:43 +0800

    add followers function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,8 @@ class UsersController < ApplicationController
   end
 
   def followers
-    @followers # 基於測試規格，必須講定變數名稱
+    @user = User.find(params[:id])
+    @followers = @user.followers# 基於測試規格，必須講定變數名稱
   end
 
   def likes
