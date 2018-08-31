b0e674c47f0edecf5a7f30bf521fa89bcb3046d9
allenpan <panjau@gmail.com>
2018-08-31 22:39:02 +0800

    fix incorrect redirect path in #like #unlike

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -11,7 +11,7 @@ class TweetsController < ApplicationController
     @tweet.user = current_user
     if @tweet.save
       flash[:notice] = "You Tweet was successfully created"
-      redirect_to root_path
+      redirect_to tweets_path
     else
       flash.now[:alert] = "Your Tweet was failed to create"
       render :index
@@ -21,14 +21,14 @@ class TweetsController < ApplicationController
   def like
     @tweet = Tweet.find(params[:id])
     @tweet.likes.create!(user: current_user)
-    redirect_back(fallback_location: root_path)
+    redirect_back(fallback_location: tweets_path)
   end
 
   def unlike
     @tweet = Tweet.find(params[:id])
     like_items = Like.where(tweet: @tweet, user: current_user)
     like_items.destroy_all
-    redirect_back(fallback_location: root_path)
+    redirect_back(fallback_location: tweets_path)
   end
 
 
