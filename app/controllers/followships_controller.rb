5e1b5f06acc57cac2e9749a282e9b6c541daa29f
allenpan <panjau@gmail.com>
2018-08-31 22:39:42 +0800

    fix can't follow self issue

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -1,13 +1,15 @@
 class FollowshipsController < ApplicationController
   def create
+    
     @followship = current_user.followships.build(following_id: params[:following_id])
-
-    if @followship.save
-      flash[:notice] = "Successfilly followed"
-      redirect_back(fallback_location: tweets_user_path(params[:following_id]))
-    else
-      flash[:alert] = @followship.errors.full_messages.to_sentence
-      redirect_back(fallback_location: tweets_user_path(params[:following_id]))
+    if @followship.user_id != @followship.following_id
+      if @followship.save
+        flash[:notice] = "Successfilly followed"
+        redirect_back(fallback_location: tweets_user_path(params[:following_id]))
+      else
+        flash[:alert] = @followship.errors.full_messages.to_sentence
+        redirect_back(fallback_location: tweets_user_path(params[:following_id]))
+      end
     end
   end
 
