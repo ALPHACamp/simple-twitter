ad4af518f80eaa1eb4b3b7197d75565615889939
huier <andychen522.ambit@gmail.com>
2018-09-01 14:07:39 +0800

    add @user action on  TweetsController

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -7,7 +7,8 @@ class TweetsController < ApplicationController
     @tweet = Tweet.new
     # 每頁10則tweet，按時間排序
     @tweets = Tweet.order(updated_at: :desc).page(params[:page]).per(10)
-    @users # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
+    # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
+    @users = User.order(followers_count: :desc).limit(10)
   end
 
   def create
