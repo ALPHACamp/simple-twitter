2266a323aa1bc339624503274e78f49c9f825ebe
huier <andychen522.ambit@gmail.com>
2018-09-03 22:44:54 +0800

    modify index action on Admin::TweetsController

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -2,14 +2,15 @@ class Admin::TweetsController < Admin::BaseController
   
   def index
     # 顯示全部 tweets 的資料
-    @tweets = Tweet.all
+    # @tweets = Tweet.all
+    @tweets = Tweet.order(updated_at: :desc).page(params[:page]).per(10)
   end
 
   def destroy
     @tweets = Tweet.find(params[:id])
     @tweets.destroy
     redirect_to admin_tweets_path
-    falsh[:alert] = "tweet was delete"
+    flash[:alert] = "tweet was delete"
   end
 
   private
