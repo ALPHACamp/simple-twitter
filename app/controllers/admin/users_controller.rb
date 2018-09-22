dc5e96712a65507082ad5728b9f26766bb4d1a90
huier <andychen522.ambit@gmail.com>
2018-09-22 23:29:48 +0800

    add destroy action on users controller for admin

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,5 +2,18 @@ class Admin::UsersController < Admin::BaseController
   def index
     @users = User.order(tweets_count: :desc)
   end
+
+  def destroy
+    @users = User.find(params[:id])
+    @users.destroy
+    redirect_to admin_users_path
+    flash[:alert] = "User was delete"
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :avatar, :introduction)
+  end
   
 end
