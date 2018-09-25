8715f4b8fbd736f0d025314db9cebb4befd43d1b
huier <andychen522.ambit@gmail.com>
2018-09-26 01:58:20 +0800

    fix bug: Tweet #post or #like and like will redirect to index

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -15,7 +15,7 @@ class TweetsController < ApplicationController
     @tweet = current_user.tweets.build(tweet_params)
     if @tweet.save
       flash[:notice] = "tweet was successfully created"
-      redirect_to root_path
+      redirect_to :tweets
     else
       flash[:alert] = "tweet was failed to created"
       render :index
@@ -26,7 +26,7 @@ class TweetsController < ApplicationController
     # 先用 params[:id] 取出tweet物件, 新建一筆 Like 紀錄，並設定 tweet_id 和 user_id 的外鍵。
     Like.create(tweet: @tweet, user: current_user)
     # @tweet.likes.create!(user: current_user)
-    redirect_back(fallback_location: root_path)
+    redirect_back(fallback_location: :tweets)
   end
 
   def unlike
@@ -34,7 +34,7 @@ class TweetsController < ApplicationController
     likes = Like.where(tweet: @tweet, user: current_user)
     # 而刪除集合要搭配 destroy_all 來使用
     likes.destroy_all
-    redirect_back(fallback_location: root_path)
+    redirect_back(fallback_location: :tweets)
   end
 
   def destroy
